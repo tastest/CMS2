06cd9c2e3dd132cd7b434a583cef1186e0f936a0
jmuelmen <jmuelmen>
2008-08-12 22:01:29 +0000

    common selections for all analyses

--- NtupleMacros/Tools/selections.C
+++ NtupleMacros/Tools/selections.C
@@ -1,10 +1,13 @@
- //===========================================================
+//===========================================================
 //
 // Various selection functions are kept here
 //
 //============================================================
 #include "Math/LorentzVector.h"
 #include "TMath.h"
+// #include "selections.h"
+//#include <vector>
+//#include "CMS1.h"
 #include "TDatabasePDG.h"
 
 //----------------------------------------------------------------
@@ -19,42 +22,42 @@ bool inZmassWindow (float mass) {
 // true electron
 //----------------------------------------------------------------
 // bool trueElectron(int index) {
-//   if ( abs(els_mc_id[index]) != 11 ) return false;
-//   if ( abs(els_mc_motherid[index]) == 23 || abs(els_mc_motherid[index]) == 24) return true;
+//   if ( abs(cms2.els_mc_id()[index]) != 11 ) return false;
+//   if ( abs(cms2.els_mc_motherid()[index]) == 23 || abs(cms2.els_mc_motherid()[index]) == 24) return true;
 //   return false;
 // }
-//----------------------------------------------------------------
-// true muon
-//----------------------------------------------------------------
+// //----------------------------------------------------------------
+// // true muon
+// //----------------------------------------------------------------
 // bool trueMuon(int index) {
-//    if ( abs(mus_mc_id[index]) != 13 ) return false;
-//    if ( abs(mus_mc_motherid[index]) == 23 || abs(mus_mc_motherid[index]) == 24) return true;
+//    if ( abs(cms2.mus_mc_id()[index]) != 13 ) return false;
+//    if ( abs(cms2.mus_mc_motherid()[index]) == 23 || abs(cms2.mus_mc_motherid()[index]) == 24) return true;
 //   return false;
 // }
 //----------------------------------------------------------------
 // Electron ID without isolation
 //----------------------------------------------------------------
 bool goodElectronWithoutIsolation(int index) {
-  if ( els_tightId()[index]     !=  1) return false;
-  if ( els_closestMuon()[index] != -1) return false;
-  if ( abs(els_d0()[index]) > 0.025)   return false;
+  if ( cms2.els_tightId().at(index)     !=  1) return false;
+  if ( cms2.els_closestMuon().at(index) != -1) return false;
+  if ( abs(cms2.els_d0().at(index)) > 0.025)   return false;
   return true;
 }
 //----------------------------------------------------------------
 // Muon ID without isolation
 //---------------------------------------------------------------
 bool goodMuonWithoutIsolation(int index) {
-  if (mus_gfit_chi2()[index]/mus_gfit_ndof()[index] > 5.) return false;
-  if (abs(mus_d0()[index])   > 0.25) return false;
-  if (mus_validHits()[index] < 7)    return false;
+  if (cms2.mus_gfit_chi2().at(index)/cms2.mus_gfit_ndof().at(index) > 5.) return false;
+  if (abs(cms2.mus_d0().at(index))   > 0.25) return false;
+  if (cms2.mus_validHits().at(index) < 7)    return false;
   return true;
 }
 //-----------------------------------------------------------
 // Electron Isolation
 //-----------------------------------------------------------
 bool passElectronIsolation(int index) {
-  double sum = els_tkIso()[index];
-  double pt  = els_p4()[index].pt();
+  double sum = cms2.els_tkIso().at(index);
+  double pt  = cms2.els_p4().at(index).pt();
    if ( pt/(pt+sum) < 0.92) return false;
   return true;  
 } 
@@ -62,10 +65,10 @@ bool passElectronIsolation(int index) {
 // Muon Isolation
 //-----------------------------------------------------------
 bool passMuonIsolation(int index) {
-  double sum =  mus_iso03_sumPt()[index] +  
-                mus_iso03_emEt()[index]  +
-                mus_iso03_hadEt()[index];
-  double pt  = mus_p4()[index].pt(); 
+  double sum =  cms2.mus_iso03_sumPt().at(index) +  
+                cms2.mus_iso03_emEt().at(index)  +
+                cms2.mus_iso03_hadEt().at(index);
+  double pt  = cms2.mus_p4().at(index).pt();
   if ( pt/(pt+sum) < 0.92) return false;
   return true;  
 }
@@ -85,6 +88,23 @@ bool goodElectronIsolated(int index) {
   if (!passElectronIsolation(index))       return false;
   return true;
 }
+//--------------------------------------------
+// Pass 2 MET selection
+//--------------------------------------------
+bool pass2Met (int i_hyp) {
+  // for e-e and mu-mu
+  if (cms2.hyp_type()[i_hyp] == 0 || cms2.hyp_type()[i_hyp] == 3) {
+    if (cms2.hyp_met()[i_hyp] < 30) return false;
+    //    if ( fabs(hyp_p4[i_hyp]->mass()-90.0)<10.0) return false;
+    if( cms2.hyp_met()[i_hyp]/cms2.hyp_p4()[i_hyp].pt()<0.6 && 
+	acos(cos(cms2.hyp_metPhi()[i_hyp]-cms2.hyp_p4()[i_hyp].phi()-3.1416))<0.25 ) return false;
+  }
+  // for e-mu and mu-e
+  if (cms2.hyp_type()[i_hyp] == 1 || cms2.hyp_type()[i_hyp] == 2) {
+    if (cms2.hyp_met()[i_hyp] < 20) return false;
+  }
+  return true;
+}
 
 //-------------------------------------------------
 // Auxiliary function to scan the doc line and 
@@ -92,23 +112,24 @@ bool goodElectronIsolated(int index) {
 //-------------------------------------------------
 int getDrellYanType() {
   bool foundZ;
-  int size = genps_id().size();
+  int size = cms2.genps_id().size();
   for (int jj=0; jj<size; jj++) {
-    if (genps_id()[jj] == 23) {
+    if (cms2.genps_id().at(jj) == 23) {
       foundZ = true;
       if (jj+3 > size) {
 	std::cout << 
-         "Found Z but not enough room in doc lines for leptons?" << std::endl;
-	return 999;
+	  "Found Z but not enough room in doc lines for leptons?" << std::endl;
+        return 999;
       }
-      if (abs(genps_id()[jj+1]) == 11) return 0;  //DY->ee
-      if (abs(genps_id()[jj+1]) == 13) return 1;  //DY->mm
-      if (abs(genps_id()[jj+1]) == 15) return 2;  //DY->tautau
+      if (abs(cms2.genps_id().at(jj+1)) == 11) return 0;  //DY->ee
+      if (abs(cms2.genps_id().at(jj+1)) == 13) return 1;  //DY->mm
+      if (abs(cms2.genps_id().at(jj+1)) == 15) return 2;  //DY->tautau
     }
   }
   std::cout << "Does not look like a DY event" << std::endl;
   return 999;
 }
+
 //--------------------------------------------
 // Booleans for DY
 //------------------------------------------
@@ -124,6 +145,7 @@ bool isDYtt() {
   if (getDrellYanType() == 2) return true;
   return false;
 }
+
 //--------------------------------------------------------------------
 // Veto events if there are two leptons in the 
 // event that make the Z mass.  This uses the mus and els
@@ -132,12 +154,6 @@ bool isDYtt() {
 //
 // Both leptons must be 20 GeV, and pass the same cuts as 
 // the hypothesis leptons, except that one of them can be non-isolated
-//
-// Because isolation is not in the els block, for Z->ee veto we are 
-// forced to use the hyp_block for one of them (if we want to put 
-// an isolation requirement).  This would be a problem only for 
-// Z->ee + 2 other hyp leptons (unlikely)
-// We also assume that the leptons in hyp block have already passed isolation
 //---------------------------------------------------------------------
 bool additionalZveto() {
 
@@ -145,69 +161,65 @@ bool additionalZveto() {
   bool veto=false;
 
   // first, look for Z->mumu
-  for (unsigned int i=0; i<mus_p4().size(); i++) {
-    if (mus_p4()[i].pt() < 20.)     continue;
+  for (int i=0; i < cms2.mus_p4().size(); i++) {
+    if (cms2.mus_p4().at(i).pt() < 20.)     continue;
     if (!goodMuonWithoutIsolation(i)) continue;
 
-    for (unsigned int j=i+1; j<mus_p4().size(); j++) {
-      if (mus_p4()[j].pt() < 20.) continue;
+    for (int j=i+1; j < cms2.mus_p4().size(); j++) {
+      if (cms2.mus_p4().at(j).pt() < 20.) continue;
       if (!goodMuonWithoutIsolation(j)) continue;
-      if (mus_charge()[i] == mus_charge()[j]) continue;
+      if (cms2.mus_charge().at(i) == cms2.mus_charge().at(j)) continue;
 
       // At least one of them has to pass isolation
       if (!passMuonIsolation(i) && !passMuonIsolation(j)) continue;
 
       // Make the invariant mass
       ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > 
-                vec = mus_p4()[i] + mus_p4()[j];
+                vec = cms2.mus_p4().at(i) + cms2.mus_p4().at(j);
       if ( inZmassWindow(vec.mass()) ) return true;
 
     }
   }
 
-  // Now look at Z->ee.  
-  for (unsigned int i=0; i<evt_nels(); i++) {
-    if (els_p4()[i].pt() < 20.)     continue;
+  // now, look for Z->ee
+  for (int i=0; i < cms2.els_p4().size(); i++) {
+    if (cms2.els_p4().at(i).pt() < 20.)     continue;
     if (!goodElectronWithoutIsolation(i)) continue;
 
-    for (unsigned int j=i+1; j<evt_nels(); j++) {
-      if (els_p4()[j].pt() < 20.) continue;
+    for (int j=i+1; j<cms2.els_p4().size(); j++) {
+      if (cms2.els_p4().at(j).pt() < 20.) continue;
       if (!goodElectronWithoutIsolation(j)) continue;
-      if (els_charge()[i] == els_charge()[j]) continue;
+      if (cms2.els_charge().at(i) == cms2.els_charge().at(j)) continue;
 
       // At least one of them has to pass isolation
       if (!passElectronIsolation(i) && !passElectronIsolation(j)) continue;
 
       // Make the invariant mass
       ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > 
-                vec = els_p4()[i] + els_p4()[j];
+                vec = cms2.els_p4().at(i) + cms2.els_p4().at(j);
       if ( inZmassWindow(vec.mass()) ) return true;
+
     }
   }
   // done
   return veto;
 }
 
-
-
-  
-
-
 //------------------------------------------------------------
 // Not a selection function per se, but useful nonetheless:
 // dumps the documentation lines for this event
 //------------------------------------------------------------
 void dumpDocLines() {
-  int size = genps_id().size();
+  int size = cms2.genps_id().size();
   // Initialize particle database
   TDatabasePDG *pdg = new TDatabasePDG();
   std::cout << "------------------------------------------" << std::endl;
   for (int j=0; j<size; j++) {
-    cout << setw(9) << left << pdg->GetParticle(genps_id()[j])->GetName() << " "
-         << setw(7) << right << setprecision(4) << genps_p4()[j].pt() << "  "
-         << setw(7) << right << setprecision(4) << genps_p4()[j].phi() << "  "
-         << setw(10) << right << setprecision(4) << genps_p4()[j].eta() << "  "
-         << setw(10) << right << setprecision(4) << genps_p4()[j].mass() << endl;
+    cout << setw(9) << left << pdg->GetParticle(cms2.genps_id().at(j))->GetName() << " "
+         << setw(7) << right << setprecision(4) << cms2.genps_p4().at(j).pt() << "  "
+         << setw(7) << right << setprecision(4) << cms2.genps_p4().at(j).phi() << "  "
+         << setw(10) << right << setprecision(4) << cms2.genps_p4().at(j).eta() << "  "
+         << setw(10) << right << setprecision(4) << cms2.genps_p4().at(j).mass() << endl;
   }
   // Clean up
   delete pdg;
@@ -253,4 +265,3 @@ void correctMETmuons_crossedE(double& met, double& metPhi,
    met   = sqrt(metx*metx + mety*mety);
    metPhi = atan2(mety, metx);
 }
-
