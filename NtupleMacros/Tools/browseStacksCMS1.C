c16c4d81cfd3bdfad5d21a95c917fc63a79285ed
gutsche <gutsche>
2008-08-31 13:12:37 +0000

    TriLepton stack plots

--- NtupleMacros/Tools/browseStacksCMS1.C
+++ NtupleMacros/Tools/browseStacksCMS1.C
@@ -50,3 +50,67 @@ void browseStacks( bool makePictures=false, bool wait=true ) {
    }
    if (makePictures) c->Print("out/stacks.ps]");
 }
+
+void browseStacksTrilep( bool makePictures=false, bool wait=true ) {
+
+  bool keep2D=false;
+
+// Find out what the names of the existing histograms are
+// The histogram names are XX_YY_ZZ, where XX is the sample,
+// eg, "tt", YY is the actual name, ZZ is the final state, eg, "ee"
+  TObjArray* myNames = getMyHistosNames("tt","mpmpmp",keep2D);
+
+
+// Now loop over histograms, and make stacks
+   TCanvas *c = new TCanvas();
+   c->Divide(6,4);
+  const unsigned int allBuckets = 20;
+  char *suffix[allBuckets+1];
+  suffix[0]  = "mpmpmp";
+  suffix[1]  = "mpmpmm";
+  suffix[2]  = "mpmmmm";
+  suffix[3]  = "mmmmmm";
+  suffix[4]  = "mpmpep";
+  suffix[5]  = "mpmpem";
+  suffix[6]  = "mpmmep";
+  suffix[7]  = "mpmmem";
+  suffix[8]  = "mmmmep";
+  suffix[9]  = "mmmmem";
+  suffix[10] = "mpepep";
+  suffix[11] = "mpepem";
+  suffix[12] = "mpemem";
+  suffix[13] = "mmepep";
+  suffix[14] = "mmepem";
+  suffix[15] = "mmemem";
+  suffix[16] = "epepep";
+  suffix[17] = "epepem";
+  suffix[18] = "epemem";
+  suffix[19] = "ememem";
+  suffix[20] = "all";
+   if (makePictures) c->Print("out/stacks.ps[");
+   for (int i=0; i<myNames->GetEntries(); i++) {
+     //  cout << myNames->At(i)->GetName() << endl;
+     for (int sample=0; sample<allBuckets; sample++) {
+       hist::stack(Form("st_%s_%s",myNames->At(i)->GetName(),suffix[sample]),
+                    Form("%s_%s$",myNames->At(i)->GetName(), suffix[sample]));
+        THStack* thisStack = (THStack*) gROOT->FindObjectAny(
+                              Form("st_%s_%s", myNames->At(i)->GetName(), suffix[sample]));
+	TLegend* thisLeg = hist::legend(thisStack, "lpf", 0, 0);
+        c->cd(sample+1);
+        thisStack->Draw("hist");
+        thisLeg->Draw();
+        c->Update();
+     }
+     if (makePictures) {
+       c->Print("out/stacks.ps");
+       //       c->Print(Form("out/stacks_%d.png",i+1));
+       c->Print(Form("out/stacks_%s.png",myNames->At(i)->GetName()));
+     }
+     if (wait) {
+       cout << "Enter carriage return for the next set of plots....q to quit" << endl;
+       char in = getchar();
+       if (in == 'q') break;
+     }
+   }
+   if (makePictures) c->Print("out/stacks.ps]");
+}
