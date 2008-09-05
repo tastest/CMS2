92b5cd5ec6188e5357dcab572e09573078dcf4c6
olivito <olivito>
2008-09-05 15:49:11 +0000

    This commit was manufactured by cvs2svn to create tag 'fkw_muontag_v1'.

--- NtupleMacros/Tools/setupCMS1.C
+++
@@ -1,7 +0,0 @@
-// Loads a bunch of tools in interpretative mode
-{
-  gROOT->LoadMacro("../Tools/getMyHistosNamesCMS1.C");
-  gROOT->LoadMacro("../Tools/histtoolsCMS1.C");
-  gROOT->LoadMacro("../Tools/browseStacksCMS1.C");
-  
-  }
