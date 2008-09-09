41c1e8ca6166d21eb95943efc197bb70828c52c5
dmytro <dmytro>
2008-09-09 02:12:24 +0000

    looper to extract useful events. Currently just duplicate removal

---
+++ NtupleMacros/WW/dima/EventListCreator.C
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include "TChain.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TEventList.h"
+#include "EventListCreator.h"
+
+TEventList* EventListCreator::getAllUniqueEvents( TTree* tree, 
+				      const char* name /*= "AllUniqueEvents"*/, 
+				      const char* title /*= "All events without duplicates"*/ )
+{
+  Init(tree);
+  TEventList* list = new TEventList(name,title);
+  std::set<ULong64_t> known_events;
+  unsigned int nEvents = tree->GetEntries();
+  for( unsigned int event = 0; event < nEvents; ++event) {
+    GetEntry(event);
+    ULong64_t key = int((evt_met()-int(evt_met()))*10000)<<48 + evt_run()<<32+ evt_event();
+    if ( ! known_events.insert(key).second ) list->Enter( event );
+  }
+  return list;
+}
+
