da28ff25801f1325468f9fdce5a8813617d569fa
fkw <fkw>
2008-09-22 12:48:15 +0000

    changed name of input file to be consistent with README Dima wrote

--- NtupleMacros/WW/doTable.C
+++ NtupleMacros/WW/doTable.C
@@ -7,7 +7,7 @@
 gROOT->ProcessLine(".x ../Tools/setup.C");
 
 //hist file:
-TFile *ftt = TFile::Open("processed_data.root");
+TFile *ftt = TFile::Open("processed_data_tag.root");
 
  float DYee[4], DYmm[4], DYtt[4], tt[4], wjets[4], wz[4], zz[4], ww[4], h140[4], tw[4];
 
@@ -31,8 +31,8 @@ for ( int njet=1; njet<4; ++njet) {// actual njet is one less than this counter!
   DYtt[0] = dytt_hnJetLepVeto_em->GetBinContent(njet);
   tt[0] = ttbar_hnJetLepVeto_em->GetBinContent(njet);
   wjets[0] = wjets_hnJetLepVeto_em->GetBinContent(njet);
-  wz[0] = wz_hnJetLepVeto_em->GetBinContent(njet);
-  zz[0] = zz_hnJetLepVeto_em->GetBinContent(njet);
+  //  wz[0] = wz_hnJetLepVeto_em->GetBinContent(njet);
+  //  zz[0] = zz_hnJetLepVeto_em->GetBinContent(njet);
   ww[0] = ww_hnJetLepVeto_em->GetBinContent(njet);
   // h140[0] = H140_hnJetLepVeto_em->GetBinContent(njet);
   tw[0] = tw_hnJetLepVeto_em->GetBinContent(njet);
@@ -43,8 +43,8 @@ for ( int njet=1; njet<4; ++njet) {// actual njet is one less than this counter!
   DYtt[1] = dytt_hnJetLepVeto_mm->GetBinContent(njet);
   tt[1] = ttbar_hnJetLepVeto_mm->GetBinContent(njet);
   wjets[1] = wjets_hnJetLepVeto_mm->GetBinContent(njet);
-  wz[1] = wz_hnJetLepVeto_mm->GetBinContent(njet);
-  zz[1] = zz_hnJetLepVeto_mm->GetBinContent(njet);
+  //  wz[1] = wz_hnJetLepVeto_mm->GetBinContent(njet);
+  //  zz[1] = zz_hnJetLepVeto_mm->GetBinContent(njet);
   ww[1] = ww_hnJetLepVeto_mm->GetBinContent(njet);
   // h140[1] = H140_hnJetLepVeto_mm->GetBinContent(njet);
   tw[1] = tw_hnJetLepVeto_mm->GetBinContent(njet);
@@ -55,8 +55,8 @@ for ( int njet=1; njet<4; ++njet) {// actual njet is one less than this counter!
   DYtt[2] = dytt_hnJetLepVeto_ee->GetBinContent(njet);
   tt[2] = ttbar_hnJetLepVeto_ee->GetBinContent(njet);
   wjets[2] = wjets_hnJetLepVeto_ee->GetBinContent(njet);
-  wz[2] = wz_hnJetLepVeto_ee->GetBinContent(njet);
-  zz[2] = zz_hnJetLepVeto_ee->GetBinContent(njet);
+  //  wz[2] = wz_hnJetLepVeto_ee->GetBinContent(njet);
+  //  zz[2] = zz_hnJetLepVeto_ee->GetBinContent(njet);
   ww[2] = ww_hnJetLepVeto_ee->GetBinContent(njet);
   // h140[2] = H140_hnJetLepVeto_ee->GetBinContent(njet);
   tw[2] = tw_hnJetLepVeto_ee->GetBinContent(njet);
