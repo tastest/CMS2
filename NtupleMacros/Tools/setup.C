4b14cb4ac733194b8cd432103030b5c5a37207e1
olivito <olivito>
2008-10-03 21:35:49 +0000

    This commit was manufactured by cvs2svn to create tag 'V00_05_01'.

--- NtupleMacros/Tools/setup.C
+++
@@ -1,7 +0,0 @@
-// Loads a bunch of tools in interpretative mode
-{
-  gROOT->LoadMacro("../Tools/getMyHistosNames.C");
-  gROOT->LoadMacro("../Tools/histtools.C");
-  gROOT->LoadMacro("../Tools/browseStacks.C");
-  
-  }
