d9750fbba034b62ed4d3b61a57b98b1d1697bdfb
jmuelmen <jmuelmen>
2008-10-15 06:25:48 +0000

    histogram tools that don't depend on any cms2 stuff

---
+++ NtupleMacros/Tools/histtools.cc
@@ -0,0 +1,44 @@
+#include "TH1.h"
+#include "TGraph.h"
+
+typedef TH1F H;
+
+H cumulate (const H &in, bool increasing) 
+{
+     H h_out(in.GetName(), in.GetTitle(), in.GetNbinsX(), 
+	     in.GetBinLowEdge(1), in.GetBinLowEdge(in.GetNbinsX() + 1));
+     double sum = 0;
+     if (increasing) {
+	  for (int j = 0; j <= in.GetNbinsX() + 1; ++j) {
+	       sum += in.GetBinContent(j);
+	       h_out.SetBinContent(j, sum);
+	  }
+     } else {
+	  for (int j = in.GetNbinsX() + 1; j >= 0; --j) {
+	       sum += in.GetBinContent(j);
+	       h_out.SetBinContent(j, sum);
+	  }
+     }
+     return h_out;
+}
+
+TGraph eff_rej (const H &signal, H &background, bool normalize) 
+{
+     H sig = signal;
+     if (normalize)
+	  sig.Scale(1 / sig.Integral(1, sig.GetNbinsX()));
+     H bg = background;
+     if (normalize)
+	  bg.Scale(1 / bg.Integral(1, bg.GetNbinsX()));
+     H sig_cum = cumulate(sig);
+     H bg_cum = cumulate(bg);
+     TGraph ret(signal.GetNbinsX());
+     for (int i = 1; i <= signal.GetNbinsX(); ++i) {
+	  const double x = sig_cum.GetBinCenter(i);
+ 	  const double sig = sig_cum.GetBinContent(i);
+	  const double bg = bg_cum.GetBinContent(bg_cum.FindBin(x));
+	  ret.SetPoint(i - 1, sig, bg); // gotta love offsets
+//  	  printf("point %d: %f sig, %f bg\n", i, sig, bg);
+     }
+     return ret;
+}
