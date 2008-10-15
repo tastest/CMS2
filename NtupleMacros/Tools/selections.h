cd326f9249181d3e839412756a61207062dd2ca5
jmuelmen <jmuelmen>
2008-10-15 08:32:43 +0000

    added declarations

--- NtupleMacros/Tools/selections.h
+++ NtupleMacros/Tools/selections.h
@@ -7,13 +7,30 @@ bool goodMuonWithoutIsolation(int index);
 bool passElectronIsolation(int index);
 bool passMuonIsolation(int index);
 bool goodMuonIsolated(int index);
-bool goodElectronIsolated(int index);
+bool goodElectronIsolated(int index, bool use_calo_iso = false);
 bool pass2Met(int index);
 double nearestDeltaPhi(double Phi, int index);
 double MetSpecial(double MET, double MetPhi, int index);
 bool pass4Met(int index);
+bool passMuonBVeto (int i_dilep, bool soft_nonisolated);
+bool passTriLepVeto (int i_dilep);
+int tagMuonIdx (int i_dilep);
+double tagMuonPt (int i_dilep);
+double tagMuonRelIso (int i_dilep);
+bool additionalZveto();
 void correctMETmuons_crossedE(double& met, double& metPhi, 
 			      double muon_pt, double muon_phi,
 			      double muon_track_theta, double muon_track_phi,
 			      double mu_crossedem_dep, double mu_crossedhad_dep, double mu_crossedho_dep );
+bool isDYee();
+bool isDYmm();
+bool isDYtt();
+bool nTrkJets(int i_hyp);
+bool passTrkJetVeto(int i_hyp);
+
+double mu_rel_iso (int index);
+double el_rel_iso (int index, bool use_calo_iso);
+double reliso_lt (int i_hyp, bool use_calo_iso = false);
+double reliso_ll (int i_hyp, bool use_calo_iso = false);
+
 #endif
