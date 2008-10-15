b3974a0203effc4e1d0f5b2868b84c3c640a0e89
jmuelmen <jmuelmen>
2008-10-15 08:43:50 +0000

    for dictionary

---
+++ NtupleMacros/WW/linkdef.h
@@ -0,0 +1,11 @@
+#ifdef __CINT__
+#pragma link off all    globals;
+#pragma link off all    classes;
+#pragma link C++ all    functions;
+
+#pragma link off nestedclasses;
+#pragma link off nestedtypedefs;
+#pragma link C++ class WWResultsLooper;
+#pragma link C++ class WWDYInEMuLooper;
+
+#endif
