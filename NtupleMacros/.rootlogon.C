7c93272334b663a184b31d19f4809d86bab041a0
jmuelmen <jmuelmen>
2008-10-30 20:46:34 +0000

    load the right libraries

---
+++ NtupleMacros/.rootlogon.C
@@ -0,0 +1,8 @@
+//
+{
+	gSystem->Load("libPhysics.so");
+	gSystem->Load("libEG.so");
+	gSystem->Load("libMathCore.so");
+	gSystem->Load("libCMS2NtupleMacrosTools.so");
+	gSystem->Load("libCMS2NtupleMacrosWW.so");
+}
