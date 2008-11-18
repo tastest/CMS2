46a3e67ce7cb957fc301d83de152cd4142787ee6
dmytro <dmytro>
2008-11-18 16:24:14 +0000

    load CORE library

--- NtupleMacros/.rootlogon.C
+++ NtupleMacros/.rootlogon.C
@@ -4,6 +4,7 @@
 	gSystem->Load("libPhysics.so");
 	gSystem->Load("libEG.so");
 	gSystem->Load("libMathCore.so");
+	gSystem->Load("libCMS2NtupleMacrosCORE.so");
 	gSystem->Load("libCMS2NtupleMacrosTools.so");
 	gSystem->Load("libCMS2NtupleMacrosWW.so");
         gSystem->Load("libCMS2NtupleMacrosDSG.so");
