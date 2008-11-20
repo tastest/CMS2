4e3ac67c706dc512ceb3663e69f4f95d9250d92b
olivito <olivito>
2008-11-20 23:04:20 +0000

    This commit was manufactured by cvs2svn to create tag 'M00_01_00'.

--- NtupleMaker/python/trackMaker_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-trackMaker = cms.EDFilter("TrackMaker",
-    # track collection
-    tracksInputTag = cms.InputTag("generalTracks"),
-    beamSpotInputTag = cms.InputTag("beamSpotMaker")
-)
-
-
