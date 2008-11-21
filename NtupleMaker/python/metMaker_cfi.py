397fed2e199b2bb319738dd72788325deafbba22
olivito <olivito>
2008-11-21 15:09:13 +0000

    This commit was manufactured by cvs2svn to create tag 'M00_01_02'.

--- NtupleMaker/python/metMaker_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-metMaker = cms.EDFilter("METMaker",
-     metInputTag = cms.InputTag("met"),
-     # MC particles
-    genParticlesInputTag = cms.InputTag("genParticles")
-)
-
-
