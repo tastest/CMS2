f388ba48026d3d926ea77b2d5bd1c5242ef9e48b
dmytro <dmytro>
2009-02-05 15:09:44 +0000

    show errors in the table dump

--- NtupleMacros/WW/doTable.C
+++ NtupleMacros/WW/doTable.C
@@ -1,91 +1,135 @@
+#include "TFile.h"
+#include "TH1F.h"
+#include <iostream>
+void doTable()
 {
-// Load and compile something to allow proper treatment of vectors
-// Not clear that it is needed
-//gROOT->LoadMacro("loader.C+");
-
-// Load various tools
-gROOT->ProcessLine(".x ../Tools/setup.C");
-
-//hist file:
-TFile *ftt = TFile::Open("processed_data_tag.root");
-
- float DYee[4], DYmm[4], DYtt[4], tt[4], wjets[4], wz[4], zz[4], ww[4], h140[4], tw[4];
-
-for ( int njet=1; njet<4; ++njet) {// actual njet is one less than this counter!
-  for (int i=0; i<4; i++){
-    DYee[i] = 0.0;
-    DYmm[i] = 0.0;
-    DYtt[i] = 0.0;
-    tt[i] = 0.0;
-    wjets[i] = 0.0;
-    wz[i] = 0.0;
-    zz[i] = 0.0;
-    ww[i] = 0.0;
-    h140[i] = 0.0;
-    tw[i] = 0.0;
-  }
-
-  //do the emu row:
-  DYee[0] = dyee_hnJetLepVeto_em->GetBinContent(njet);
-  DYmm[0] = dymm_hnJetLepVeto_em->GetBinContent(njet);
-  DYtt[0] = dytt_hnJetLepVeto_em->GetBinContent(njet);
-  tt[0] = ttbar_hnJetLepVeto_em->GetBinContent(njet);
-  wjets[0] = wjets_hnJetLepVeto_em->GetBinContent(njet);
-  wz[0] = wz_hnJetLepVeto_em->GetBinContent(njet);
-  zz[0] = zz_hnJetLepVeto_em->GetBinContent(njet);
-  ww[0] = ww_hnJetLepVeto_em->GetBinContent(njet);
-  // h140[0] = H140_hnJetLepVeto_em->GetBinContent(njet);
-  tw[0] = tw_hnJetLepVeto_em->GetBinContent(njet);
+  using namespace std;
   
-  //do the mumu row:
-  DYee[1] = dyee_hnJetLepVeto_mm->GetBinContent(njet);
-  DYmm[1] = dymm_hnJetLepVeto_mm->GetBinContent(njet);
-  DYtt[1] = dytt_hnJetLepVeto_mm->GetBinContent(njet);
-  tt[1] = ttbar_hnJetLepVeto_mm->GetBinContent(njet);
-  wjets[1] = wjets_hnJetLepVeto_mm->GetBinContent(njet);
-  wz[1] = wz_hnJetLepVeto_mm->GetBinContent(njet);
-  zz[1] = zz_hnJetLepVeto_mm->GetBinContent(njet);
-  ww[1] = ww_hnJetLepVeto_mm->GetBinContent(njet);
-  // h140[1] = H140_hnJetLepVeto_mm->GetBinContent(njet);
-  tw[1] = tw_hnJetLepVeto_mm->GetBinContent(njet);
-  
-  //do the ee row:
-  DYee[2] = dyee_hnJetLepVeto_ee->GetBinContent(njet);
-  DYmm[2] = dymm_hnJetLepVeto_ee->GetBinContent(njet);
-  DYtt[2] = dytt_hnJetLepVeto_ee->GetBinContent(njet);
-  tt[2] = ttbar_hnJetLepVeto_ee->GetBinContent(njet);
-  wjets[2] = wjets_hnJetLepVeto_ee->GetBinContent(njet);
-  wz[2] = wz_hnJetLepVeto_ee->GetBinContent(njet);
-  zz[2] = zz_hnJetLepVeto_ee->GetBinContent(njet);
-  ww[2] = ww_hnJetLepVeto_ee->GetBinContent(njet);
-  // h140[2] = H140_hnJetLepVeto_ee->GetBinContent(njet);
-  tw[2] = tw_hnJetLepVeto_ee->GetBinContent(njet);
-  
-  char* finalState[4];
-  finalState[0] = "emu";
-  finalState[1] = "mumu";
-  finalState[2] = "ee";
-  finalState[3] = "total";
-  
-  cout << "  " << endl;
-  cout << " Njet = " << njet-1 << endl;
-  cout << "| | DY ee| DY mumu | DY tautau | ttbar | Wjets | WZ | ZZ | WW | TW | " << endl;
+  //hist file:
+  TFile *ftt = TFile::Open("processed_data_tag.root");
+  assert(ftt);
 
-  for (int i=0; i<4; i++){
+  TH1F *DYee[4], *DYmm[4], *DYtt[4], *tt[4], *wjets[4], *wz[4], *zz[4], *ww[4], *tw[4];
+
+  for ( int njet=1; njet<4; ++njet) {// actual njet is one less than this counter!
+
+    //do the emu row:
+    DYee[0]  = dynamic_cast<TH1F*>(ftt->Get("dyee_hnJet_em"));
+    DYmm[0]  = dynamic_cast<TH1F*>(ftt->Get("dymm_hnJet_em"));
+    DYtt[0]  = dynamic_cast<TH1F*>(ftt->Get("dytt_hnJet_em"));
+    tt[0]    = dynamic_cast<TH1F*>(ftt->Get("ttbar_hnJet_em"));
+    wjets[0] = dynamic_cast<TH1F*>(ftt->Get("wjets_hnJet_em"));
+    wz[0]    = dynamic_cast<TH1F*>(ftt->Get("wz_hnJet_em"));
+    zz[0]    = dynamic_cast<TH1F*>(ftt->Get("zz_hnJet_em"));
+    ww[0]    = dynamic_cast<TH1F*>(ftt->Get("ww_hnJet_em"));
+    tw[0]    = dynamic_cast<TH1F*>(ftt->Get("tw_hnJet_em"));
+  
+    //do the mumu row:
+    DYee[1]  = dynamic_cast<TH1F*>(ftt->Get("dyee_hnJet_mm"));
+    DYmm[1]  = dynamic_cast<TH1F*>(ftt->Get("dymm_hnJet_mm"));
+    DYtt[1]  = dynamic_cast<TH1F*>(ftt->Get("dytt_hnJet_mm"));
+    tt[1]    = dynamic_cast<TH1F*>(ftt->Get("ttbar_hnJet_mm"));
+    wjets[1] = dynamic_cast<TH1F*>(ftt->Get("wjets_hnJet_mm"));
+    wz[1]    = dynamic_cast<TH1F*>(ftt->Get("wz_hnJet_mm"));
+    zz[1]    = dynamic_cast<TH1F*>(ftt->Get("zz_hnJet_mm"));
+    ww[1]    = dynamic_cast<TH1F*>(ftt->Get("ww_hnJet_mm"));
+    tw[1]    = dynamic_cast<TH1F*>(ftt->Get("tw_hnJet_mm"));
+  
+    //do the ee row:
+    DYee[2]  = dynamic_cast<TH1F*>(ftt->Get("dyee_hnJet_ee"));
+    DYmm[2]  = dynamic_cast<TH1F*>(ftt->Get("dymm_hnJet_ee"));
+    DYtt[2]  = dynamic_cast<TH1F*>(ftt->Get("dytt_hnJet_ee"));
+    tt[2]    = dynamic_cast<TH1F*>(ftt->Get("ttbar_hnJet_ee"));
+    wjets[2] = dynamic_cast<TH1F*>(ftt->Get("wjets_hnJet_ee"));
+    wz[2]    = dynamic_cast<TH1F*>(ftt->Get("wz_hnJet_ee"));
+    zz[2]    = dynamic_cast<TH1F*>(ftt->Get("zz_hnJet_ee"));
+    ww[2]    = dynamic_cast<TH1F*>(ftt->Get("ww_hnJet_ee"));
+    tw[2]    = dynamic_cast<TH1F*>(ftt->Get("tw_hnJet_ee"));
+  
+    //do the total row:
+    if (DYee[0]) 
+      DYee[3] = new TH1F(*(DYee[0]));
+    else
+      DYee[3] = 0;
+    if (DYmm[0])
+      DYmm[3] = new TH1F(*(DYmm[0]));
+    else
+      DYmm[3] = 0;
+    if (DYtt[0])
+      DYtt[3] = new TH1F(*(DYtt[0]));
+    else
+      DYtt[3] = 0;
+    if (tt[0]) 
+      tt[3] = new TH1F(*(tt[0]));
+    else
+      tt[3] = 0;
+    if (wjets[0])
+      wjets[3] = new TH1F(*(wjets[0]));
+    else
+      wjets[3] = 0;
+    if (wz[0])
+      wz[3] = new TH1F(*(wz[0]));
+    else 
+      wz[0] = 0;
+    if (zz[0])
+      zz[3] = new TH1F(*(zz[0]));
+    else
+      zz[3] = 0;
+    if (ww[0])
+      ww[3] = new TH1F(*(ww[0]));
+    else
+      ww[3] = 0;
+    if (tw[0])
+      tw[3] = new TH1F(*(tw[0]));
+    else
+      tw[3] = 0;
+  
+    char* finalState[4];
+    finalState[0] = "emu";
+    finalState[1] = "mumu";
+    finalState[2] = "ee";
+    finalState[3] = "total";
+  
+    cout << "  " << endl;
+    cout << " Njet = " << njet-1 << endl;
+    cout << "| |  *DY ee*  |  *DY mumu*  |  *DY tautau*  |  *ttbar*  |  *Wjets*  |  *WZ*  |  *ZZ*  |  *WW*  |  *TW*  | " << endl;
     
-    cout << "| " << finalState[i] << " | " << DYee[i] << " | " << DYmm[i] << " | " << DYtt[i] << " | " << tt[i] << " | " << wjets[i] << " | " << wz[i] << " | " << zz[i] << " | " << ww[i] << " | " << tw[i] << " | " << endl;
-
-    DYee[3] = DYee[3]+DYee[i];
-    DYmm[3] = DYmm[3]+DYmm[i];
-    DYtt[3] = DYtt[3]+DYtt[i];
-    tt[3] = tt[3]+tt[i];
-    wjets[3] = wjets[3]+wjets[i];
-    wz[3] = wz[3]+wz[i];
-    zz[3] = zz[3]+zz[i];
-    ww[3] = ww[3]+ww[i];
-    //   h140[3] = h140[3]+h140[i];
-    tw[3] = tw[3]+tw[i];
+    for (int i=0; i<4; i++){
+      
+      cout << "| " << finalState[i] << " |  ";
+      cout.setf(ios::fixed,ios::floatfield);
+      cout.precision(1);
+      if (DYee[i]) cout << DYee[i]->GetBinContent(njet) << " &plusmn; " << DYee[i]->GetBinError(njet);
+      cout << "  |  ";
+      if (DYmm[i]) cout << DYmm[i]->GetBinContent(njet) << " &plusmn; " << DYmm[i]->GetBinError(njet);
+      cout << "  |  ";
+      if (DYtt[i]) cout << DYtt[i]->GetBinContent(njet) << " &plusmn; " << DYtt[i]->GetBinError(njet);
+      cout << "  |  ";
+      if (tt[i]) cout << tt[i]->GetBinContent(njet) << " &plusmn; " << tt[i]->GetBinError(njet);
+      cout << "  |  ";
+      if (wjets[i]) cout << wjets[i]->GetBinContent(njet) << " &plusmn; " << wjets[i]->GetBinError(njet);
+      cout << "  |  ";
+      if (wz[i]) cout << wz[i]->GetBinContent(njet) << " &plusmn; " << wz[i]->GetBinError(njet);
+      cout << "  |  ";
+      if (zz[i]) cout << zz[i]->GetBinContent(njet) << " &plusmn; " << zz[i]->GetBinError(njet);
+      cout << "  |  ";
+      if (ww[i]) cout << ww[i]->GetBinContent(njet) << " &plusmn; " << ww[i]->GetBinError(njet);
+      cout << "  |  ";
+      if (tw[i]) cout << tw[i]->GetBinContent(njet) << " &plusmn; " << tw[i]->GetBinError(njet);
+      cout << "  |  " <<endl;
+      
+      if ( i>0 && i<3 ) {
+	if (DYee[3] && DYee[i]) DYee[3]->Add(DYee[i]);
+	if (DYmm[3] && DYmm[i]) DYmm[3]->Add(DYmm[i]);
+	if (DYtt[3] && DYtt[i]) DYtt[3]->Add(DYtt[i]);
+	if (tt[3]   && tt[i])   tt[3]->Add(tt[i]);
+	if (wjets[3]&&wjets[i]) wjets[3]->Add(wjets[i]);
+	if (wz[3]   && wz[i])   wz[3]->Add(wz[i]);
+	if (zz[3]   && zz[i])   zz[3]->Add(zz[i]);
+	if (ww[3]   && ww[i])   ww[3]->Add(ww[i]);
+	if (tw[3]   && tw[i])   tw[3]->Add(tw[i]);
     
+      }
+    }//end of for loop over njet
   }
-}//end of for loop over njet
 }
