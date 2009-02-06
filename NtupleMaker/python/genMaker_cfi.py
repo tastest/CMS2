552e1c628014f8a0ab389e342e8bc63682834c3b
olivito <olivito>
2009-02-06 21:48:23 +0000

    This commit was manufactured by cvs2svn to create tag 'elfakes_2_2_pass1'.

--- NtupleMaker/python/genMaker_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-genMaker = cms.EDFilter("GenMaker",
-    ntupleOnlyStatus3 = cms.bool(True),
-    genParticlesInputTag = cms.InputTag("genParticles")
-)
-
-
