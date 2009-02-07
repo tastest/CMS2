f57946b645cd40404a91c381d74e12fa73381b64
dmytro <dmytro>
2009-02-07 01:23:20 +0000

    default setup to get things working

---
+++ NtupleMacros/WW/rootlogon.C
@@ -0,0 +1,10 @@
+{
+   gROOT->SetStyle("Plain");
+   gStyle->SetPalette(1);
+   gStyle->SetOptStat("nemruoi");
+   cout << "loading..." <<endl;
+   gSystem->Load("libCintex");
+   Cintex::Enable();
+   gSystem->Load("libFWCoreFWLite");
+   AutoLibraryLoader::enable();
+}
