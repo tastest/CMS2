f24d193fa1ba1033207ea42ba8b6490b0e2954cf
olivito <olivito>
2009-02-12 16:25:12 +0000

    This commit was manufactured by cvs2svn to create tag 'sk_tdilgp021209'.

--- NtupleMaker/python/patElectronMaker_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-patElectronMaker = cms.EDFilter("PATElectronMaker",
-    # pat jet collection
-    patElectronsInputTag = cms.InputTag("selectedLayer1Electrons")
-)
-
-
