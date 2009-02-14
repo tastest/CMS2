eaa1ff4189efe9bd0b35a15103d63203a7ffbb90
slava77 <slava77>
2009-02-14 03:23:01 +0000

    moving some code from the looper to selections: add leptonXXXyyyXX methods with (id,index) as argument; moved event-qualifying dilepton picker to selections; explicit use of PAT quantities for TTDil selections

--- NtupleMacros/TTDil/slava/selections.C
+++ NtupleMacros/TTDil/slava/selections.C
@@ -53,6 +53,16 @@ double electronCalIsolation(int index){
   double pt  = els_p4().at(index).pt();
   return pt/(pt+sum);
 }
+double electronTrkIsolationPAT(int index){
+  double sum = els_pat_trackIso().at(index);
+  double pt  = els_p4().at(index).pt();
+  return pt/(pt+sum);
+}
+double electronCalIsolationPAT(int index){
+  double sum = els_pat_caloIso().at(index);
+  double pt  = els_p4().at(index).pt();
+  return pt/(pt+sum);
+}
 
 bool passElectronIsolation(int index) {
   return ( electronIsolation(index) >= 0.92 );
@@ -80,6 +90,16 @@ double muonTrkIsolation(int index){
 double muonCalIsolation(int index){
   double sum =  mus_iso03_emEt().at(index)  +
     mus_iso03_hadEt().at(index);
+  double pt  = mus_p4().at(index).pt();
+  return  pt/(pt+sum);
+}
+double muonTrkIsolationPAT(int index){
+  double sum =  mus_pat_trackIso().at(index);
+  double pt  = mus_p4().at(index).pt();
+  return  pt/(pt+sum);
+}
+double muonCalIsolationPAT(int index){
+  double sum =  mus_pat_caloIso().at(index);
   double pt  = mus_p4().at(index).pt();
   return  pt/(pt+sum);
 }
@@ -381,19 +401,26 @@ bool looseElectronSelectionNoIsoTTDil08(int index) {
   return true;
 }
 
+// factorize this stuff out
+float electronTrkIsolationTTDil08(int index){
+  return electronTrkIsolationPAT(index);
+}
+float electronCalIsolationTTDil08(int index){
+  return electronCalIsolationPAT(index);
+}
 
 bool looseElectronSelectionTTDil08(int index) {
   if ( ! looseElectronSelectionNoIsoTTDil08(index) ) return false;
 
-  if ( electronTrkIsolation(index) < 0.5 ) return false;
-  if ( electronCalIsolation(index) < 0.5 ) return false;
+  if ( electronTrkIsolationTTDil08(index) < 0.5 ) return false;
+  if ( electronCalIsolationTTDil08(index) < 0.5 ) return false;
 
   return true;
 }
 
 bool passElectronIsolationTTDil08(int index){
-  if ( electronTrkIsolation(index) < 0.9 ) return false;
-  if ( electronCalIsolation(index) < 0.8 ) return false;
+  if ( electronTrkIsolationTTDil08(index) < 0.9 ) return false;
+  if ( electronCalIsolationTTDil08(index) < 0.8 ) return false;
 
   return true;
 }
@@ -417,20 +444,63 @@ bool looseMuonSelectionNoIsoTTDil08(int index) {
   return true;
 }
 
+bool lepton20Eta2p4(int id, int index){
+  if (abs(id)==11) return electron20Eta2p4(index);
+  if (abs(id)==13) return muon20Eta2p4(index);
+  return false;
+}
+
+// factorize this stuff out
+float muonTrkIsolationTTDil08(int index){
+  return muonTrkIsolationPAT(index);
+}
+float muonCalIsolationTTDil08(int index){
+  return muonCalIsolationPAT(index);
+}
+
+float leptonTrkIsolationTTDil08(int id, int index){
+  if (abs(id) == 11) return electronTrkIsolationTTDil08(index);
+  if (abs(id) == 13) return muonTrkIsolationTTDil08(index);
+  return -1;
+}
+float leptonCalIsolationTTDil08(int id, int index){
+  if (abs(id) == 11) return electronCalIsolationTTDil08(index);
+  if (abs(id) == 13) return muonCalIsolationTTDil08(index);
+  return -1;
+}
+
 bool looseMuonSelectionTTDil08(int index) {
   if (! looseMuonSelectionNoIsoTTDil08(index) ) return false;
   
-  if ( muonTrkIsolation(index) < 0.5 ) return false;
-  if ( muonCalIsolation(index) < 0.5 ) return false;
+  if ( muonTrkIsolationTTDil08(index) < 0.5 ) return false;
+  if ( muonCalIsolationTTDil08(index) < 0.5 ) return false;
 
   return true;
 }
 
 bool passMuonIsolationTTDil08(int index) {
-  if ( muonTrkIsolation(index) < 0.9 ) return false;
-  if ( muonCalIsolation(index) < 0.9 ) return false;
+  if ( muonTrkIsolationTTDil08(index) < 0.9 ) return false;
+  if ( muonCalIsolationTTDil08(index) < 0.9 ) return false;
 
   return true;
+}
+
+//now make it figure out which lepton type it is
+bool passLeptonIsolationTTDil08(int id, int index){
+  if (abs(id) == 11) return passElectronIsolationTTDil08(index);
+  if (abs(id) == 13) return passMuonIsolationTTDil08(index);
+  return false;
+}
+
+bool looseLeptonSelectionNoIsoTTDil08(int id, int index){
+  if (abs(id) == 11) return looseElectronSelectionNoIsoTTDil08(index);  
+  if (abs(id) == 13) return looseMuonSelectionNoIsoTTDil08(index);
+  return false;
+}
+bool looseLeptonSelectionTTDil08(int id, int index){
+  if (abs(id) == 11) return looseElectronSelectionTTDil08(index);  
+  if (abs(id) == 13) return looseMuonSelectionTTDil08(index);
+  return false;
 }
 
 //------------------------------------------------------------------------------------
@@ -514,4 +584,183 @@ bool additionalZveto(bool useTTDil08 = false) {
   return veto;
 }
 
+
+// Trigger-related selections
+
+//Bits passing for hyp type
+bool passTriggersMu9orLisoE15(int dilType) {
+  bool hltLooseIsoEle15_LW_L1R = ((evt_HLT2() & (1<<(47-32))) != 0);
+  bool hltMu9 = ((evt_HLT3() & (1<<(82-64))) != 0);
+  
+  if (dilType == 0 && ! (hltMu9) ) return false;
+  if ((dilType == 1 || dilType == 2) && ! (hltMu9 || hltLooseIsoEle15_LW_L1R)) return false;
+  if (dilType == 3 && ! hltLooseIsoEle15_LW_L1R) return false;     
+
+  return true;
+}
+
+
+bool passTriggersTTDil08JanTrial(int dilType) {
+  bool hltIsoEle18_L1R = ((evt_HLT2() & (1<<(45-32))) != 0);
+  bool hltDoubleIsoEle12_L1R = ((evt_HLT2() & (1<<(54-32))) != 0); 
+  bool hltMu15_L1Mu7 = ((evt_HLT3() & (1<<(86-64))) != 0); 
+  bool hltDoubleMu3 = ((evt_HLT3() & (1<<(90-64))) != 0);
+  bool hltIsoEle10_Mu10_L1R = ((evt_HLT4() & (1<<(126-96))) != 0);
+  
+  if (dilType == 0 && ! (hltMu15_L1Mu7 || hltDoubleMu3) ) return false;
+  if ((dilType == 1 || dilType == 2) 
+      && ! (hltIsoEle18_L1R || hltMu15_L1Mu7 || hltIsoEle10_Mu10_L1R)) return false;
+  if (dilType == 3 && ! (hltIsoEle18_L1R || hltDoubleIsoEle12_L1R) ) return false; 
+  return true;
+}
+
+
+int eventDilIndexByWeightTTDil08(const std::vector<unsigned int>& goodHyps, int& strasbourgDilType, bool printDebug = false){
+  int result = -1;
+  unsigned int nGoodHyps = goodHyps.size();
+  if ( nGoodHyps == 0 ) return result;
+
+  float maxWeight = -1;
+  unsigned int maxWeightIndex = 9999;
+  
+  for (unsigned int hypIdxL=0; hypIdxL < nGoodHyps; ++hypIdxL){
+    unsigned int hypIdx = goodHyps[hypIdxL];
+    float hypWeight_lt = 0;
+    float hypWeight_ll = 0;
+    float hypWeight_iso = 0;
+    float hypWeight = 0;
+    unsigned int i_lt = hyp_lt_index().at(hypIdx);
+    unsigned int i_ll = hyp_ll_index().at(hypIdx);
+
+    int id_lt = hyp_lt_id().at(hypIdx);
+    int id_ll = hyp_ll_id().at(hypIdx);
+
+    float isoTk_lt = leptonTrkIsolationTTDil08(id_lt, i_lt);
+    float isoTk_ll = leptonTrkIsolationTTDil08(id_ll, i_ll);
+
+    float isoCal_lt = leptonCalIsolationTTDil08(id_lt, i_lt);
+    float isoCal_ll = leptonCalIsolationTTDil08(id_ll, i_ll);
+
+    //ad-hoc selection of weights
+    if (abs(id_lt) == 11){
+      //I want to select "trk & cal"-isolated ones
+      hypWeight_iso += (isoTk_lt*isoCal_lt - 0.25); //shift by 0.25 to be positive-definite
+      if (els_tightId().at(i_lt)) hypWeight_lt += 0.2;
+    }
+    if (abs(id_lt) == 13){
+      //I want to select "trk & cal"-isolated ones	    
+      hypWeight_iso += (isoTk_lt*isoCal_lt - 0.25);//shift by 0.25 to be positive-definite
+      hypWeight_lt += 0.4;
+    }
+    if (abs(id_ll) == 11){
+      //I want to select "trk & cal"-isolated ones
+      hypWeight_iso *= (isoTk_ll*isoCal_ll - 0.25); //shift by 0.25 to be positive-definite
+      if (els_tightId().at(i_ll)) hypWeight_ll += 0.2;
+    }
+    if (abs(id_ll) == 13){
+      //I want to select "trk & cal"-isolated ones
+      hypWeight_iso *= (isoTk_ll*isoCal_ll - 0.25); //shift by 0.25 to be positive-definite
+      hypWeight_ll += 0.4;
+    }
+    float pt_lt = hyp_lt_p4().at(hypIdx).pt();
+    float pt_ll = hyp_ll_p4().at(hypIdx).pt();
+    hypWeight_lt += (1. - 20./pt_lt*20./pt_lt);
+    hypWeight_ll += (1. - 20./pt_ll*20./pt_ll);
+
+    hypWeight = hypWeight_ll*hypWeight_lt*hypWeight_iso; //again, desire to have both good
+
+    if (hypWeight > maxWeight){
+      maxWeight = hypWeight;
+      maxWeightIndex = hypIdx;
+    }
+  }
+
+
+  //Now let's implement the Strasbourg-type disambiguation/dispatch
+  //ee
+  {
+    std::vector<unsigned int> looseEls(0);
+    std::vector<unsigned int> looseMus(0);
+    for (unsigned int iEl =0; iEl < els_p4().size(); ++iEl){
+      if (looseElectronSelectionTTDil08(iEl)){
+	looseEls.push_back(iEl);
+      }
+    }
+    for (unsigned int iMu =0; iMu < mus_p4().size(); ++iMu){
+      if (looseMuonSelectionTTDil08(iMu)){
+	looseMus.push_back(iMu);
+      }
+    }
+	    
+    bool pass_elec = false;
+    if (looseEls.size()>1){
+      if (els_charge().at(looseEls[0]) != els_charge().at(looseEls[1])){
+	pass_elec = true;
+      }
+      if (looseMus.size()>0 && mus_p4().at(looseMus[0]).pt() > els_p4().at(looseEls[1]).pt()) pass_elec = false;
+      if (looseMus.size()>0 && 
+	  ( ( muonTrkIsolation(looseMus[0]) > electronTrkIsolation(looseEls[0]) 
+	      && mus_charge().at(looseMus[0]) != els_charge().at(looseEls[0]) )
+	    || ( muonTrkIsolation(looseMus[0]) > electronTrkIsolation(looseEls[1])
+		 && mus_charge().at(looseMus[0]) != els_charge().at(looseEls[0]))
+	    )
+	  ) pass_elec = false; 
+    }
+    bool pass_muon = false;
+    if (looseMus.size()>1){
+      for (unsigned int iMu=0; iMu < looseMus.size(); ++iMu){
+	for (unsigned int jMu=iMu+1; jMu < looseMus.size(); ++jMu){
+	  if (mus_charge().at(looseMus[iMu]) != mus_charge().at(looseMus[jMu])) pass_muon = true;
+	}
+      }
+      if (looseEls.size()>0 && els_p4().at(looseEls[0]).pt() > mus_p4().at(looseMus[1]).pt()
+	  && mus_charge().at(looseMus[1]) != els_charge().at(looseEls[0])) pass_muon = false;
+    }
+    bool pass_elecmuon = false;
+    if (looseMus.size() > 0 && looseEls.size() > 0){
+      if (! pass_elec && ! pass_muon ){
+	if (mus_charge().at(looseMus[0]) != els_charge().at(looseEls[0])) pass_elecmuon = true;
+	if (! pass_elecmuon && looseEls.size()>1){
+	  if (mus_charge().at(looseMus[0]) != els_charge().at(looseEls[0])) pass_elecmuon = true;
+	}
+      }
+    }
+
+    unsigned int passStatus = 0;
+    if (pass_muon) passStatus++;
+    if (pass_elecmuon) passStatus++;
+    if (pass_elec) passStatus++;
+    if (passStatus > 1) std::cout<<"ERROR: inconsistent assignment"<<std::endl;
+    if (passStatus == 1){
+      if (pass_muon) strasbourgDilType = 0;
+      if (pass_elecmuon) strasbourgDilType = 1;
+      if (pass_elec) strasbourgDilType = 2;
+    }
+  }
+
+  if (printDebug){
+    int genpDilType = genpDileptonType();
+    if (genpDilType>=0 ){ std::cout<<"Dil type "<<genpDilType<<std::endl;
+      if (nGoodHyps > 1){
+	int maxWeightType = hyp_type().at(maxWeightIndex);
+	if ((maxWeightType == 0 && genpDilType == 0)
+	    || ( (maxWeightType == 1 || maxWeightType == 2) && genpDilType == 1)
+	    || (maxWeightType == 3 && genpDilType == 2)){
+	  std::cout<<"Dil type "<<genpDilType<<" ; Strasbourg dil type "<<strasbourgDilType 
+		   <<" assigned correctly by maxWeight method";
+	  std::cout<<" out of"; for(unsigned int iih=0;iih<nGoodHyps;++iih)std::cout<<" "<<hyp_type().at(goodHyps[iih]);
+	  std::cout<<std::endl;
+	} else {
+	  std::cout<<"Dil type "<<genpDilType<<" ; Strasbourg dil type "<<strasbourgDilType 
+		   <<" assigned incorrectly by maxWeight method";
+	  std::cout<<" out of"; for(unsigned int iih=0;iih<nGoodHyps;++iih)std::cout<<" "<<hyp_type().at(goodHyps[iih]);
+	  std::cout<<std::endl;	    
+	}
+      }
+    }
+  }
+
+  result = maxWeightIndex;
+  return result;
+}
 
