eef8fa783977301d8c878bbf991ccda122843fe8
olivito <olivito>
2009-02-19 22:48:49 +0000

    This commit was manufactured by cvs2svn to create tag 'DYEst_22X_v1'.

--- NtupleMacros/TTDil/slava/makeAllPlots.C
+++
@@ -1,9 +0,0 @@
-void makeAllPlots(char* fname, bool logScale=false){
-  gROOT->SetStyle("Plain");
-  gROOT->LoadMacro("loader.C+");
-  gROOT->ProcessLine(".x setup.C(true)"); //don't need FWLite to make plots
-  hist::loadHist(fname);
-  
-  browseStacks(true, false, true, 1.1, logScale, logScale ? false : true);
-  gSystem->Exit(0);
-}
