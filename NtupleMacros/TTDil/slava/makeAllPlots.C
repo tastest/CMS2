b559c8c6c2b524a8bd25b7fd2e73925208d11cf0
olivito <olivito>
2009-03-13 01:37:37 +0000

    This commit was manufactured by cvs2svn to create tag 'V01-02-08'.

--- NtupleMacros/TTDil/slava/makeAllPlots.C
+++
@@ -1,8 +0,0 @@
-void makeAllPlots(char* fname, bool logScale=false){
-  gROOT->SetStyle("Plain");
-  gROOT->ProcessLine(".x setup.C(true)"); //don't need FWLite to make plots
-  hist::loadHist(fname);
-  
-  browseStacks(true, false, true, 1.1, logScale, logScale ? false : true);
-  gSystem->Exit(0);
-}
