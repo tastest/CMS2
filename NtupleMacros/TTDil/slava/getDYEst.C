2c1bb21e22e7f9116fd0f33a1ce1341f7e515304
kalavase <kalavase>
2009-04-22 23:12:49 +0000

    Fixed some errors, moved to using nJet bins to calculate #s instead of dilMass hists

--- NtupleMacros/TTDil/slava/getDYEst.C
+++ NtupleMacros/TTDil/slava/getDYEst.C
@@ -13,7 +13,6 @@
 using namespace std;
 
 
-//void printR(TString fname, int nJets, TString hyp_type);
 Double_t getError2(const TH1F *hist, const Int_t binMin, const Int_t binMax)
 {
 
@@ -27,30 +26,42 @@ Double_t getError2(const TH1F *hist, const Int_t binMin, const Int_t binMax)
 }
 
 
+
+  
+
+
 pair<Double_t, Double_t> getR(TString fname, Int_t nJets, TString hyp_type) {
 
   TFile *f = TFile::Open(fname.Data(), "READ");
+  TString histoname_in(Form("DY%s_hnJetinZwindow_%s", hyp_type.Data(), hyp_type.Data()));
+  TString histoname_out(Form("DY%s_hnJetoutZwindow_%s", hyp_type.Data(), hyp_type.Data()));
+  TH1F *h_nJets_in = (TH1F*)f->Get(histoname_in.Data());
+  TH1F *h_nJets_out = (TH1F*)f->Get(histoname_out.Data());
   
-  //Get the DY dilMass histo for this jet bin and type
-  TString histoname(Form("DY%s_hdilMass_%ij_%s", hyp_type.Data(), nJets,hyp_type.Data()));
-  TH1F *h_mll = (TH1F*)f->Get(histoname.Data());
-  if (h_mll==NULL) {
-    cout << "Can't find " << histoname << ". Returning" << endl;
-    return make_pair(0.,0.);
+  
+  Double_t nEvts_in  = 0;
+  Double_t nEvts_out = 0;
+  Double_t nEvts_inErr  = 0;
+  Double_t nEvts_outErr = 0;
+
+  if(nJets <= 1) {
+    nEvts_in  = h_nJets_in->GetBinContent(nJets + 1);
+    nEvts_out = h_nJets_out->GetBinContent(nJets + 1);
+    nEvts_inErr  = sqrt(getError2(h_nJets_in, nJets + 1, nJets + 1));
+    nEvts_outErr = sqrt(getError2(h_nJets_out, nJets + 1, nJets + 1));
+  } else {
+    Int_t binHigh = h_nJets_in->GetNbinsX() + 1;
+    for(Int_t i = 3; i <= binHigh; i++) {
+      nEvts_in = nEvts_in + h_nJets_in->GetBinContent(i);
+      nEvts_out = nEvts_out + h_nJets_out->GetBinContent(i);
+    }
+    nEvts_inErr = sqrt(getError2(h_nJets_in, 3, binHigh));
+    nEvts_outErr = sqrt(getError2(h_nJets_out, 3, binHigh));
   }
-  Int_t binLow = h_mll->FindBin(76.01);
-  Int_t binHigh = h_mll->FindBin(105.09);
-  Double_t nEvts_in = h_mll->Integral(binLow, binHigh);
-  Double_t nEvts_out = h_mll->Integral(0, h_mll->GetNbinsX()+1) - nEvts_in;
-  Double_t nEvts_inErr = sqrt(getError2(h_mll, binLow, binHigh));
-  Double_t nEvts_outErr = sqrt(getError2(h_mll, 0, binLow) + getError2(h_mll, binHigh+1, h_mll->GetNbinsX()+1));
-  float R = nEvts_out/(float)nEvts_in;
-  //Error on R is R*R*(pow(nEvts_outErr/nEvts_out,2)+ pow(nEvts_inErr/nEvts_in,2))
-  float R_Err2 = pow(nEvts_outErr/nEvts_in, 2) + pow(nEvts_out*nEvts_inErr,2)/pow(nEvts_in,4);
   
+  Double_t R = nEvts_out/nEvts_in;
+  Double_t R_Err2 = pow(nEvts_outErr/nEvts_in, 2) + pow(nEvts_out*nEvts_inErr,2)/pow(nEvts_in,4);
   
-  cout << "R +/- err: " << R << "+/-" << sqrt(R_Err2) 
-  << " (" << nEvts_in << "," << nEvts_out << ")" << endl;
   f->Close();
   return make_pair(R, R_Err2);
 
@@ -59,42 +70,46 @@ pair<Double_t, Double_t> getR(TString fname, Int_t nJets, TString hyp_type) {
 pair<Double_t, Double_t>  getkFromDY(TString fname, int nJets, TString hyp_type) {
   
   //getk in DY only (emu of course)
-  TFile *f = TFile::Open(fname.Data(), "READ");
-  TString histoname(Form("DYee_hdilMass_%ij_ee", nJets));
-  TH1F *h_ee = (TH1F*)f->Get(histoname.Data());
-  h_ee->TH1F::Sumw2();
-  histoname = Form("DYmm_hdilMass_%ij_mm", nJets);
-  TH1F *h_mm = (TH1F*)f->Get(histoname.Data());
-  h_mm->TH1F::Sumw2();
-  
-  Int_t binLow = h_ee->FindBin(76.01);
-  Int_t binHigh = h_ee->FindBin(105.09);
-  Double_t nEvts_in_ee = h_ee->Integral(binLow, binHigh);
-  Double_t nEvts_in_eeErr2 = getError2(h_ee, binLow, binHigh);
-
-  binLow = h_mm->FindBin(76.01);
-  binHigh = h_mm->FindBin(105.09);
-  Double_t nEvts_in_mm = h_mm->Integral(binLow, binHigh);
-  Double_t nEvts_in_mmErr2 = getError2(h_mm, binLow, binHigh);
-  
-  Double_t k, kErr2;
-  
-  if(hyp_type == "mm") {
-    k = 0.5*sqrt(nEvts_in_mm/nEvts_in_ee);
-    kErr2 = (1./16.)*(nEvts_in_mmErr2/nEvts_in_ee/nEvts_in_mm + nEvts_in_eeErr2*nEvts_in_mm/pow(nEvts_in_ee,3));
-  } else if(hyp_type == "ee" ) {
+   TFile *f = TFile::Open(fname.Data(), "READ");
+   TString histoname("DYee_hnJetinZwindow_ee");
+   TH1F *h_ee = (TH1F*)f->Get(histoname.Data());
+   h_ee->TH1F::Sumw2();
+   histoname = "DYmm_hnJetinZwindow_mm";
+   TH1F *h_mm = (TH1F*)f->Get(histoname.Data());
+   h_mm->TH1F::Sumw2();
+   
+   Double_t nEvts_in_ee     = 0;
+   Double_t nEvts_inErr2_ee  = 0;
+   Double_t nEvts_in_mm     = 0;
+   Double_t nEvts_inErr2_mm  = 0;
+   
+   if(nJets <= 1) {
+     nEvts_in_ee  = h_ee->GetBinContent(nJets+1);
+     nEvts_in_mm  = h_mm->GetBinContent(nJets+1);
+     nEvts_inErr2_ee = getError2(h_ee, nJets+1, nJets+1);
+     nEvts_inErr2_mm = getError2(h_mm, nJets+1, nJets+1);
+   } else {
+     Int_t binHigh = h_ee->GetNbinsX() + 1;
+     for(Int_t i = 3; i <= binHigh; i++) {
+       nEvts_in_ee = nEvts_in_ee + h_ee->GetBinContent(i);
+       nEvts_in_mm = nEvts_in_mm + h_ee->GetBinContent(i);
+     }
+     nEvts_inErr2_ee = getError2(h_ee, 3, binHigh);
+     nEvts_inErr2_mm = getError2(h_mm, 3, binHigh);
+   }
+			   
+   Double_t k     = 0.;
+   Double_t kErr2 = 0.;
+   if(hyp_type == "mm") {
+      k = 0.5*sqrt(nEvts_in_mm/nEvts_in_ee);
+      kErr2 = (1./16.)*(nEvts_inErr2_mm/nEvts_in_ee/nEvts_in_mm + nEvts_inErr2_ee*nEvts_in_mm/pow(nEvts_in_ee,3));
+   } else if(hyp_type == "ee" ) {
     k = 0.5*sqrt(nEvts_in_ee/nEvts_in_mm);
-    kErr2 = (1./16.)*(nEvts_in_eeErr2/nEvts_in_mm/nEvts_in_ee + nEvts_in_mmErr2*nEvts_in_ee/pow(nEvts_in_mm,3));
-  }
-  //cout << "k " << k << " +/- " << kErr << endl;
+    kErr2 = (1./16.)*(nEvts_inErr2_ee/nEvts_in_mm/nEvts_in_ee + nEvts_inErr2_mm*nEvts_in_ee/pow(nEvts_in_mm,3));
+   }
 
-  //Double_t eff_movere = nEvts_in_mm/nEvts_in_ee;
-  //Double_t eff_movere_Err = sqrt(nEvts_in_mmErr2/pow(nEvts_in_ee,2) + pow(nEvts_in_mm,2)*nEvts_in_eeErr2/pow(nEvts_in_ee,4));
-
-  //cout << "efficiency, " << nJets << "jets: " << eff_movere << " +/- " << eff_movere_Err << endl;
-  
-  f->Close();
-  return make_pair(k, kErr2);
+   f->Close();
+   return make_pair(k,kErr2);
   
 }
   
@@ -111,51 +126,54 @@ pair<Double_t, Double_t> getkFromAll(TString fname, int nJets, TString hyp_type)
     TString histoName(l_keys->At(i)->GetName());
     if(histoName.Contains("WQQ")) continue;
     if(histoName.Contains("wz")) continue;
-    if(histoName.EndsWith("ee") && histoName.Contains(Form("hdilMass_%ij_",nJets) ) ) 
+    if(histoName.EndsWith("ee") && histoName.Contains("hnJetinZwindow"))
       vhistos_ee.push_back((TH1F*)f->Get(histoName.Data()));
-    if(histoName.EndsWith("mm") && histoName.Contains(Form("hdilMass_%ij_",nJets) ) ) 
+    if(histoName.EndsWith("mm") && histoName.Contains("hnJetinZwindow"))
       vhistos_mm.push_back((TH1F*)f->Get(histoName.Data()));
   }
-
-
-  //get the numbers for ee
+  
+ //get the numbers for ee
   Double_t nEvts_in_ee = 0.0;
-  Double_t nEvts_in_eeErr2 = 0.0;
-  for(unsigned int i = 0 ; i < vhistos_ee.size(); i++) {
-    Int_t binLow  = vhistos_ee.at(i)->FindBin(76.01);
-    Int_t binHigh = vhistos_ee.at(i)->FindBin(105.09);
-    nEvts_in_ee += vhistos_ee.at(i)->Integral(binLow, binHigh);
-    nEvts_in_eeErr2 += getError2(vhistos_ee.at(i), binLow, binHigh);
+  Double_t nEvts_inErr2_ee = 0.0;
+  for(unsigned int j = 0 ; j < vhistos_ee.size(); j++) {
+    if(nJets <= 1) {
+      nEvts_in_ee     += vhistos_ee.at(j)->GetBinContent(nJets+1);
+      nEvts_inErr2_ee += getError2(vhistos_ee.at(j), nJets+1, nJets+1);
+    } else {
+      Int_t binHigh = vhistos_ee.at(j)->GetNbinsX() + 1;
+      for(Int_t i = 3; i <= binHigh; i++) 
+	nEvts_in_ee +=vhistos_ee.at(j)->GetBinContent(i);
+      nEvts_inErr2_ee = getError2(vhistos_ee.at(j), 3, binHigh);
+    }
   }
   
   Double_t nEvts_in_mm = 0.0;
-  Double_t nEvts_in_mmErr2 = 0.0;
-  for(unsigned int i = 0 ; i < vhistos_mm.size(); i++) {
-    Int_t binLow  = vhistos_mm.at(i)->FindBin(76.01);
-    Int_t binHigh = vhistos_mm.at(i)->FindBin(105.09);
-    nEvts_in_mm += vhistos_mm.at(i)->Integral(binLow, binHigh);
-    nEvts_in_mmErr2 += getError2(vhistos_mm.at(i), binLow, binHigh);
+  Double_t nEvts_inErr2_mm = 0.0;
+  for(unsigned int j = 0 ; j < vhistos_mm.size(); j++) {
+    if(nJets <=1) {
+      nEvts_in_mm += vhistos_mm.at(j)->GetBinContent(nJets+1);
+      nEvts_inErr2_mm += getError2(vhistos_mm.at(j), nJets+1, nJets+1);
+    } else {
+      Int_t binHigh = vhistos_mm.at(j)->GetNbinsX() + 1;
+      for(Int_t i = 3; i <= binHigh; i++) 
+	nEvts_in_mm +=   vhistos_mm.at(j)->GetBinContent(i);
+      nEvts_inErr2_mm += getError2(vhistos_mm.at(j), 3, binHigh);
+    }
   }
   
-  Double_t k, kErr2;
+  Double_t k     = 0.;
+  Double_t kErr2 = 0.; 
   if(hyp_type == "mm") {
     k = 0.5*sqrt(nEvts_in_mm/nEvts_in_ee);
-    kErr2 = (1./16.)*(nEvts_in_mmErr2/nEvts_in_ee/nEvts_in_mm + nEvts_in_eeErr2*nEvts_in_mm/pow(nEvts_in_ee,3));
+    kErr2 = (1./16.)*(nEvts_inErr2_mm/nEvts_in_ee/nEvts_in_mm + nEvts_inErr2_ee*nEvts_in_mm/pow(nEvts_in_ee,3));
   } else if(hyp_type == "ee") {
     k = 0.5*sqrt(nEvts_in_ee/nEvts_in_mm);
-    kErr2 = (1./16.)*(nEvts_in_eeErr2/nEvts_in_mm/nEvts_in_ee + nEvts_in_mmErr2*nEvts_in_ee/pow(nEvts_in_mm,3));
+    kErr2 = (1./16.)*(nEvts_inErr2_ee/nEvts_in_mm/nEvts_in_ee + nEvts_inErr2_mm*nEvts_in_ee/pow(nEvts_in_mm,3));
   }
-    
-    
-    
-  //cout << "k " << k << " +/- " << kErr << endl;
-  
-  //Double_t eff_movere = nEvts_in_mm/nEvts_in_ee;
-  //Double_t eff_movere_Err2 = nEvts_in_mmErr2/pow(nEvts_in_ee,2) + pow(nEvts_in_mm,2)*nEvts_in_eeErr2/pow(nEvts_in_ee,4);
-  
-  //cout << "efficiency, " << nJets << "jets: " << eff_movere << " +/- " << eff_movere_Err << endl;
+
   f->Close();
   return make_pair(k, kErr2);
+
 }
     
   
@@ -165,35 +183,39 @@ pair<Double_t,Double_t> getTrueNOut(TString fname, int nJets, TString hyptype ) 
   TFile *f = TFile::Open(fname.Data(), "READ");
   
   //Get the DY dilMass histo for this jet bin and type
-  TString histoname(Form("DY%s_hdilMass_%ij_%s", hyptype.Data(), nJets,hyptype.Data()));
-  TH1F *h_dy_mll = (TH1F*)f->Get(histoname.Data());
-  histoname = Form("zz_hdilMass_%ij_%s", nJets,hyptype.Data());
-  TH1F *h_zz_mll = (TH1F*)f->Get(histoname.Data());
-  if (h_dy_mll==NULL) {
+  TString histoname(Form("DY%s_hnJetoutZwindow_%s", hyptype.Data(),hyptype.Data()));
+  TH1F *h_nJets_dy = (TH1F*)f->Get(histoname.Data());
+  histoname = Form("zz_hnJetoutZwindow_%s", hyptype.Data());
+  TH1F *h_nJets_zz = (TH1F*)f->Get(histoname.Data());
+  if (h_nJets_dy==NULL) {
     cout << "Can't find " << histoname << ". Returning" << endl;
     return make_pair(0.,0.);
   }
 
-  if (h_zz_mll==NULL) {
+  if (h_nJets_zz==NULL) {
     cout << "Can't find " << histoname << ". Returning" << endl;
     return make_pair(0.,0.);
   }
     
-  Int_t binLow  = h_dy_mll->FindBin(76.01);
-  Int_t binHigh = h_dy_mll->FindBin(105.09);
-  Double_t nOut = h_dy_mll->Integral(0, binLow-1) + h_dy_mll->Integral(binHigh+1, h_dy_mll->GetNbinsX()+1);
-  Double_t nOutErr2 = getError2(h_dy_mll, 0, binLow-1) + getError2(h_dy_mll, binHigh+1, h_dy_mll->GetNbinsX()+1);
+  Double_t nOut     = 0.;
+  Double_t nOutErr2 = 0.;
+  
+  if(nJets <=1) {
+    nOut = h_nJets_dy->GetBinContent(nJets+1) + h_nJets_zz->GetBinContent(nJets+1);
+    nOutErr2 = getError2(h_nJets_dy, nJets+1, nJets+1) + getError2(h_nJets_zz, nJets+1, nJets+1);
+  } else {
+    Int_t binHigh = h_nJets_dy->GetNbinsX()+1;
+    for(Int_t i = 3; i <= binHigh; i++) 
+      nOut     += h_nJets_dy->GetBinContent(i) + h_nJets_zz->GetBinContent(i);
+    nOutErr2 += getError2(h_nJets_dy, 3, binHigh) + getError2(h_nJets_zz, 3, binHigh);
+  }
 
-  binLow  = h_zz_mll->FindBin(76.01);
-  binHigh = h_zz_mll->FindBin(105.09);
-  nOut = nOut + h_zz_mll->Integral(0, binLow-1) + h_zz_mll->Integral(binHigh+1, h_zz_mll->GetNbinsX()+1);
-  nOutErr2 = nOutErr2 + getError2(h_zz_mll, 0, binLow-1) + getError2(h_zz_mll, binHigh+1, h_zz_mll->GetNbinsX()+1);
-  
-  delete h_dy_mll;
-  delete h_zz_mll;
-  
+  delete h_nJets_dy;
+  delete h_nJets_zz;
+
   f->Close();
   return make_pair(nOut, nOutErr2);
+
 
 }
 
@@ -207,18 +229,23 @@ pair<Double_t, Double_t> getnEMuIn(TString fname, int nJets) {
     TString histoName(l_keys->At(i)->GetName());
     if(histoName.Contains("WQQ")) continue;
     if(histoName.Contains("wz")) continue;
-    if(histoName.EndsWith("em") && histoName.Contains(Form("hdilMass_%ij_",nJets) ) ) 
+    if(histoName.EndsWith("em") && histoName.Contains("hnJetinZwindow"))
       vhistos_em.push_back((TH1F*)f->Get(histoName.Data()));
     }
 
   
   Double_t nEMuIn = 0.;
   Double_t nEMuInErr2 = 0.;
-  for(unsigned int i = 0; i < vhistos_em.size(); i++) {
-    Int_t binLow  = vhistos_em.at(i)->FindBin(76.01);
-    Int_t binHigh = vhistos_em.at(i)->FindBin(105.09);
-    nEMuIn += vhistos_em.at(i)->Integral(binLow, binHigh);
-    nEMuInErr2 = getError2(vhistos_em.at(i), binLow, binHigh);
+  for(unsigned int j = 0; j < vhistos_em.size(); j++) {
+    if(nJets <= 1) {
+      nEMuIn      += vhistos_em.at(j)->GetBinContent(nJets+1);
+      nEMuInErr2  += getError2(vhistos_em.at(j), nJets+1, nJets+1);
+    } else {
+      Int_t binHigh = vhistos_em.at(j)->GetNbinsX() + 1;
+      for(Int_t i = 3; i <= binHigh; i++) 
+	nEMuIn   += vhistos_em.at(j)->GetBinContent(i);
+      nEMuInErr2 += getError2(vhistos_em.at(j), 3, binHigh);
+    }
   }
   
   return make_pair(nEMuIn, nEMuInErr2 );  
@@ -236,18 +263,23 @@ pair<Double_t, Double_t> getnLLIn(TString fname, int nJets, TString hyptype) {
     TString histoName(l_keys->At(i)->GetName());
     if(histoName.Contains("WQQ")) continue;
     if(histoName.Contains("wz")) continue;
-    if(histoName.Contains(Form("hdilMass_%ij_%s",nJets, hyptype.Data()) ) )
+    if(histoName.Contains(Form("hnJetinZwindow_%s", hyptype.Data()) ) )
       vhistos.push_back((TH1F*)f->Get(histoName.Data()));
     }
 
   
   Double_t nllIn = 0.;
   Double_t nllInErr2 = 0.;
-  for(unsigned int i = 0; i < vhistos.size(); i++) {
-    Int_t binLow  = vhistos.at(i)->FindBin(76.01);
-    Int_t binHigh = vhistos.at(i)->FindBin(105.09);
-    nllIn += vhistos.at(i)->Integral(binLow, binHigh);
-    nllInErr2 = getError2(vhistos.at(i), binLow, binHigh);
+  for(unsigned int j = 0; j < vhistos.size(); j++) {
+    if(nJets <= 1) {
+      nllIn      += vhistos.at(j)->GetBinContent(nJets+1);
+      nllInErr2  += getError2(vhistos.at(j), nJets+1, nJets+1);
+    } else {
+      Int_t binHigh = vhistos.at(j)->GetNbinsX() + 1;
+      for(Int_t i = 3; i <= binHigh; i++) 
+	nllIn   += vhistos.at(j)->GetBinContent(i);
+      nllInErr2 += getError2(vhistos.at(j), 3, binHigh);
+    }
   }
   
   return make_pair(nllIn, nllInErr2 );  
@@ -259,11 +291,8 @@ pair<Double_t, Double_t> getnLLIn(TString fname, int nJets, TString hyptype) {
 
 void getPredicted() {
   
-  //TString f_NoMETCut("myHist_isoReg_looseDil08_OS_noDupWt_isoDil08_hltMu9E15.root");
-  //TString f_wMETCut("myHist_isoReg_looseDil08_OS_noDupWt_isoDil08_preMet08_hltMu9E15.root");
-  
-  TString f_NoMETCut("myHist_1106944__looseDil08_OS_noDupWt_isoDil08_hltMu9E15.root");
-  TString f_wMETCut("myHist_1369088__looseDil08_OS_noDupWt_isoDil08_preMet08_hltMu9E15.root");
+  TString f_NoMETCut("myHist_1171456__OS_noDupWt_isoDil08_preDil08noIso_hltMu9E15.root");
+  TString f_wMETCut("myHist_1433600__OS_noDupWt_isoDil08_preDil08noIso_preMet08_hltMu9E15.root");
 
   //Declare histos for predicted numbers
   TH1F *h_Nout_ee = new TH1F("h_Nout_ee", "", 3, 0,3);
@@ -375,7 +404,7 @@ void getPredicted() {
 	 << p_truNOut_ee.first << " +/- " << sqrt(p_truNOut_ee.second) << "  |  " << endl;
 
     
-    Double_t nDYErr2 = p_nllIn_ee.first + pow(p_k.first,2)*p_nIn_emu.first + pow(p_nIn_emu.first,2)*kErr2_ee;
+    //Double_t nDYErr2 = p_nllIn_ee.first + pow(p_k.first,2)*p_nIn_emu.first + pow(p_nIn_emu.first,2)*kErr2_ee;
     //Double_t dlEvans_err = pow(predicted_ee,2)*(rOutInErr2_ee/pow(p_rOutIn_ee.first,2) + nDYErr2/pow(p_nllIn_ee.first,2));
     
     //cout << sqrt(dlEvans_err) << endl;
@@ -389,7 +418,6 @@ void getPredicted() {
   
   
   cout << endl << endl << "MM" << endl;
-  //cout << "|  *nJets*  |  *R*  |  *Nout(Predicted)*  |  *Nout(True)*  |" << endl;
   cout << "|  *nJets*  |  *NIn_ll*  |  *NIn_emu*  |  *R*  |  *Nout(Predicted)*  |  *Nout(True)*  |" << endl;
   
   //mm
