e660023560323d72863692082d2a8f9c5e20b3cc
slava77 <slava77>
2009-05-07 00:15:28 +0000

    add doAllCombined to make histograms like used in the PAS

--- NtupleMacros/TTDil/slava/doAll.C
+++ NtupleMacros/TTDil/slava/doAll.C
@@ -48,6 +48,8 @@ void doAll(unsigned int bitmask, bool skipFWLite = false){
   // 1926144 -- baseline without tight iso (only loose iso)
   // 18735104 -- baseline JES-up
   // 10346496 -- baseline JES-down
+  // 1171456 -- baseline without MET, without zveto
+  // 2220032 -- baseline without MET, without zveto, using AN09/047 (v<=4) trigger selection
 
   // 1941504 -- baseline without duplicate removal
 
@@ -317,6 +319,218 @@ void doAll(unsigned int bitmask, bool skipFWLite = false){
   //save all the histograms
     
   const char* outFile = Form("myHist_%d_%s.root", bitmask, looper->compactConfig.c_str());
+  hist::saveHist(outFile);
+  hist::deleteHistos();
+
+  cout << "Finished with bitmask "<<bitmask << endl;
+
+
+  gSystem->Exit(0);
+
+}
+  
+void doAllCombined(unsigned int bitmask, bool skipFWLite = false){
+  //here is a list to the combinations of cuts useful for the analysis:
+  // 1957888 -- baseline
+  // 1695744 -- baseline without MET
+  // 1433600 -- baseline without zveto
+  // 1926144 -- baseline without tight iso (only loose iso)
+  // 18735104 -- baseline JES-up
+  // 10346496 -- baseline JES-down
+  // 1171456 -- baseline without MET, without zveto
+  // 2220032 -- baseline without MET, without zveto, using AN09/047 (v<=4) trigger selection
+
+  // 1941504 -- baseline without duplicate removal
+
+  
+  //cut <-> bit mask
+  //ID cuts               -> 2**0 (1)
+  //Isolation cuts        -> 2**1 (2) (default is both legs are isolated. Using relative isolation, TRK+CALO)
+  //                       2**8+2**1 (Require one-only hyp lepton to be isolated. In emu, that will mean that the muon
+  //                                  will have rel iso > 0.92 and the el will have 0.6 < relIso < 0.92)
+  //                       2**9+2**1 (Require one-only hyp lepton to be isolated. In emu, that will mean that the el
+  //                                  will have rel iso > 0.92 and the mu will have 0.6 < relIso < 0.92)
+  //                       2**9+2**8+2**1 (require both leptons to have 0.6 < relIso < 0.92)
+  //DileptonMassVeto      -> 2**2 (4)
+  //METcut                -> 2**3 (8)
+  //nJets                 -> 2**4 (16)
+  //extra MuTag           -> 2**5 (32)
+  //METveto               -> 2**6 (64)
+  //Extra MuTag (pt>5)    -> 2**7 (128)
+  //looseDileptonSelection, TTdil note 2008
+  //                      -> 2**10 (1024)
+  //fullMultipleHypsOnly  -> 2**11 (2048) !!!!! Not implemented, so does nothing right now !!!!!!!
+  //applyZWindow cut      -> 2**12 (4096)
+  //Opp. Sign Selection   -> 2**13 (8192)
+  //fillMaxWeightDilOnly  -> 2**14 (16284
+  //leptonIsolationDilSelectionTTDil08 -> 2**15 (uses trk and calo isolation seperately, reltrkIso > 0.9, 
+  //                                             relCaloIso > 0.9. NO OTHER CUTS BUT ISOLATION APPLIED)
+  //looseDilSelectionNoIsoTTDil08      -> 2**16 (basic muon preselection cuts, no isolation)
+  //lepton20Eta2p4DilSelection         -> 2**17 (only pt and eta cuts applied to leptons)
+  //metBaselineSelectionTTDil108       -> 2**18 (calls passPatMet_OF20_SF30 -> corrMET  > 20 (emu),
+  //                                             corrMET > 30 (ee, mumu))
+  //dilepMassVetoCutTTDil08            -> 2**19 pretty much what it sounds like
+  //applyTriggersMu9orLisoE15          -> 2**20 mm -> HLT_Mu9 
+  //                                            ee -> HLT_Ele15_SW_L1R
+  //                                            em -> HLT_Mu9 or HLT_Ele15_SW_L1R
+  //applyTriggersTTDil08JanTrial       -> 2**21 mm -> HLT_Mu15_L1Mu7 || HLT_DoubleMu3
+  //                                            ee -> HLT_IsoEle18_L1R || HLT_DoubleIsoEle12_L1R
+  //                                            em -> HLT_IsoEle18_L1R || HLT_Mu15_L1Mu7 || HLT_IsoEle10_Mu10_L1R
+  // dilepAdditionalMassVetoCutTTDil08 -> 2**22
+  // corJES10ptDn                      -> 2**23 rescale JES by 10% Down
+  // corJES10ptUp                      -> 2**24 rescale JES by 10% up
+  // useTcMet                          -> 2**25 use tcmet
+  // useJPT                            -> 2**26 use JPT
+  // muJetClean                        -> 2**27 do not count jets within 0.4 of muons 
+  // dilTruthMatch                     -> 2**28 require the lepton to be MCtruth matched (coming off a hard-scattering lepton)
+ 
+  // Load various tools  
+  gROOT->ProcessLine(Form(".x setup.C(%d)",skipFWLite));
+
+  // Load and compile the looping code
+  gSystem->CompileMacro("ttDilCounts_looper.C", "++k", "libttDilCounts_looper");
+  
+
+  // Flag for jet selection
+  // true  = hyp_jet selection (15 GeV uncorrectedm eta<3)
+  // false = 30 GeV corrected, eta<2.4
+  bool oldjet=false;
+
+  // K-factors
+  //these have been k-factors NLO/LO before
+  //now using them as sample normalizations to NLO
+  
+  //these two are taken from Ceballos's pdf. 
+  //It looks like the top x-section is for mtop = 175 GeV
+  float kttdil    = 1.; //375pb, 127000 events processed
+  float kttotr    = 1.; //375pb, 127000 events processed
+
+  float kVV       = 1.;
+  float kWjets    = 1.;
+  float kDYeemm   = 1.;
+  float kDYtautau = 1.;
+  float kQCD      = 1.;
+  float kt        = 1.;
+
+  // Prescales
+  int prettdil    = 1;
+  int prettotr    = 1;
+  int preVV       = 1;
+  int preWjets    = 1;
+  int preDYeemm   = 1;
+  int preDYtautau = 1;
+  int preQCD      = 1;
+  int pret        = 1;
+
+  // Flags for files to run over
+  bool runttdil    = true;
+  bool runttotr    = true;
+  bool runVV       = true;
+  bool runWjets    = true;
+  bool runDYeemm   = true;
+  bool runDYtautau = true;
+  bool runQCD      = true;
+  bool runt        = true;
+
+  TChain* chtopdil = new TChain("Events");
+  pickSkimIfExists(chtopdil, "data/TTJets-madgraph_Fall08_IDEAL_V9_v2/merged*.root", "_skimSimple2020anydil");
+
+  TChain* chtopotr = new TChain("Events");
+  pickSkimIfExists(chtopotr, "data/TTJets-madgraph_Fall08_IDEAL_V9_v2/merged*.root", "_skimSimple2020nodil");
+
+  TChain* chVV = new TChain("Events");
+  pickSkimIfExists(chVV, "data/WW_Summer08_IDEAL_V9_v1/merged*.root", "");
+  pickSkimIfExists(chVV, "data/WZ_incl_Summer08_IDEAL_V9_v2/merged*.root", ""); // can try WZ_3l-Pythia
+  pickSkimIfExists(chVV, "data/ZZ_Summer08_IDEAL_V9_v1/merged*.root", "");
+  
+  TChain* chWjets = new  TChain("Events");
+  pickSkimIfExists(chWjets, "data/WJets-madgraph_Fall08_IDEAL_V9_v1/merged*.root", "");
+
+  TChain* chDYtautau = new  TChain("Events");
+  pickSkimIfExists(chDYtautau, "data/ZJets-madgraph_Fall08_IDEAL_V9_reco-v2/merged*.root", "_skimSimple2020tautau");
+  //the low-mass splice has no choice other than the skim
+  pickSkimIfExists(chDYtautau, "data/Ztautau_M20_Summer08_IDEAL_V9_v1/merged*.root_skimSimple2020_20m50", "");
+  pickSkimIfExists(chDYtautau, "data/VQQ-madgraph_Fall08_IDEAL_V9_v1/merged*.root", "");
+  
+  TChain* chDYeemm = new  TChain("Events");
+  pickSkimIfExists(chDYeemm, "data/ZJets-madgraph_Fall08_IDEAL_V9_reco-v2/merged*.root", "_skimSimple2020ee");
+  //the low-mass splice has no choice other than the skim
+  pickSkimIfExists(chDYeemm, "data/Zee_M20_Summer08_IDEAL_V9_reco-v3/merged*.root_skimSimple2020_20m50", "");
+  pickSkimIfExists(chDYeemm, "data/ZJets-madgraph_Fall08_IDEAL_V9_reco-v2/merged*.root", "_skimSimple2020mm");
+  //the low-mass splice has no choice other than the skim
+  pickSkimIfExists(chDYeemm, "data/Zmumu_M20_Summer08_IDEAL_V9_reco-v2/merged*.root_skimSimple2020_20m50", "");
+  pickSkimIfExists(chDYeemm, "data/VQQ-madgraph_Fall08_IDEAL_V9_v1/merged*.root", "");
+  
+  //ppMuX
+  TChain* chQCD = new  TChain("Events");
+  pickSkimIfExists(chQCD, "data/InclusiveMuPt15/merged*.root", "_skimSimple2020"); 
+  pickSkimIfExists(chQCD, "data/QCD_EMenriched_Pt20to30/merged*.root", "_skimSimple2020");
+  pickSkimIfExists(chQCD, "data/QCD_EMenriched_Pt30to80/merged*.root", "_skimSimple2020");
+  pickSkimIfExists(chQCD, "data/QCD_EMenriched_Pt80to170/merged*.root", "_skimSimple2020");
+  pickSkimIfExists(chQCD, "data/QCD_BCtoE_Pt20to30/merged*.root", "_skimSimple2020");
+  pickSkimIfExists(chQCD, "data/QCD_BCtoE_Pt30to80/merged*.root", "_skimSimple2020");
+  pickSkimIfExists(chQCD, "data/QCD_BCtoE_Pt80to170/merged*.root", "_skimSimple2020");
+
+  //tW
+  TChain* cht = new  TChain("Events");
+  pickSkimIfExists(cht, "data/SingleTop_sChannel-madgraph-LHE/merged*.root", ""); 
+  pickSkimIfExists(cht, "data/SingleTop_tChannel-madgraph-LHE/merged*.root", ""); 
+  pickSkimIfExists(cht, "data/SingleTop_tWChannel-madgraph-LHE/merged*.root", ""); 
+
+  // Define colors numbers:
+  gStyle->SetPalette(1);
+  enum EColor { kWhite, kBlack, kRed, kGreen, kBlue, kYellow, kMagenta, kCyan };
+  
+
+
+  ttDilCounts_looper* looper = new ttDilCounts_looper();
+  
+  // Process files one at a time, and color them as needed
+  if (runttdil) {
+    cout << "Processing ttbar dileptonic.. "<<endl;
+    looper->ScanChain(chtopdil,"ttdil", kttdil, prettdil, oldjet, bitmask);
+    cout << "Done Processing ttbar dileptonic.. "<<endl;
+    hist::color("ttdil", kYellow);
+  }
+  if (runttotr) {
+    cout << "Processing ttbar no-dileptons.. "<<endl;
+    looper->ScanChain(chtopotr,"ttotr", kttotr, prettotr, oldjet, bitmask);
+    hist::color("ttotr", 30);
+  }
+  if (runVV) {
+    cout << "Processing VV.."<<endl;
+    looper->ScanChain(chVV,"VV", kVV, preVV, oldjet, bitmask);
+    hist::color("VV", kRed);
+  }
+  if (runWjets) {
+    cout << "Processing Wjets.."<<endl;
+    looper->ScanChain(chWjets,"wjets", kWjets, preWjets, oldjet, bitmask);
+    hist::color("wjets", 40);
+  }
+  if (runDYtautau) {
+    cout << "Processing DY->tautau" << endl;
+    looper->ScanChain(chDYtautau,"DYtautau", kDYtautau, preDYtautau, oldjet, bitmask);
+    hist::color("DYtautau", kBlack);
+  }
+  if (runDYeemm) {
+    cout << "Processing DY->ee/mm" << endl;
+    looper->ScanChain(chDYeemm,"DYeemm", kDYeemm, preDYeemm, oldjet, bitmask);
+    hist::color("DYeemm", kMagenta);
+  }
+  if (runQCD) {
+    cout << "Processing ppMuX and EM"<<endl;
+    looper->ScanChain(chQCD,"QCD", kQCD, preQCD, oldjet, bitmask);
+    hist::color("QCD", 51);
+  }
+  if (runt) {
+    cout << "Processing t"<<endl;
+    looper->ScanChain(cht,"t", kt, pret, oldjet, bitmask);
+    hist::color("t_", 63);
+  }
+    
+  //save all the histograms
+    
+  const char* outFile = Form("myHistComb_%d_%s.root", bitmask, looper->compactConfig.c_str());
   hist::saveHist(outFile);
   hist::deleteHistos();
 
