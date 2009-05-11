6a9505f268ba41d856b4ae73c5a9887f84703e43
dmytro <dmytro>
2009-05-11 00:55:50 +0000

    update for ARC

--- NtupleMacros/WW/plot_met.cxx
+++ NtupleMacros/WW/plot_met.cxx
@@ -3,7 +3,6 @@
   gROOT->SetStyle("Plain");
   c1 = new TCanvas("c1","c1",500,500);
   s = new THStack("s","");
-  TLegend* leg = new TLegend(0.75, 0.75, 0.99, 0.99);
   
   ttbar_hmet_all->Rebin(10);
   tw_hmet_all = (TH1F*)f->Get("tw_hmet_all");
@@ -11,7 +10,6 @@
   ttbar_hmet_all->Add(tw_hmet_all);
   ttbar_hmet_all->SetFillColor(kYellow);
   s->Add(ttbar_hmet_all);
-  leg->AddEntry(ttbar_hmet_all, "Top", "lpf");	
   
   dytt_hmet_all->Rebin(10);
   dyee_hmet_all = (TH1F*)f->Get("dyee_hmet_all");
@@ -22,7 +20,6 @@
   dytt_hmet_all->Add(dymm_hmet_all);
   dytt_hmet_all->SetFillColor(kBlack);
   s->Add(dytt_hmet_all);
-  leg->AddEntry(dytt_hmet_all, "DY", "lpf");	
   
   wz_hmet_all->Rebin(10);
   zz_hmet_all = (TH1F*)f->Get("zz_hmet_all");
@@ -30,20 +27,25 @@
   wz_hmet_all->Add(zz_hmet_all);
   wz_hmet_all->SetFillColor(kBlue);
   s->Add(wz_hmet_all);
-  leg->AddEntry(wz_hmet_all, "ZZ/WZ", "lpf");	
 
   wjets_hmet_all->Rebin(10);
   wjets_hmet_all->SetFillColor(kGray);
   s->Add(wjets_hmet_all);
-  leg->AddEntry(wjets_hmet_all, "Wjets", "lpf");	
     
   ww_hmet_all->Rebin(10);
   ww_hmet_all->SetFillColor(kRed);
   s->Add(ww_hmet_all);
-  leg->AddEntry(ww_hmet_all, "WW", "lpf");	
 	
   s->Draw("hist");
-  s->GetXaxis()->SetTitle("MET, [GeV]");
+  s->GetXaxis()->SetTitle("MET, GeV");
+  s->SetTitle("CMS Preliminary");
   s->Draw("hist");
+
+  TLegend* leg = new TLegend(0.75, 0.75, 0.99, 0.99);
+  leg->AddEntry(ww_hdilMass_all, "WW", "lpf");	
+  leg->AddEntry(wjets_hdilMass_all, "Wjets", "lpf");	
+  leg->AddEntry(wz_hdilMass_all, "ZZ/WZ", "lpf");	
+  leg->AddEntry(dytt_hdilMass_all, "DY", "lpf");	
+  leg->AddEntry(ttbar_hdilMass_all, "Top", "lpf");	
   leg->Draw();
 }
--- NtupleMacros/WW/plot_mll.cxx
+++ NtupleMacros/WW/plot_mll.cxx
@@ -3,7 +3,6 @@
   gROOT->SetStyle("Plain");
   c1 = new TCanvas("c1","c1",500,500);
   s = new THStack("s","");
-  TLegend* leg = new TLegend(0.75, 0.75, 0.99, 0.99);
   
   ttbar_hdilMass_all->Rebin(10);
   tw_hdilMass_all = (TH1F*)f->Get("tw_hdilMass_all");
@@ -11,7 +10,6 @@
   ttbar_hdilMass_all->Add(tw_hdilMass_all);
   ttbar_hdilMass_all->SetFillColor(kYellow);
   s->Add(ttbar_hdilMass_all);
-  leg->AddEntry(ttbar_hdilMass_all, "Top", "lpf");	
   
   dytt_hdilMass_all->Rebin(10);
   dyee_hdilMass_all = (TH1F*)f->Get("dyee_hdilMass_all");
@@ -22,7 +20,6 @@
   dytt_hdilMass_all->Add(dymm_hdilMass_all);
   dytt_hdilMass_all->SetFillColor(kBlack);
   s->Add(dytt_hdilMass_all);
-  leg->AddEntry(dytt_hdilMass_all, "DY", "lpf");	
   
   wz_hdilMass_all->Rebin(10);
   zz_hdilMass_all = (TH1F*)f->Get("zz_hdilMass_all");
@@ -30,20 +27,32 @@
   wz_hdilMass_all->Add(zz_hdilMass_all);
   wz_hdilMass_all->SetFillColor(kBlue);
   s->Add(wz_hdilMass_all);
-  leg->AddEntry(wz_hdilMass_all, "ZZ/WZ", "lpf");	
 
   wjets_hdilMass_all->Rebin(10);
   wjets_hdilMass_all->SetFillColor(kGray);
   s->Add(wjets_hdilMass_all);
-  leg->AddEntry(wjets_hdilMass_all, "Wjets", "lpf");	
     
   ww_hdilMass_all->Rebin(10);
   ww_hdilMass_all->SetFillColor(kRed);
   s->Add(ww_hdilMass_all);
-  leg->AddEntry(ww_hdilMass_all, "WW", "lpf");	
 	
   s->Draw("hist");
-  s->GetXaxis()->SetTitle("Mass, [GeV]");
+  s->GetXaxis()->SetTitle("Mass, GeV/c^{2}");
+  /*
+  // make it bigger and uglier for ARC
+  s->GetXaxis()->SetLabelSize(0.06);
+  s->GetXaxis()->SetTitleSize(0.06);
+  s->GetYaxis()->SetLabelSize(0.06);
+  s->GetYaxis()->SetLabelOffset(0.02);
+  */
+  s->SetTitle("CMS Preliminary");
   s->Draw("hist");
+  
+  TLegend* leg = new TLegend(0.75, 0.75, 0.99, 0.99);
+  leg->AddEntry(ww_hdilMass_all, "WW", "lpf");	
+  leg->AddEntry(wjets_hdilMass_all, "Wjets", "lpf");	
+  leg->AddEntry(wz_hdilMass_all, "ZZ/WZ", "lpf");	
+  leg->AddEntry(dytt_hdilMass_all, "DY", "lpf");	
+  leg->AddEntry(ttbar_hdilMass_all, "Top", "lpf");	
   leg->Draw();
 }
