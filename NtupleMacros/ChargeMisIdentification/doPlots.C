095cb1c9e53cd1a30e926731c278219115e91947
gutsche <gutsche>
2009-05-25 18:39:25 +0000

    add efficiency boxes for forward and barrel in eta plots

--- NtupleMacros/ChargeMisIdentification/doPlots.C
+++ NtupleMacros/ChargeMisIdentification/doPlots.C
@@ -3,6 +3,17 @@
   TCanvas *canvas = new TCanvas;
   TPaveText *box = 0;
   double eff = 0.;
+  double barrel_num = 0.;
+  double barrel_den = 0.;
+  double forward_num = 0.;
+  double forward_den = 0.;
+  double barrel_eff = 0.;
+  double forward_eff = 0.;
+
+  double f_b_eta_divider = 1.479;
+
+  TH1F *els_pt_sim = (TH1F*)gFile->Get("els_pt_sim");
+  TH1F *els_eta_sim = (TH1F*)gFile->Get("els_eta_sim");
 
   TH1F *els_pt_recosim = (TH1F*)gFile->Get("els_pt_recosim");
   TH1F *els_eta_recosim = (TH1F*)gFile->Get("els_eta_recosim");
@@ -43,6 +54,12 @@
   canvas->SetLeftMargin(0.155);
   canvas->SetBottomMargin(0.155);
   eff = els_eta_reco->Integral() / els_eta_sim->Integral() * 100;
+  barrel_num = els_eta_reco->Integral(els_eta_reco->FindBin(-1*f_b_eta_divider),els_eta_reco->FindBin(f_b_eta_divider));
+  barrel_den = els_eta_sim->Integral(els_eta_sim->FindBin(-1*f_b_eta_divider),els_eta_sim->FindBin(f_b_eta_divider));
+  barrel_eff = barrel_num / barrel_den * 100;
+  forward_num = els_eta_reco->Integral(0,els_eta_reco->FindBin(-1*f_b_eta_divider)) + els_eta_reco->Integral(els_eta_reco->FindBin(f_b_eta_divider),els_eta_reco->GetNbinsX()+1);
+  forward_den = els_eta_sim->Integral(0,els_eta_sim->FindBin(-1*f_b_eta_divider)) + els_eta_sim->Integral(els_eta_sim->FindBin(f_b_eta_divider),els_eta_sim->GetNbinsX()+1);
+  forward_eff = forward_num / forward_den * 100;
   els_eta_reco->Divide(els_eta_sim);
   els_eta_reco->SetName("RecoEff");
   els_eta_reco->SetTitle("Reconstruction efficiency (reco/sim) in #eta");
@@ -50,10 +67,11 @@
   els_eta_reco->SetMaximum(1.01);
   els_eta_reco->SetMinimum(0.);
   els_eta_reco->Draw();
-  box = new TPaveText(-1,0.4,1,0.5);
+  box = new TPaveText(-1,0.3,1,0.5);
   box->SetBorderSize(1);
   box->SetFillColor(0);
-  box->InsertText(Form("reco eff: %2.1f %s",eff,"%"));
+  box->InsertText(Form("reco eff barrel (|#eta| #leq 1.479): %2.1f %s",barrel_eff,"%"));
+  box->InsertText(Form("reco eff forward (|#eta| > 1.479) : %2.1f %s",forward_eff,"%"));
   box->Draw();
   canvas->Print("RecoEfficiencyEta.png");
 
@@ -78,7 +96,12 @@
   canvas->Clear();
   canvas->SetLeftMargin(0.155);
   canvas->SetBottomMargin(0.155);
-  eff = els_eta_recosim_incorCharge->Integral() / els_eta_recosim->Integral() * 100;
+  barrel_num = els_eta_recosim_incorCharge->Integral(els_eta_recosim_incorCharge->FindBin(-1*f_b_eta_divider),els_eta_recosim_incorCharge->FindBin(f_b_eta_divider));
+  barrel_den = els_eta_recosim->Integral(els_eta_recosim->FindBin(-1*f_b_eta_divider),els_eta_recosim->FindBin(f_b_eta_divider));
+  barrel_eff = barrel_num / barrel_den * 100;
+  forward_num = els_eta_recosim_incorCharge->Integral(0,els_eta_recosim_incorCharge->FindBin(-1*f_b_eta_divider)) + els_eta_recosim_incorCharge->Integral(els_eta_recosim_incorCharge->FindBin(f_b_eta_divider),els_eta_recosim_incorCharge->GetNbinsX()+1);
+  forward_den = els_eta_recosim->Integral(0,els_eta_recosim->FindBin(-1*f_b_eta_divider)) + els_eta_recosim->Integral(els_eta_recosim->FindBin(f_b_eta_divider),els_eta_recosim->GetNbinsX()+1);
+  forward_eff = forward_num / forward_den * 100;
   els_eta_recosim_incorCharge->Divide(els_eta_recosim);
   els_eta_recosim_incorCharge->SetName("ChargeIDIneff");
   els_eta_recosim_incorCharge->SetTitle("Charge id inefficiency (recosim_incorCharge/recosim) in #eta");
@@ -86,10 +109,11 @@
   els_eta_recosim_incorCharge->SetMaximum(0.1);
   els_eta_recosim_incorCharge->SetMinimum(0.);
   els_eta_recosim_incorCharge->Draw();
-  box = new TPaveText(-1,0.085,1,0.095);
+  box = new TPaveText(-1,0.075,1,0.095);
   box->SetBorderSize(1);
   box->SetFillColor(0);
-  box->InsertText(Form("charge id ineff: %2.1f %s",eff,"%"));
+  box->InsertText(Form("charge id ineff barrel (|#eta| #leq 1.479): %2.1f %s",barrel_eff,"%"));
+  box->InsertText(Form("charge id ineff forward (|#eta| > 1.479) : %2.1f %s",forward_eff,"%"));
   box->Draw();
   canvas->Print("ChargeIDInefficiencyEta.png");
 
@@ -108,88 +132,5 @@
   box->InsertText(Form("els wo assoc. trk: %2.1f %s",eff,"%"));
   box->Draw();
   canvas->Print("TrkWithoutAssocTrk.png");
-
-
-//   canvas->Clear();
-//   canvas->SetLeftMargin(0.155);
-//   canvas->SetBottomMargin(0.155);
-//   els_pt_recosim->Draw();
-//   canvas->Print("els_pt_recosim.png");
-
-//   canvas->Clear();
-//   canvas->SetLeftMargin(0.155);
-//   canvas->SetBottomMargin(0.155);
-//   els_eta_recosim->Draw();
-//   canvas->Print("els_eta_recosim.png");
-
-//   canvas->Clear();
-//   canvas->SetLeftMargin(0.155);
-//   canvas->SetBottomMargin(0.155);
-//   els_pt_recosim_incorCharge->Divide(els_pt_recosim);
-//   els_pt_recosim_incorCharge->Draw();
-//   els_pt_recosim_incorCharge->SetName("inCorChargeFractionPt");
-//   els_pt_recosim_incorCharge->SetTitle("Incorrect Charge Fraction in p_{T}");
-//   els_pt_recosim_incorCharge->SetYTitle("Fraction");
-//   els_pt_recosim_incorCharge->SetMaximum(0.1);
-//   canvas->Print("InCorChargeFractionPt.png");
-
-//   canvas->Clear();
-//   canvas->SetLeftMargin(0.155);
-//   canvas->SetBottomMargin(0.155);
-//   els_eta_recosim_incorCharge->Divide(els_eta_recosim);
-//   els_eta_recosim_incorCharge->Draw();
-//   els_eta_recosim_incorCharge->SetName("inCorChargeFraction");
-//   els_eta_recosim_incorCharge->SetTitle("Incorrect Charge Fraction in #eta");
-//   els_eta_recosim_incorCharge->SetYTitle("Fraction");
-//   els_eta_recosim_incorCharge->SetMaximum(0.1);
-//   canvas->Print("InCorChargeFractionEta.png");
-
-//   canvas->Clear();
-//   canvas->SetLeftMargin(0.155);
-//   canvas->SetBottomMargin(0.155);
-//   els_pt_recosim_corCharge->Divide(els_pt_recosim);
-//   els_pt_recosim_corCharge->Draw();
-//   els_pt_recosim_corCharge->SetName("ChargeIdEfficiency");
-//   els_pt_recosim_corCharge->SetTitle("Charge Id Efficiency in p_{T}");
-//   els_pt_recosim_corCharge->SetYTitle("Efficiency");
-//   els_pt_recosim_corCharge->SetMaximum(1.02);
-//   els_pt_recosim_corCharge->SetMinimum(0.9);
-//   canvas->Print("ChargeIdEfficiencyPt.png");
-
-//   canvas->Clear();
-//   canvas->SetLeftMargin(0.155);
-//   canvas->SetBottomMargin(0.155);
-//   els_eta_recosim_corCharge->Divide(els_eta_recosim);
-//   els_eta_recosim_corCharge->Draw();
-//   els_eta_recosim_corCharge->SetName("ChargeIdEfficiency");
-//   els_eta_recosim_corCharge->SetTitle("Charge Id Efficiency in #eta");
-//   els_eta_recosim_corCharge->SetYTitle("Efficiency");
-//   els_eta_recosim_corCharge->SetMaximum(1.02);
-//   els_eta_recosim_corCharge->SetMinimum(0.9);
-//   canvas->Print("ChargeIdEfficiencyEta.png");
-
-//   canvas->Clear();
-//   canvas->SetLeftMargin(0.155);
-//   canvas->SetBottomMargin(0.155);
-//   els_pt_reco_corCharge->Divide(els_pt_reco);
-//   els_pt_reco_corCharge->Draw();
-//   els_pt_reco_corCharge->SetName("ChargeIdPurity");
-//   els_pt_reco_corCharge->SetTitle("Charge Id Purity in p_{T}");
-//   els_pt_reco_corCharge->SetYTitle("Purity");
-//   els_pt_reco_corCharge->SetMaximum(1.02);
-//   els_pt_reco_corCharge->SetMinimum(0.9);
-//   canvas->Print("ChargeIdPurityPt.png");
-
-//   canvas->Clear();
-//   canvas->SetLeftMargin(0.155);
-//   canvas->SetBottomMargin(0.155);
-//   els_eta_reco_corCharge->Divide(els_eta_reco);
-//   els_eta_reco_corCharge->Draw();
-//   els_eta_reco_corCharge->SetName("ChargeIdPurity");
-//   els_eta_reco_corCharge->SetTitle("Charge Id Purity in p_{T}");
-//   els_eta_reco_corCharge->SetYTitle("Purity");
-//   els_eta_reco_corCharge->SetMaximum(1.02);
-//   els_eta_reco_corCharge->SetMinimum(0.9);
-//   canvas->Print("ChargeIdPurityEta.png");
 
 }
