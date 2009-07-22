13e15375d5bd2a71da4093b2d8c968d529a390d6
gutsche <gutsche>
2009-07-22 08:19:12 +0000

    add physics samples

--- NtupleMacros/ChargeMisIdentification/do.C
+++ NtupleMacros/ChargeMisIdentification/do.C
@@ -7,7 +7,17 @@
   gROOT->LoadMacro("ScanChain.C+");
 
   TChain *chain = new TChain("Events");
-  chain->Add("/store/disk02/gutsche/cms2-V1-02-06/SingleElectronFlatPt5To100/merged_ntuple*.root");
+//  chain->Add("/store/disk02/gutsche/cms2-V1-02-06/SingleElectronFlatPt5To100/merged_ntuple*.root"); // cms-tas03
+  chain->Add("/store/disk01/data/gutsche/cms2-V1-02-06/SingleElectronFlatPt5To100/merged_ntuple*.root"); // cms-tas01
+//   chain->Add("/store/disk01/data/cms2-V01-03-01/WZ_incl_Summer08_IDEAL_V11_redigi_v1/*.root"); // cms-tas01
+//   chain->Add("/store/disk01/data/cms2-V01-03-01/WW_Summer08_IDEAL_V11_redigi_v1/*.root"); // cms-tas01
+//   chain->Add("/store/disk01/data/cms2-V01-03-01/ZZ_Summer08_IDEAL_V11_redigi_v1/*.root"); // cms-tas01
+//   chain->Add("/store/disk01/data/cms2-V01-03-01/WJets-madgraph_Summer08_IDEAL_V11_redigi_v1/*.root"); // cms-tas01
+//   chain->Add("/store/disk01/data/cms2-V01-03-01/ZJets-madgraph_Summer08_IDEAL_V11_redigi_v1/*.root"); // cms-tas01
+//   chain->Add("/store/disk01/data/cms2-V01-03-01/TTJets-madgraph_Fall08_IDEAL_V11_redigi_v10/*.root"); // cms-tas01
+//   chain->Add("/store/disk01/data/cms2-V01-03-01/SingleTop_tWChannel_Summer08_IDEAL_V11_redigi_v3/*.root"); // cms-tas01
+
+
   ScanChain(chain);
 
   //save all the histograms
