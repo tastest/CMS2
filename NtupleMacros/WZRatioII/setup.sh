092c414a68f68270e6677a54cd5dff9db7524a8f
dlevans <dlevans>
2009-08-03 11:47:25 +0000

    update to make ntuple location right

--- NtupleMacros/WZRatioII/setup.sh
+++ NtupleMacros/WZRatioII/setup.sh
@@ -3,5 +3,11 @@
 DIR=`pwd`
 DIR2=$PWD/../../
 export CMS2_LOCATION=$DIR2
-export CMS2_NTUPLE_LOCATION=/Users/dlevans/store/
+
+LOC=/Users/dlevans/store/
+if [ `hostname` = "cms-tas03.cern.ch" ]; then
+	LOC=/store/disk01/
+fi
+
+export CMS2_NTUPLE_LOCATION=$LOC
 
