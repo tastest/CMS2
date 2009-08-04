7a3658dc33f52224bfe297b29595ffdd7270a9a9
warren <warren>
2009-08-04 13:27:44 +0000

    Added new functions for getting 2d hists, sum of histograms, making summed stacks, and making sum of two minus third stack.

--- NtupleMacros/Tools/HistogramUtilities.h
+++ NtupleMacros/Tools/HistogramUtilities.h
@@ -10,6 +10,7 @@
 
 // root includes
 #include "TH1F.h"
+#include "TH2F.h"
 #include "THStack.h"
 #include "TLegend.h"
 #include "TString.h"
@@ -20,26 +21,32 @@
 
 class HistogramUtilities {
 
-	public:
-		HistogramUtilities(TString fileName, Double_t lumiNorm);
-		~HistogramUtilities() { delete file_; }
+public:
+  HistogramUtilities(TString fileName, Double_t lumiNorm = 1.0);
+  ~HistogramUtilities() { delete file_; }
 
-		TH1F* getHistogram(sources_t theSources, TString var, TString nJets, TString hyp_type, Int_t rebin = 1);
-		THStack* getStack(sources_t theSources, TString var, TString nJets, TString hyp_type, Int_t rebin = 1);
-		TLegend* getLegend(sources_t theSources, TString var, TString nJets, TString hyp_type);
+  TH1F* getHistogram(sources_t theSources, TString var, TString nJets, TString hyp_type, Int_t rebin = 1);
+  TH1F* getHistogramSum(sources_t theSources, TString var, TString nJets, TString hyp_type, Int_t rebin = 1);
+  TH2F* get2dHistogram(sources_t theSources, TString var, TString nJets, TString hyp_type, Int_t rebin = 1);
 
-		void setOrder(std::vector<DataSource> potentialSources);
+  THStack* getStack(sources_t theSources, TString var, TString nJets, TString hyp_type, Int_t rebin = 1);
+  THStack* getSumStack(sources_t theSources, TString var, TString nJets, TString hyp1, TString hyp2, Int_t rebin = 1);
+  THStack* getSumDifStack(sources_t theSources, TString var, TString nJets, TString hyp1, TString hyp2, TString hyp3, Int_t rebin = 1);
+
+  TLegend* getLegend(sources_t theSources, TString var, TString nJets, TString hyp_type);
+
+  void setOrder(std::vector<DataSource> potentialSources);
 
 
-	private:
+private:
 
-		sources_t makeBit(sources_t source) {
-			return 1ll << source;
-		}
+  sources_t makeBit(sources_t source) {
+	return 1ll << source;
+  }
 
-		TFile *file_;
-		std::vector<DataSource> sources_;
-		Double_t lumiNorm_;
+  TFile *file_;
+  std::vector<DataSource> sources_;
+  Double_t lumiNorm_;
 };
 
 #endif
