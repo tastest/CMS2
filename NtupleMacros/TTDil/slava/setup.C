3040889824578a06876341b7a1af65965f9a4759
slava77 <slava77>
2009-09-03 21:37:04 +0000

    reduce number of options to look for the CORE packages: it's either here in the local directory or two dirs up as in the CVS tree

--- NtupleMacros/TTDil/slava/setup.C
+++ NtupleMacros/TTDil/slava/setup.C
@@ -21,7 +21,7 @@ void setup(bool skipFWLite = false){
 
   std::string cms2Location = "";
   if (getenv("CMS2_LOCATION") ) cms2Location = Form("%s/NtupleMacros", getenv("CMS2_LOCATION"));
-  else {
+  else if (1>2){
     std::cout<<"CMS2_LOCATION is not set: it should point to what you where a nominal /pathOrRelativePath/CMS2 is"<<std::endl;
     if (getenv("CMSSW_BASE")){
       cms2Location = Form("%s/src/CMS2/NtupleMacros", getenv("CMSSW_BASE"));
@@ -29,6 +29,6 @@ void setup(bool skipFWLite = false){
     }
   }
 
-  //punch in more quesses where NtupleMacros are -- don't keep multiple copies ;)
-  gSystem->AddIncludePath(Form(" -w -I./ -I./CORE -I%s", cms2Location.c_str() ));
+  //punch in more guesses where NtupleMacros are -- don't keep multiple copies ;)
+  gSystem->AddIncludePath(Form(" -w -I./ -I../../" ));
 }
