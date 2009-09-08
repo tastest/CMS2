4ded399e30cf09e554e15f204830597ee4cfad03
dlevans <dlevans>
2009-09-08 08:43:27 +0000

    make setup script get the right cms 2 location, but actually correctly this time

--- NtupleMacros/HybridLooper/setup.sh
+++ NtupleMacros/HybridLooper/setup.sh
@@ -1,4 +1,3 @@
-
-export CMS2_LOCATION=` $PWD | sed 's/NtupleMacros\/HybridLooper//'`
+export CMS2_LOCATION=`echo $PWD | sed 's/NtupleMacros\/HybridLooper//'`
 export CMS2_NTUPLE_LOCATION=/store/disk01/
 
