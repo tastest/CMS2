df0a7ca2a5ac8f72edfec8f7bfedc2f5a686abb3
fgolf <fgolf>
2009-09-10 13:29:12 +0000

    updated class definitions to use LorentzVectors with floats rather than doubles

--- Dictionaries/src/classes.h
+++ Dictionaries/src/classes.h
@@ -19,11 +19,11 @@ namespace {
       std::vector<std::vector<std::vector<float> > > vf3d;
       edm::Wrapper<std::vector<std::vector<std::vector<float> > > > wvf3d;
       
-      std::vector<std::vector<math::XYZTLorentzVector> > vp2d;
-      edm::Wrapper<std::vector<std::vector<math::XYZTLorentzVector> > > wvp2d;
+      std::vector<std::vector<math::XYZTLorentzVectorF> > vp2d;
+      edm::Wrapper<std::vector<std::vector<math::XYZTLorentzVectorF> > > wvp2d;
       
-      std::vector<std::vector<std::vector<math::XYZTLorentzVector> > > vp3d;
-      edm::Wrapper<std::vector<std::vector<std::vector<math::XYZTLorentzVector> > > > wvp3d;
+      std::vector<std::vector<std::vector<math::XYZTLorentzVectorF> > > vp3d;
+      edm::Wrapper<std::vector<std::vector<std::vector<math::XYZTLorentzVectorF> > > > wvp3d;
       
       TString s;
       edm::Wrapper<TString> ws;
