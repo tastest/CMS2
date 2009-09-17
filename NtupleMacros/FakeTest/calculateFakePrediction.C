4fb6b9f579261e8b04dc44fb447754249811fbd0
olivito <olivito>
2009-09-17 21:47:10 +0000

    This commit was manufactured by cvs2svn to create tag 'V02-00-06'.

--- NtupleMacros/FakeTest/calculateFakePrediction.C
+++
@@ -1,7 +0,0 @@
-#include <iomanip>
-#include <iostream>
-void calculateFakePrediction(double nFakeables, double nFakeables_error, double fakerate, double fakerate_error) {
-double prediction = nFakeables * fakerate;
-double prediction_error = sqrt(nFakeables*nFakeables*fakerate_error*fakerate_error+fakerate*fakerate*nFakeables_error*nFakeables_error);
-cout << "Fake prediction: " << fixed << setprecision(1) << prediction << "+-" << prediction_error << endl;
-}
--- NtupleMacros/HybridLooper/setup.sh
+++
@@ -1,3 +0,0 @@
-export CMS2_LOCATION=`echo $PWD | sed 's/NtupleMacros\/HybridLooper//'`
-export CMS2_NTUPLE_LOCATION=/store/disk01/
-
