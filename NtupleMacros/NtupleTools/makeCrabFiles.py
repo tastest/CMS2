58f61a60be71b2d71d66defd8d83a3c730ed83a8
slava77 <slava77>
2009-10-05 01:55:18 +0000

    blacklist all french t2

--- NtupleMacros/NtupleTools/makeCrabFiles.py
+++ NtupleMacros/NtupleTools/makeCrabFiles.py
@@ -46,9 +46,13 @@ def makeCrabConfig():
         outFile.write('##UCSD \n')
 #        outFile.write('CE_white_list = T2_US_UCSD\n')
 #        outFile.write('SE_white_list = T2_US_UCSD\n')
+        outFile.write('CE_black_list = T2_FR_CCIN2P3\n')
         outFile.write('SE_black_list = T2_FR_CCIN2P3\n')
+        outFile.write('CE_black_list = T2_FR_GRIF_IRFU\n')
         outFile.write('SE_black_list = T2_FR_GRIF_IRFU\n')
+        outFile.write('CE_black_list = T2_FR_GRIF_LLR\n')
         outFile.write('SE_black_list = T2_FR_GRIF_LLR\n')
+        outFile.write('CE_black_list = T2_FR_IPHC\n')
         outFile.write('SE_black_list = T2_FR_IPHC\n')
 
         outFile.write('rb                 = CERN\n')
