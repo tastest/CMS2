2e5cd23dfb501b17762cc4d28eee8ba72cdaeda5
dlevans <dlevans>
2009-11-11 19:05:03 +0000

    first commit of new data source bits for use in HistogramUtilities with the new samples.  Despite how it sounds this is actually useful

--- NtupleMacros/Tools/DataSource.cc
+++ NtupleMacros/Tools/DataSource.cc
@@ -1,29 +1,48 @@
 
 #include "DataSource.h"
 
-DataSource fH_WW() 	{ return DataSource("ww", 	H_WW); 		}
-DataSource fH_TTBAR() 	{ return DataSource("ttbar", 	H_TTBAR); 	}
+// 7 TeV V02-00-12
+
 DataSource fH_DYMM() 	{ return DataSource("dymm", 	H_DYMM);	}
 DataSource fH_DYEE() 	{ return DataSource("dyee", 	H_DYEE);	}
 DataSource fH_DYTT() 	{ return DataSource("dytt", 	H_DYTT); 	}
-DataSource fH_WJETS() 	{ return DataSource("wjets", 	H_WJETS); 	}
-DataSource fH_TW()	{ return DataSource("tw", 	H_TW); 		}
+
+DataSource fH_TTBAR()   { return DataSource("ttbar",    H_TTBAR);       }
 DataSource fH_ZZ() 	{ return DataSource("zz", 	H_ZZ); 		}
 DataSource fH_WZ() 	{ return DataSource("wz", 	H_WZ); 		}
+DataSource fH_WW()      { return DataSource("ww",       H_WW);          }
 
-DataSource fH_EM30_80()      { return DataSource("em30_80",       H_EM30_80);          }
-DataSource fH_BC30_80()      { return DataSource("bc30_80",       H_BC30_80);          }
-DataSource fH_WENU()      { return DataSource("wenu",       H_WENU);          }
+DataSource fH_BC20_30()      { return DataSource("QCDBCtoEPt20to30",       H_BC20_30);          }
+DataSource fH_BC30_80()      { return DataSource("QCDBCtoEPt30to80",       H_BC30_80);          }
+DataSource fH_BC80_170()      { return DataSource("QCDBCtoEPt80to170",       H_BC80_170);          }
+DataSource fH_BC30_170()      { return DataSource("QCDBCtoEPt30to170",       H_BC30_170);          }
+DataSource fH_BC20_170()      { return DataSource("QCDBCtoEPt20to170",       H_BC20_170);          }
 
-DataSource fH_QCD30()      { return DataSource("QCDpt30",       H_QCD30, 14);          }
+DataSource fH_EM20_30()      { return DataSource("QCDEMenrichedPt20to30",       H_EM20_30);          }
+DataSource fH_EM30_80()      { return DataSource("QCDEMenrichedPt30to80",       H_EM30_80);          }
+DataSource fH_EM80_170()      { return DataSource("QCDEMenrichedPt80to170",       H_EM80_170);          }
+DataSource fH_EM30_170()      { return DataSource("QCDEMenrichedPt30to170",       H_EM30_170);          }
+DataSource fH_EM20_170()      { return DataSource("QCDEMenrichedPt20to170",       H_EM20_170);          }
+
+DataSource fH_WENU()      { return DataSource("we",       H_WENU);          }
+DataSource fH_WMUNU()      { return DataSource("wm",       H_WMUNU);          }
+DataSource fH_WTAUNU()      { return DataSource("wt",       H_WTAUNU);          }
+
+DataSource fH_QCD30()      { return DataSource("QCDpt30",       H_QCD30);          }
+DataSource fH_MU30()       { return DataSource("InclusiveMuPt15",       H_MU30);          };
+
+// 7 TeV V02-00-08 - probably only used by DLE
+DataSource fH_WENU_7TeV() { return DataSource("wenu", H_WENU_7TeV); }
+DataSource fH_QCD30_7TeV() { return DataSource("qcd_pt30", H_QCD30_7TeV); }
+DataSource fH_PHOTONJET_7TeV() { return DataSource("photonjet", H_PHOTONJET_7TeV); }
+
+// old stuff?
+DataSource fH_TW()      { return DataSource("tw",       H_TW);          }
+DataSource fH_WJETS()   { return DataSource("wjets",    H_WJETS);       }
+DataSource fH_MU15_SINGLE() { return DataSource("InclusiveMuPt15", H_MU15_SINGLE, 28); }
 DataSource fH_QCD80()      { return DataSource("QCDpt80",       H_QCD80, 17);          }
 DataSource fH_WJET_ALP()   { return DataSource("wjetsAlpgen", H_WJET_ALP, 40); }
 DataSource fH_ZEEJET_ALP()   { return DataSource("dyeeAlpgen", H_ZEEJET_ALP, 42); }
 DataSource fH_ZMMJET_ALP()   { return DataSource("dymmAlpgen", H_ZMMJET_ALP, 44); }
 DataSource fH_ZTTJET_ALP()   { return DataSource("dyttAlpgen", H_ZTTJET_ALP, 46); }
-
-DataSource fH_MU15_SINGLE() { return DataSource("InclusiveMuPt15", H_MU15_SINGLE, 28); }
-
-DataSource fH_WENU_7TeV()      { return DataSource("wenu",       H_WENU_7TeV);          }
-
 
--- NtupleMacros/Tools/DataSource.h
+++ NtupleMacros/Tools/DataSource.h
@@ -9,20 +9,38 @@
 typedef UInt_t sources_t;
 
 enum {
-        H_WW      ,
+
         H_TTBAR   ,
         H_DYMM    ,
         H_DYEE    ,
         H_DYTT    ,
-        H_WJETS   ,
-        H_TW      ,
+
         H_ZZ      ,
         H_WZ,
+        H_WW      ,
+
 	H_WENU,
+	H_WMUNU,
+	H_WTAUNU,
+
+        H_EM20_30,
 	H_EM30_80,
-	H_BC30_80,
+	H_EM80_170,
+	H_EM20_170,
+	H_EM30_170,
+
+        H_BC20_30,
+        H_BC30_80,
+        H_BC80_170,
+        H_BC20_170,
+        H_BC30_170,
+
 	H_QCD30,
         H_QCD80,
+	H_MU30,
+
+        H_TW      ,
+        H_WJETS   ,
 	H_WJET_ALP,
 	H_ZEEJET_ALP,
         H_ZMMJET_ALP,
@@ -30,7 +48,8 @@ enum {
 	H_MU15_SINGLE,
 
 	H_WENU_7TeV,
-
+	H_QCD30_7TeV,
+	H_PHOTONJET_7TeV,
 };
 
 const static sources_t sources_all =
@@ -88,27 +107,51 @@ class DataSource {
 
 };
 
-DataSource fH_WW();
-DataSource fH_TTBAR();
+// consistent set of 7 TeV ntuples
+// are available with V02-00-12
+
 DataSource fH_DYMM();
 DataSource fH_DYEE();
 DataSource fH_DYTT();
-DataSource fH_WJETS();
-DataSource fH_TW();
+
+DataSource fH_TTBAR(); 
 DataSource fH_ZZ();
 DataSource fH_WZ();
+DataSource fH_WW();
 
 DataSource fH_WENU();
+DataSource fH_WMUNU();
+DataSource fH_WTAUNU();
+
+DataSource fH_EM20_30();
 DataSource fH_EM30_80();
+DataSource fH_EM80_170();
+DataSource fH_EM30_170();
+DataSource fH_EM20_170();
+
+DataSource fH_BC20_30();
 DataSource fH_BC30_80();
+DataSource fH_BC80_170();
+DataSource fH_BC30_170();
+DataSource fH_BC20_170();
 
 DataSource fH_QCD30();
+DataSource fH_MU30();
+
+// these were used with V02-00-08 7 TeV ntuples
+// will leave them here for the time being...
+DataSource        fH_WENU_7TeV();
+DataSource        fH_QCD30_7TeV();
+DataSource        fH_PHOTONJET_7TeV();
+
+// old?
+DataSource fH_WJETS();
+DataSource fH_TW();
 DataSource fH_QCD80();
 DataSource fH_WJET_ALP();
 DataSource fH_ZEEJET_ALP();
 DataSource fH_ZMMJET_ALP();
 DataSource fH_ZTTJET_ALP();
-
 DataSource fH_MU15_SINGLE();
 
 DataSource fH_WENU_7TeV();
