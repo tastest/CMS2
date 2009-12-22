07050d9f23324e342d3955a491cab352ee7c8d6a
olivito <olivito>
2009-12-22 18:56:40 +0000

    This commit was manufactured by cvs2svn to create branch 'jmu_20100108'.

--- NtupleMaker/python/jptMaker_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-jptMaker = cms.EDFilter("JPTMaker",
-                        jptInputTag       = cms.InputTag("JetPlusTrackZSPCorJetAntiKt5")
-)
--- NtupleMaker/python/trkJetSequence_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from RecoJets.JetProducers.TracksForJets_cff import *
-from RecoJets.JetProducers.ak5TrackJets_cfi import *
-
-cms2TrkJetSequence = cms.Sequence( tracksForJets * ak5TrackJets)
