231784711686f3f1710e9f46cc75f31334241450
dlevans <dlevans>
2010-01-12 13:39:01 +0000

    adding reco flag of hottest crystal in a tower for 3_3_X branch

--- NtupleMaker/src/CaloTowerMaker.cc
+++ NtupleMaker/src/CaloTowerMaker.cc
@@ -30,12 +30,16 @@ Implementation:
 
 //#include "DataFormats/CaloRecHit/interface/CaloID.h"
 //#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "RecoEcal/EgammaCoreTools/interface/EcalClusterTools.h"
+#include "DataFormats/EcalDetId/interface/EcalSubdetector.h"
 #include "Geometry/Records/interface/CaloGeometryRecord.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/DetId/interface/DetId.h"
 //#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
 #include "DataFormats/Common/interface/Ref.h"
+#include "Geometry/CaloTopology/interface/CaloTopology.h"
+#include "Geometry/CaloEventSetup/interface/CaloTopologyRecord.h"
 
 typedef math::XYZTLorentzVector LorentzVector;
 typedef math::XYZPoint Point;
@@ -204,13 +208,26 @@ CaloTowerMaker::CaloTowerMaker(const edm::ParameterSet& iConfig)
   produces<std::vector<unsigned int> >("twrsnumRecoveredHcalCells").setBranchAlias("twrs_numRecoveredHcalCells");
   //  uint numProblematicHcalCells() const { return ((twrStatusWord_ >> 21) & 0x7); }
   produces<std::vector<unsigned int> >("twrsnumProblematicHcalCells").setBranchAlias("twrs_numProblematicHcalCells");
+
+  // the recoflag of the max energy crystal in the tower
+  produces<std::vector<int> >("twrsemMaxRecoFlag").setBranchAlias("twrs_emMaxRecoFlag");
+  // the energy of the max energy crystal in the tower
+  produces<std::vector<float> >("twrsemMax").setBranchAlias("twrs_emMax");
+  // the energy in 3x3 crystals centred on the max energy crystal
+  produces<std::vector<float> >("twrsem3x3").setBranchAlias("twrs_em3x3");
+  // as above for 5x5 crystals
+  produces<std::vector<float> >("twrsem5x5").setBranchAlias("twrs_em5x5");
+  //number of crystals 
+  produces<std::vector<int> >("twrsnumCrystals").setBranchAlias("twrs_numCrystals");
   
   // add superclusters to the ntuple if they have ET > scEtMin_
   //   scEtMin_ = iConfig.getParameter<double>("scEtMin");
 
-// input Tags
+   // input Tags
    primaryVertexInputTag_ = iConfig.getParameter<edm::InputTag>("primaryVertexInputTag");
    caloTowersInputTag_ = iConfig.getParameter<edm::InputTag>("caloTowersInputTag");
+   ecalRecHitsInputTag_EE_ = iConfig.getParameter<edm::InputTag>("ecalRecHitsInputTag_EE");
+   ecalRecHitsInputTag_EB_ = iConfig.getParameter<edm::InputTag>("ecalRecHitsInputTag_EB");
 
   // initialise this
   //  cachedCaloGeometryID_ = 0;
@@ -219,6 +236,11 @@ CaloTowerMaker::CaloTowerMaker(const edm::ParameterSet& iConfig)
 
 void CaloTowerMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
+
+   // calo topology
+   edm::ESHandle<CaloTopology> pTopology;
+   iSetup.get<CaloTopologyRecord>().get(pTopology);
+   topology_ = pTopology.product();
 
 //   // get the calo geometry
 //   if (cachedCaloGeometryID_ != iSetup.get<CaloGeometryRecord>().cacheIdentifier()) {
@@ -248,13 +270,21 @@ void CaloTowerMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
     //    return ;
   }
 
+  // get hits
+  edm::Handle<EcalRecHitCollection> rhcHandleEE;
+  iEvent.getByLabel(ecalRecHitsInputTag_EE_, rhcHandleEE);
+  const EcalRecHitCollection *recHitsEE = rhcHandleEE.product();
+
+  edm::Handle<EcalRecHitCollection> rhcHandleEB;
+  iEvent.getByLabel(ecalRecHitsInputTag_EB_, rhcHandleEB);
+  const EcalRecHitCollection *recHitsEB = rhcHandleEB.product();
+
+  // ecal cluster shape variables
+  // do not use the lazy tools because need to get the hits anyway
+  EcalClusterTools clusterTools;
 
 //   // get hoe variable
 //   HoECalculator hoeCalc(caloGeometry_);
-
-//   // ecal cluster shape variables
-//   EcalClusterLazyTools lazyTools(iEvent, iSetup,
-// 				 ecalRecHitsInputTag_EB_, ecalRecHitsInputTag_EE_);
 
   std::auto_ptr<unsigned int> evt_ntwrs (new unsigned int);
 
@@ -286,6 +316,12 @@ void CaloTowerMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   std::auto_ptr<std::vector<unsigned int> > vector_twrs_numBadHcalCells (new std::vector<unsigned int>);
   std::auto_ptr<std::vector<unsigned int> > vector_twrs_numRecoveredHcalCells (new std::vector<unsigned int>);
   std::auto_ptr<std::vector<unsigned int> > vector_twrs_numProblematicHcalCells (new std::vector<unsigned int>);
+
+  std::auto_ptr<std::vector<int> > vector_twrs_emMaxRecoFlag      (new std::vector<int>);
+  std::auto_ptr<std::vector<float> > vector_twrs_emMax      (new std::vector<float>);
+  std::auto_ptr<std::vector<float> > vector_twrs_em3x3      (new std::vector<float>);
+  std::auto_ptr<std::vector<float> > vector_twrs_em5x5      (new std::vector<float>);
+  std::auto_ptr<std::vector<int>   > vector_twrs_numCrystals(new std::vector<int>);
 
   *evt_ntwrs = 0;
 
@@ -326,6 +362,59 @@ void CaloTowerMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
     vector_twrs_numRecoveredHcalCells->push_back(j->numRecoveredHcalCells());
     vector_twrs_numProblematicHcalCells->push_back(j->numProblematicHcalCells());
 
+    // find the detids of the crystals in this calo tower
+    const std::vector<DetId> &towerDetIds = j->constituents(); 
+    float emMax = 0.0;
+    float emE = 0.0;
+    DetId emMaxId(0);
+    int recoFlag = -1;
+    EcalRecHitCollection::const_iterator emHit;
+
+    // loop on detids in the tower
+    for (size_t i = 0; i < towerDetIds.size(); ++i) {
+
+      // find the energy of this detId if it is in the ecal
+      if (towerDetIds[i].subdetId() == EcalEndcap) {
+	 emHit = recHitsEE->find(towerDetIds[i]);
+         emE = clusterTools.recHitEnergy(towerDetIds[i], recHitsEE);
+      }
+      if (towerDetIds[i].subdetId() == EcalBarrel) { 
+	 emHit = recHitsEB->find(towerDetIds[i]);
+         emE = clusterTools.recHitEnergy(towerDetIds[i], recHitsEB);
+      }
+      // compare with previous highest energy
+      if (emE > emMax) {
+	   if ((emHit != recHitsEE->end() && towerDetIds[i].subdetId() == EcalEndcap) ||
+		(emHit != recHitsEB->end() && towerDetIds[i].subdetId() == EcalBarrel)) {
+	    		recoFlag = emHit->recoFlag();
+	   }
+           emMax = emE; 
+	   emMaxId = towerDetIds[i];
+        }
+    }
+
+    // get the e3x3 and e5x5 centred on this detid
+    // - this will include crystals not in the tower if necessary
+    float em3x3;
+    float em5x5;
+    // the function to compute the matrix energy takes a basic cluster as an argument
+    // but uses nothing from it!?!
+    reco::BasicCluster dummyCluster;
+    if (emMaxId.subdetId() == EcalEndcap) {
+        em3x3 = clusterTools.matrixEnergy(dummyCluster, recHitsEE, topology_, emMaxId, -1, 1, -1, 1);
+        em5x5 = clusterTools.matrixEnergy(dummyCluster, recHitsEE, topology_, emMaxId, -2, 2, -2, 2);
+    }
+    if (emMaxId.subdetId() == EcalBarrel) {
+        em3x3 = clusterTools.matrixEnergy(dummyCluster, recHitsEB, topology_, emMaxId, -1, 1, -1, 1);
+        em5x5 = clusterTools.matrixEnergy(dummyCluster, recHitsEB, topology_, emMaxId, -2, 2, -2, 2);
+    }
+
+    vector_twrs_emMaxRecoFlag->push_back(recoFlag);
+    vector_twrs_emMax->push_back(emMax);
+    vector_twrs_em3x3->push_back(em3x3);
+    vector_twrs_em5x5->push_back(em5x5);
+    vector_twrs_numCrystals->push_back(j->numCrystals());
+    
   }
 
 // 	vector_scs_sigmaEtaEta->push_back( sqrt(covariances[0]) );
@@ -359,6 +448,12 @@ void CaloTowerMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   iEvent.put(vector_twrs_numBadHcalCells, "twrsnumBadHcalCells");
   iEvent.put(vector_twrs_numRecoveredHcalCells, "twrsnumRecoveredHcalCells");
   iEvent.put(vector_twrs_numProblematicHcalCells, "twrsnumProblematicHcalCells");
+
+  iEvent.put(vector_twrs_emMaxRecoFlag, "twrsemMaxRecoFlag");
+  iEvent.put(vector_twrs_emMax, "twrsemMax");
+  iEvent.put(vector_twrs_em3x3, "twrsem3x3");
+  iEvent.put(vector_twrs_em5x5, "twrsem5x5");
+  iEvent.put(vector_twrs_numCrystals, "twrsnumCrystals");
 
 }
 
