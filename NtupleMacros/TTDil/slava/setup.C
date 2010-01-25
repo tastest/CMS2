43745fa6eb75c599f89d8538a222136f1331b5d2
slava77 <slava77>
2010-01-25 22:52:18 +0000

    use minifwlite, compile, fewwer choices to look for includes

--- NtupleMacros/TTDil/slava/setup.C
+++ NtupleMacros/TTDil/slava/setup.C
@@ -1,5 +1,6 @@
 // Loads a bunch of tools in interpretative mode
 void setup(bool skipFWLite = false){
+  gSystem->Load("libMiniFWLite");
   if (!skipFWLite){
     gSystem->Load("libFWCoreFWLite");
     AutoLibraryLoader::enable();
@@ -10,25 +11,27 @@ void setup(bool skipFWLite = false){
   
   // Load and compile something to allow proper treatment of vectors
   // Not clear that it is needed
+  gSystem->AddIncludePath("-O3  -w -I./ -I../../");
+
   gSystem->CompileMacro("loader.C", "++k", "libloader");
 
   gSystem->CompileMacro("getMyHistosNames.C", "++k", "libgetMyHistosNames");
   gSystem->CompileMacro("histtools.C", "++k", "libhisttools");
   gSystem->CompileMacro("browseStacks.C", "++k", "libbrowseStacks");
+  gSystem->CompileMacro("CORE/CMS2.cc", "++k", "libCMS2");
 
   gROOT->ProcessLine(".L tdrstyle.C");
   setTDRStyle();
 
   std::string cms2Location = "";
-  if (getenv("CMS2_LOCATION") ) cms2Location = Form("%s/NtupleMacros", getenv("CMS2_LOCATION"));
-  else if (1>2){
-    std::cout<<"CMS2_LOCATION is not set: it should point to what you where a nominal /pathOrRelativePath/CMS2 is"<<std::endl;
-    if (getenv("CMSSW_BASE")){
-      cms2Location = Form("%s/src/CMS2/NtupleMacros", getenv("CMSSW_BASE"));
-      std::cout<<"You have CMSSW_BASE -- will try "<<cms2Location.c_str()<<std::endl;
-    }
-  }
+  //  if (getenv("CMS2_LOCATION") ) cms2Location = Form("%s/NtupleMacros", getenv("CMS2_LOCATION"));
+  //  else if (1>2){
+  //    std::cout<<"CMS2_LOCATION is not set: it should point to what you where a nominal /pathOrRelativePath/CMS2 is"<<std::endl;
+  //    if (getenv("CMSSW_BASE")){
+  //      cms2Location = Form("%s/src/CMS2/NtupleMacros", getenv("CMSSW_BASE"));
+  //      std::cout<<"You have CMSSW_BASE -- will try "<<cms2Location.c_str()<<std::endl;
+  //    }
+  //  }
 
   //punch in more guesses where NtupleMacros are -- don't keep multiple copies ;)
-  gSystem->AddIncludePath(Form(" -w -I./ -I../../" ));
 }
