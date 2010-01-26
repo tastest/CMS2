fa11fce628d00d54c0f2bbe31299931c7c04a2f8
slava77 <slava77>
2010-01-26 00:46:42 +0000

    more parameters to the command line

--- NtupleMacros/TTDil/slava/histscripts/makeAllPlots.C
+++ NtupleMacros/TTDil/slava/histscripts/makeAllPlots.C
@@ -1,21 +1,23 @@
-void makeAllPlots(char* fname, bool logScale=false,char* hPatt = 0, char* refName, int titleStyle = 1, bool noLegend = false, char* bsmName=0){
+void makeAllPlots(char* fname, bool logScale=false,char* hPatt = 0, char* refName=0, 
+		  int titleStyle = 1, int colorStyle=0, int orderStyle=0,
+		  bool noLegend = false, char* bsmName=0){
   gROOT->SetStyle("Plain");
+  ROOT->gErrorIgnoreLevel = 0;
   gROOT->ProcessLine(".x setup.C(true)"); //don't need FWLite to make plots
   hist::loadHist(fname, 0, hPatt);
   
   if (refName!=0){
     hist::loadHist(refName, "ref", hPatt);
   }  
-
   browseStacks(true,                         //makePictures
 	       false,                        //wait
 	       titleStyle,                   //addHistName
 	       1.1,                          // maxYrescale
 	       logScale,                     // logScale
 	       logScale ? false : true,      // set min to 0
-	       0,//mod                            // color scheme (0 original tas, 1 as in pas 09-002, 2 almost like 1 from the wheel
+	       colorStyle,//mod              // color scheme (0 original tas, 1 as in pas 09-002, 2 almost like 1 from the wheel
 	       noLegend,                     // no legend on the plot
-	       0,//mod                             // order scheme
+	       orderStyle,//mod              // order scheme
 	       bsmName);
   gSystem->Exit(0);
 }
