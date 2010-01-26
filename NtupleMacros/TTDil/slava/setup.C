5ca44a68518a7ca2eab268a55136af8dff290e08
slava77 <slava77>
2010-01-26 18:30:51 +0000

    compile it all here

--- NtupleMacros/TTDil/slava/setup.C
+++ NtupleMacros/TTDil/slava/setup.C
@@ -15,7 +15,8 @@ void setup(bool skipFWLite = false){
   gSystem->CompileMacro("histscripts/getMyHistosNames.C", "++k", "libgetMyHistosNames");
   gSystem->CompileMacro("histscripts/histtools.C", "++k", "libhisttools");
   gSystem->CompileMacro("histscripts/browseStacks.C", "++k", "libbrowseStacks");
-
+  gSystem->CompileMacro("CORE/CMS2.cc", "++k", "libCMS2");
+  gSystem->CompileMacro("ttDilCounts_looper.C", "++k", "libttDilCounts_looper");
   gROOT->ProcessLine(".L tdrstyle.C");
   setTDRStyle();
 
