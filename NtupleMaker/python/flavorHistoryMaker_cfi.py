0679e91b9e13bf20e49def9923d9f76a45da6164
olivito <olivito>
2010-01-28 19:53:33 +0000

    This commit was manufactured by cvs2svn to create tag 'V00-00-01'.

--- NtupleMaker/python/flavorHistoryMaker_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-flavorHistoryMaker = cms.EDFilter("FlavorHistoryMaker",                                  
-    flavorHistoryFilterTag  = cms.InputTag("flavorHistoryFilter")
-)
-
