ea75e0de4b46086bfd05ef60740e47bb88f376bd
olivito <olivito>
2010-01-28 22:24:06 +0000

    This commit was manufactured by cvs2svn to create tag 'Reproduce_2XX_SUSY_SS_100128'.

--- NtupleMaker/python/patMETMaker_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-patMETMaker = cms.EDFilter("PATMETMaker",
-                           patMETsInputTag = cms.InputTag("layer1METs")
-)
-
-
--- NtupleMaker/python/photonMaker_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-photonMaker = cms.EDFilter("PhotonMaker",
-    # Photon collection
-    photonsInputTag = cms.InputTag("photons"),
-)
-
