48638cb7b75eefead320d4267d7987c685714448
dlevans <dlevans>
2010-01-29 16:22:42 +0000

    update to plotting script

--- NtupleMacros/TTDil/dle/plot.C
+++ NtupleMacros/TTDil/dle/plot.C
@@ -1,5 +1,5 @@
 
-void plot(TString fileName, TString det)
+void plot(TString fileName, TString det, TString version)
 {
 
 	gROOT->ProcessLine(".L tdrStyle.C");
@@ -9,6 +9,13 @@ void plot(TString fileName, TString det)
 	TH1F *h1_numerator = (TH1F*)f.Get("ttbar_hyp_lt_" + det + "_pt_ee");
 	TH1F *h1_denom_old = (TH1F*)f.Get("ttbar_hyp_lt_" + det + "_pt_idold_ee");
         TH1F *h1_denom_new = (TH1F*)f.Get("ttbar_hyp_lt_" + det + "_pt_idnew_ee");
+
+        TH1F *h1_denom_iso_old = (TH1F*)f.Get("ttbar_hyp_lt_" + det + "_pt_isoold_ee");
+        TH1F *h1_denom_iso_new = (TH1F*)f.Get("ttbar_hyp_lt_" + det + "_pt_isonew_ee");
+
+	//
+	// id
+	//
 
 	TGraphAsymmErrors *gr_eff_old = new TGraphAsymmErrors();
 	gr_eff_old->BayesDivide(h1_denom_old, h1_numerator);
@@ -26,6 +33,27 @@ void plot(TString fileName, TString det)
 	gr_eff_new->SetMarkerColor(kBlue);
 	gr_eff_new->SetLineColor(kBlue);
 
+	//
+	// iso
+	//
+
+        TGraphAsymmErrors *gr_eff_iso_old = new TGraphAsymmErrors();
+        gr_eff_iso_old->BayesDivide(h1_denom_iso_old, h1_numerator);
+        gr_eff_iso_old->GetYaxis()->SetRangeUser(0, 1.1);
+        gr_eff_iso_old->GetYaxis()->SetTitle("Efficiency");
+        gr_eff_iso_old->GetXaxis()->SetTitle("LT p_{T} (GeV)");
+        gr_eff_iso_old->SetMarkerColor(kRed);
+        gr_eff_iso_old->SetLineColor(kRed);
+
+        TGraphAsymmErrors *gr_eff_iso_new = new TGraphAsymmErrors();
+        gr_eff_iso_new->BayesDivide(h1_denom_iso_new, h1_numerator);
+        gr_eff_iso_new->GetYaxis()->SetRangeUser(0, 1.1);
+        gr_eff_iso_new->GetYaxis()->SetTitle("Efficiency");
+        gr_eff_iso_new->GetXaxis()->SetTitle("LT p_{T} (GeV)");
+        gr_eff_iso_new->SetMarkerColor(kBlue);
+        gr_eff_iso_new->SetLineColor(kBlue);
+
+
 	TLegend *lg = new TLegend(0.5, 0.2, 0.9, 0.4);
 	lg->SetFillColor(kWhite);
 	lg->SetLineColor(kWhite);
@@ -38,7 +66,19 @@ void plot(TString fileName, TString det)
 	gr_eff_old->Draw("AP");
 	gr_eff_new->Draw("P");
 	lg->Draw();
-	c1->SaveAs("eff_mc_" + det + ".png");
+	c1->SaveAs("results/eff_mc" + version + "_" + det + ".png");
+
+
+	lg->Clear();
+        lg->AddEntry(gr_eff_iso_old, "relsusy_iso", "lp");
+        lg->AddEntry(gr_eff_iso_new, "relsusy_iso_cand01", "lp");
+
+        c1->cd();
+        gr_eff_iso_old->Draw("AP");
+        gr_eff_iso_new->Draw("P");
+        lg->Draw();
+        c1->SaveAs("results/effiso_mc" + version + "_" + det + ".png");
+
 
 }
 
