c582558ecd12f6b3e242bbddb4ce7d8949ff3f3a
dmytro <dmytro>
2010-01-29 17:20:19 +0000

    disabled fwlite for mac needs

--- NtupleMacros/WW/rootlogon.C
+++ NtupleMacros/WW/rootlogon.C
@@ -2,9 +2,9 @@
    gROOT->SetStyle("Plain");
    gStyle->SetPalette(1);
    gStyle->SetOptStat("nemruoi");
-   cout << "loading..." <<endl;
-   gSystem->Load("libCintex");
-   Cintex::Enable();
-   gSystem->Load("libFWCoreFWLite");
-   AutoLibraryLoader::enable();
+   // cout << "loading..." <<endl;
+   // gSystem->Load("libCintex");
+   // Cintex::Enable();
+   // gSystem->Load("libFWCoreFWLite");
+   // AutoLibraryLoader::enable();
 }
