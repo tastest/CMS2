4a826ef8a6e7bafe2c8b7f20b631dd1dbd26989c
dlevans <dlevans>
2010-02-02 13:34:00 +0000

    updates to niceness of looper

--- NtupleMacros/TTDil/dle/doAll3X.C
+++ NtupleMacros/TTDil/dle/doAll3X.C
@@ -1,5 +1,4 @@
 
-
 void doAll3X() {
 
 	// from slavas code
@@ -19,37 +18,54 @@ void doAll3X() {
 	gROOT->ProcessLine(".L ../../CORE/electronSelections.cc+");
 	gROOT->ProcessLine(".L ../../CORE/utilities.cc+");
 	gROOT->ProcessLine(".L ../../CORE/selections.cc+");
+	gROOT->ProcessLine(".L ../histtools.C+");
 
 	//
 	// the looper
 	//
-	gROOT->ProcessLine(".L MyScanChain.C+");
+	gROOT->ProcessLine(".L MyScanChain.cc+");
 
 	//
 	// output file for histograms
 	//
-	TFile f_mc("histos_mc_3x.root", "RECREATE");
-	f_mc.cd();
+
+	MyScanChain *looper = new MyScanChain();
 
 	//
 	// chains for input files
-	//
+	// 
+	// SM
 	// ttbar
 	TChain *chain_ttbar = new TChain("Events");
 	chain_ttbar->Add("/store/disk02/cms2/TTbar_Summer09-MC_31X_V3_7TeV-v1/V03-00-34/merged_ntuple*.root");
 
+	// BSM
+	// LM0
+	TChain *chain_lm0 = new TChain("Events");
+	chain_lm0->Add("/store/disk02/cms2/LM0_Summer09-MC_31X_V3_7TeV-v1/V03-00-35/merged_ntuple*.root");
+
 	// 
 	// do looping
 	//
-	// ttbar
-	ScanChain(false, "ttbar", chain_ttbar);
+
+	looper->ScanChain(false, "ttbar", chain_ttbar);
+
+	looper->ScanChain(false, "lm0", chain_lm0);
 
 	//
 	// write histograms
 	// 
 
-	f_mc.Write();
-	f_mc.Close();
+	const char* outFile = "histos_mc_3x.root";
+	hist::saveHist(outFile); 
+	hist::deleteHistos();
 
+	//
+	// tidy up
+	//
+	delete looper;
+
+	delete chain_ttbar;
+	delete chain_lm0;
 }
 
