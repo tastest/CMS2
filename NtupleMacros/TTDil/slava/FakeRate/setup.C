b9a052e74b442b73806e06109477f1cd26a6962d
kalavase <kalavase>
2010-02-03 06:53:32 +0000

    added local copy of setup.C

---
+++ NtupleMacros/TTDil/slava/FakeRate/setup.C
@@ -0,0 +1,17 @@
+// Loads a bunch of tools in interpretative mode
+void setup(){
+  
+  gSystem->Load("libGui.so");
+  gSystem->Load("libPhysics.so");
+  
+ 
+  // Load and compile something to allow proper treatment of vectors
+  // Not clear that it is needed
+  gSystem->CompileMacro("../loader.C", "++k", "libloader");
+  
+  gSystem->CompileMacro("../histscripts/getMyHistosNames.C", "++k", "libgetMyHistosNames");
+  gSystem->CompileMacro("../histscripts/histtools.C", "++k", "libhisttools");
+  gSystem->CompileMacro("../histscripts/browseStacks.C", "++k", "libbrowseStacks");
+  gSystem->CompileMacro("../CORE/CMS2.cc", "++k", "libCMS2");
+
+}
