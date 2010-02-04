a68e820a50ecb3a81216f174f45e44590c016833
dlevans <dlevans>
2010-02-04 17:29:50 +0000

    a working version of the dy estimate code - I think

--- NtupleMacros/TTDil/dle/anascripts/processDYEstResults.cc
+++ NtupleMacros/TTDil/dle/anascripts/processDYEstResults.cc
@@ -1,31 +1,53 @@
 
 #include "processDYEstResults.h"
-#include "../../../Tools/AllDataSources.h"
 
 #include "TROOT.h"
 #include "TStyle.h"
 
+TH1F* getRHist(HistogramUtilities *hUtil, sources_t theSource, TString nJets, TString hyp_type)
+{
 
-const static sources_t theSources = 
-    (1ll << H_TTBAR) |
-    (1ll << H_WW) |
-    (1ll << H_WZ) |
-    (1ll << H_ZZ);
+	TH1F *h1_met_in = hUtil->getHistogram(theSource, "dyest_met_in", nJets, "_" + hyp_type);
+    TH1F *h1_met_out = hUtil->getHistogram(theSource, "dyest_met_out", nJets, "_" + hyp_type);
+	h1_met_in->Rebin(5);
+	h1_met_out->Rebin(5);
 
+	TH1F *h1_R = (TH1F*)h1_met_in->Clone();
+	h1_R->Reset();
 
+	for (Int_t i = 0; i < h1_met_in->GetNbinsX(); ++i)
+	{
+		Float_t nOut = h1_met_out->Integral(i, h1_met_out->GetNbinsX() + 1);
+		Float_t nIn = h1_met_in->Integral(i, h1_met_in->GetNbinsX() + 1);
+		Float_t err2_nIn = Utilities::getError2(h1_met_in, i, h1_met_in->GetNbinsX() + 1);
+		Float_t err2_nOut = Utilities::getError2(h1_met_out, i, h1_met_in->GetNbinsX() + 1);
 
+		Float_t R = 0;
+		Float_t err = 0;
 
-const static int metCutBin = 5;
+		if (nIn != 0 && nOut != 0) {
+			R = nOut/nIn;
+			err = sqrt( R*R*(err2_nOut/(nOut*nOut)) + (err2_nIn/(nIn*nIn)) );
+			h1_R->SetBinContent(i, R);
+			h1_R->SetBinError(i, err);
+		}
+
+	}
+
+	h1_R->GetXaxis()->SetRangeUser(20, 100);
+	delete h1_met_in;
+	delete h1_met_out;
+	return h1_R;
+
+}
 
 void get_R(HistogramUtilities *hUtil, TString hyp_type, TString nJets, Float_t &R, Float_t &err2)
 {
 	sources_t theSource = (1ll << H_DYEE);
-	//DataSource theSource = fH_DYEE();
-	//if (hyp_type == "mm") theSource = fH_DYMM();
-    if (hyp_type == "mm") theSource = (1ll << H_DYMM);
+	if (hyp_type == "mm") theSource = (1ll << H_DYMM);
 
-	TH1F *h1_met_in = hUtil->getHistogram(theSource, "met_in", nJets, "_" + hyp_type);
-	TH1F *h1_met_out = hUtil->getHistogram(theSource, "met_out", nJets, "_" + hyp_type);
+	TH1F *h1_met_in = hUtil->getHistogram(theSource, "dyest_met_in", nJets, "_" + hyp_type);
+	TH1F *h1_met_out = hUtil->getHistogram(theSource, "dyest_met_out", nJets, "_" + hyp_type);
 
 	Float_t nOut = h1_met_out->Integral(metCutBin, h1_met_out->GetNbinsX() + 1);
 	Float_t nIn = h1_met_in->Integral(metCutBin, h1_met_in->GetNbinsX() + 1);
@@ -35,48 +57,44 @@ void get_R(HistogramUtilities *hUtil, TString hyp_type, TString nJets, Float_t &
 
 	Float_t err2_nIn = Utilities::getError2(h1_met_in, metCutBin, h1_met_in->GetNbinsX() + 1);
 	Float_t err2_nOut = Utilities::getError2(h1_met_out, metCutBin, h1_met_in->GetNbinsX() + 1);
-	//      if (nIn !=0 && nOut != 0) err2 = R*R*( (err2_nOut/(nOut*nOut)) + (err2_nIn/(nIn*nIn)) );
 	err2 = R*R;
 
 	std::cout << "err2_R = " << err2 << std::endl;
 	std::cout << "R = " << R << " $\\pm$ " << sqrt(err2) << " (" << nIn << ", " << nOut << ")" <<  std::endl;
 
+	delete h1_met_in;
+	delete h1_met_out;
+
 }
 
-/*
-void get_k(TString nJets, TString hyp_type, Float_t &k, Float_t &err2)
+void get_k(HistogramUtilities *hUtil, TString nJets, TString hyp_type, Float_t &k, Float_t &err2)
 {
 	Float_t dummyErr2;
 
 	// k_eff needs to be found from the no met histogram - first do "DY ONLY"
-	//HistogramUtilities *hUtil_kMC = new HistogramUtilities("DYEstResults_GetEMuEff_TRIG.root", 30.0, true, 1.0);
-	//HistogramUtilities *hUtil_k = new HistogramUtilities("DYEstResults_GetEMuEff_TRIG.root", 30.0, true, 0.1);
-    std::vector<DataSource> sources;
-    sources.push_back( fH_TTBAR() );
-    //sources.push_back( fH_WW()  );
-    //sources.push_back( fH_WZ() );
-    //sources.push_back( fH_ZZ() );
-    HistogramUtilities *hUtil_kMC = new HistogramUtilities("../histos_mc_3x.root", sources, 1.0);
-    HistogramUtilities *hUtil_k = new HistogramUtilities("../histos_mc_3x.root", sources, 1.0);
-
 
 	// get in and out (DY ONLY)
-	Float_t nMC_mm = hUtil_kMC->getN(true, sources_dy, nJets, "mm", dummyErr2);
-	Float_t nMC_ee = hUtil_kMC->getN(true, sources_dy, nJets, "ee", dummyErr2);
+	TH1F *h1_met_in_mm = hUtil->getHistogram((1ll<<H_DYMM), "dyest_met_in", nJets, "_mm");
+	TH1F *h1_met_in_ee = hUtil->getHistogram((1ll<<H_DYEE), "dyest_met_in", nJets, "_ee");
+
+	Float_t nMC_mm = h1_met_in_mm->Integral(0, metCutBin);
+	Float_t nMC_ee = h1_met_in_ee->Integral(0, metCutBin);
 	Float_t k_effMC = sqrt(nMC_mm/nMC_ee);
 	if (hyp_type == "ee") k_effMC = sqrt(nMC_ee/nMC_mm);
 	Float_t err2_k_effMC = (1/4.0)*(1/(nMC_ee)) + ((nMC_mm)/(nMC_ee*nMC_ee));
 	if (hyp_type == "ee") err2_k_effMC = (1/4.0)*(1/(nMC_mm)) + ((nMC_ee)/(nMC_mm*nMC_mm));
 
 	// get in and out (FROM DATA)
-	Float_t n_mm = hUtil_k->getN(true, theSources, nJets, "mm", dummyErr2);
-	Float_t n_ee = hUtil_k->getN(true, theSources, nJets, "ee", dummyErr2);
+	TH1F *h1_met_in_mm_data = hUtil->getHistogram(sources_all, "dyest_met_in", nJets, "_mm");
+	TH1F *h1_met_in_ee_data = hUtil->getHistogram(sources_all, "dyest_met_in", nJets, "_ee");
+
+	Float_t n_mm = h1_met_in_mm_data->Integral(0, metCutBin);
+	Float_t n_ee = h1_met_in_ee_data->Integral(0, metCutBin);
 	std::cout << n_mm << ", " << n_ee << std::endl;
 	Float_t k_eff = sqrt(n_mm/n_ee);
 	if (hyp_type == "ee") k_eff = sqrt(n_ee/n_mm);
 	Float_t err2_k_eff = (1/4.0)*(1/(n_ee)) + ((n_mm)/(n_ee*n_ee));
 	if (hyp_type == "ee") err2_k_eff = (1/4.0)*(1/(n_mm)) + ((n_ee)/(n_mm*n_mm));
-
 
 	std::cout       << std::endl;
 	std::cout       << "TABLE --- for comapring efficiency correction factors" << std::endl;
@@ -88,8 +106,10 @@ void get_k(TString nJets, TString hyp_type, Float_t &k, Float_t &err2)
 	printf("%.2f $\\pm$ %.4f \t&", k_effMC, sqrt(err2_k_effMC));
 	printf("%.2f $\\pm$ %.4f \\\\ \\hline \n", k_eff, sqrt(err2_k_eff));
 
-	delete hUtil_kMC;
-	delete hUtil_k;
+	delete h1_met_in_mm;
+	delete h1_met_in_ee;
+	delete h1_met_in_mm_data;
+	delete h1_met_in_ee_data;
 
 	// add the combinatorics factor
 	k = 0.5 * k_eff;
@@ -98,7 +118,7 @@ void get_k(TString nJets, TString hyp_type, Float_t &k, Float_t &err2)
 	printf("k including combinatorics = %.2f $\\pm$ %.4f \\\\ \\hline \n", k, sqrt(err2));
 
 }
-*/
+
 
 // input should be hyp_type and jet_bin
 
@@ -111,8 +131,8 @@ void estimate(HistogramUtilities *hUtil, JetBins_t bin, TString hyp_type,
 	if (bin == 1) nJets = "1j";
 	if (bin == 2) nJets = "2j";
 
-//	hUtil->printInOutTruth(nJets, hyp_type);
-//	hUtil->printInOutTruth(nJets, "em");
+	//	hUtil->printInOutTruth(nJets, hyp_type);
+	//	hUtil->printInOutTruth(nJets, "em");
 
 	// values and errors
 	Float_t n_in_data_em, 		err2_n_in_data_em;
@@ -125,25 +145,19 @@ void estimate(HistogramUtilities *hUtil, JetBins_t bin, TString hyp_type,
 	Float_t k, 			err2_k;
 	Float_t R,			err2_R;
 
-
-
-	TH1F *h1_met_in_data_em = hUtil->getHistogram(theSources, "dyest_met_in", nJets, "_em");	
+	TH1F *h1_met_in_data_em = hUtil->getHistogram(sources_all, "dyest_met_in", nJets, "_em");	
 	n_in_data_em = h1_met_in_data_em->Integral(metCutBin, h1_met_in_data_em->GetNbinsX() + 1);
-    err2_n_in_data_em = n_in_data_em;
-	//n_in_data_em = hUtil->getN(true, theSources, nJets, "em", err2_n_in_data_em);
+	err2_n_in_data_em = n_in_data_em;
 
-    TH1F *h1_met_in_data_ll = hUtil->getHistogram(theSources, "dyest_met_in", nJets, "_" + hyp_type);
-    n_in_data_ll = h1_met_in_data_ll->Integral(metCutBin, h1_met_in_data_ll->GetNbinsX() + 1);
-    err2_n_in_data_ll = n_in_data_ll;
-	//n_in_data_ll = hUtil->getN(true, theSources, nJets, hyp_type, err2_n_in_data_ll);
+	TH1F *h1_met_in_data_ll = hUtil->getHistogram(sources_all, "dyest_met_in", nJets, "_" + hyp_type);
+	n_in_data_ll = h1_met_in_data_ll->Integral(metCutBin, h1_met_in_data_ll->GetNbinsX() + 1);
+	err2_n_in_data_ll = n_in_data_ll;
 
 	std::cout << n_in_data_em << ", " << n_in_data_ll << std::endl;
 
 
 	get_R(hUtil, hyp_type, nJets, R, err2_R);
-
-/*
-	get_k(nJets, hyp_type, k, err2_k);
+	get_k(hUtil, nJets, hyp_type, k, err2_k);
 
 	// estimate the peaking background in the near-Z control region
 	n_in_est_nonpeak_ll		= k*n_in_data_em;
@@ -156,15 +170,22 @@ void estimate(HistogramUtilities *hUtil, JetBins_t bin, TString hyp_type,
 	err2_n_out_est_peaking_ll 	= n_out_est_peaking_ll*n_out_est_peaking_ll*((err2_R/(R*R)) 
 			+ (err2_n_in_est_peaking_ll/(n_in_est_peaking_ll*n_in_est_peaking_ll)));
 
-	// get the true peaking background in the out region
-	n_out_peaking_ll = hUtil->getN(false, sources_peaking, nJets, hyp_type, err2_n_out_peaking_ll);
-	// expected error for 100pb
-	//	err2_n_out_peaking_ll = n_out_peaking_ll;
 
-	// get the true non peaking background in the near-Z control region
-	n_in_nonpeak_ll = hUtil->getN(true, sources_nonpeaking, nJets, hyp_type, err2_n_in_nonpeak_ll);
-	// expected error for 100pb
-	//err2_n_in_nonpeak_ll = n_in_nonpeak_ll;
+	//
+	//        err2 = Utilities::getError2(h_temp, zLow_, zHigh_);
+	//
+
+	// get the true peaking background in the out region
+
+
+	TH1F *h1_met_out_peaking_ll = hUtil->getHistogram(sources_peaking, "dyest_met_out", nJets, "_" + hyp_type);
+	n_out_peaking_ll = h1_met_out_peaking_ll->Integral(metCutBin, h1_met_out_peaking_ll->GetNbinsX() + 1);
+	err2_n_out_peaking_ll = Utilities::getError2(h1_met_out_peaking_ll, metCutBin, h1_met_out_peaking_ll->GetNbinsX() + 1);
+
+	TH1F *h1_met_in_nonpeak_ll = hUtil->getHistogram(sources_nonpeak, "dyest_met_in", nJets, "_" + hyp_type);
+	n_in_nonpeak_ll = h1_met_in_nonpeak_ll->Integral(metCutBin, h1_met_in_nonpeak_ll->GetNbinsX() + 1);
+	err2_n_in_nonpeak_ll = Utilities::getError2(h1_met_in_nonpeak_ll, metCutBin, h1_met_in_nonpeak_ll->GetNbinsX() + 1);
+
 
 	// tables
 
@@ -240,7 +261,12 @@ void estimate(HistogramUtilities *hUtil, JetBins_t bin, TString hyp_type,
 	// fill nonpeaking background estimate results histograms
 	nonpeakResults.add(bin, n_in_nonpeak_ll, sqrt(err2_n_in_nonpeak_ll),
 			n_in_est_nonpeak_ll, sqrt(err2_n_in_est_nonpeak_ll));
-*/
+
+	delete h1_met_out_peaking_ll;
+	delete h1_met_in_nonpeak_ll;
+	delete h1_met_in_data_em;
+	delete h1_met_in_data_ll;
+
 }
 
 void processDYEstResults(TString fileName)
@@ -251,13 +277,12 @@ void processDYEstResults(TString fileName)
 	gStyle->SetOptTitle(1);
 
 	// filename, met cut, doWZ, lumiNorm
-	//	HistogramUtilities *hUtil = new HistogramUtilities(fileName, 20.0, true, 0.1);
-    std::vector<DataSource> sources;
-    sources.push_back( fH_TTBAR() );
-    //sources.push_back( fH_WW()  );
-    //sources.push_back( fH_WZ() );
-    //sources.push_back( fH_ZZ() );
-    HistogramUtilities *hUtil = new HistogramUtilities(fileName, sources, 1.0);
+	std::vector<DataSource> sources;
+	sources.push_back( fH_TTBAR() );
+	sources.push_back( fH_DYMM() );
+	sources.push_back( fH_DYEE() );
+	sources.push_back( fH_ZZ() );
+	HistogramUtilities *hUtil = new HistogramUtilities(fileName, sources, 1.0);
 
 
 	gROOT->cd();
@@ -268,23 +293,20 @@ void processDYEstResults(TString fileName)
 	ResultsHistograms estResults_mm(kYellow, "DY/ZZ/WZ (#mu#mu)", "n-Jets", "Number of Events");
 	ResultsHistograms nonpeakResults_mm(kRed, "Non-peaking (#mu#mu)", "n-Jets", "Number of Events");
 
-	
-	   estimate(hUtil, J0, "mm", estResults_mm, nonpeakResults_mm);
 
-/*
-	   estimate(hUtil, J1, "mm", estResults_mm, nonpeakResults_mm);
-	   estimate(hUtil, J2, "mm", estResults_mm, nonpeakResults_mm);
+	estimate(hUtil, J0, "mm", estResults_mm, nonpeakResults_mm);
+	estimate(hUtil, J1, "mm", estResults_mm, nonpeakResults_mm);
+	estimate(hUtil, J2, "mm", estResults_mm, nonpeakResults_mm);
 
-	   estimate(hUtil, J0, "ee", estResults_ee, nonpeakResults_ee);
-	   estimate(hUtil, J1, "ee", estResults_ee, nonpeakResults_ee);
-	   estimate(hUtil, J2, "ee", estResults_ee, nonpeakResults_ee);
+	estimate(hUtil, J0, "ee", estResults_ee, nonpeakResults_ee);
+	estimate(hUtil, J1, "ee", estResults_ee, nonpeakResults_ee);
+	estimate(hUtil, J2, "ee", estResults_ee, nonpeakResults_ee);
 
 	//TLegend *lg_estimate = new TLegend(0.2, 0.6, 0.6, 0.9);
-*/
+
 	TCanvas *c1 = (TCanvas*)estResults_mm.results(0.0, 5.0);
 	c1->Draw();
 
-/*
 	TCanvas *c2 = (TCanvas*)nonpeakResults_mm.results(0.0, 30.0);
 	c2->Draw();
 
@@ -294,18 +316,29 @@ void processDYEstResults(TString fileName)
 	TCanvas *c4 = (TCanvas*)nonpeakResults_ee.results(0.0, 30.0);
 	c4->Draw();
 
-	TH1F *h1_R_ZZ_ee = (TH1F*)hUtil->getRHist(fH_ZZ(), "0j", "ee");
+    TH1F *h1_R_DY_ee = getRHist(hUtil, (1ll<<H_DYEE), "0j", "ee");
+    h1_R_DY_ee->SetLineColor(kBlue);
+    h1_R_DY_ee->SetMarkerStyle(25);
+    TH1F *h1_R_DY_mm = getRHist(hUtil, (1ll<<H_DYMM), "0j", "mm");
+    h1_R_DY_mm->SetLineColor(kRed);
+    h1_R_DY_mm->SetMarkerStyle(27);
+
+    TCanvas *c5 = new TCanvas();
+    c5->cd();
+    h1_R_DY_ee->Draw("E1");
+    h1_R_DY_mm->Draw("SAME E1");
+
+	TH1F *h1_R_ZZ_ee = getRHist(hUtil, (1ll<<H_ZZ), "0j", "ee");
 	h1_R_ZZ_ee->SetLineColor(kBlue);
 	h1_R_ZZ_ee->SetMarkerStyle(20);
-	TH1F *h1_R_ZZ_mm = (TH1F*)hUtil->getRHist(fH_ZZ(), "0j", "mm");
+	TH1F *h1_R_ZZ_mm = getRHist(hUtil, (1ll<<H_ZZ), "0j", "mm");
 	h1_R_ZZ_mm->SetLineColor(kRed);
 	h1_R_ZZ_mm->SetMarkerStyle(22);
 
-	TCanvas *c5 = new TCanvas();
-	c5->cd();
+	TCanvas *c6 = new TCanvas();
+	c6->cd();
 	h1_R_ZZ_ee->Draw("E1");
 	h1_R_ZZ_mm->Draw("SAME E1");
-	 */
 
 	delete hUtil;
 
