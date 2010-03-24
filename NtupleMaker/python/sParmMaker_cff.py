5c41d97822e309a95af3265e356ba5104db85ef6
jribnik <jribnik>
2010-03-24 22:40:37 +0000

    First commit of the SParmMaker written by Benjamin Hooberman

---
+++ NtupleMaker/python/sParmMaker_cff.py
@@ -0,0 +1,5 @@
+import FWCore.ParameterSet.Config as cms
+
+from CMS2.NtupleMaker.sParmMaker_cfi import *
+
+sParmMakerSequence      = cms.Sequence( sParmMaker)
---
+++ NtupleMaker/python/sParmMaker_cfi.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+sParmMaker = cms.EDProducer("SParmMaker",
+                            sparm_m0InputTag        = cms.InputTag("susyScanM0"),
+                            sparm_m12InputTag       = cms.InputTag("susyScanM12"),
+                            sparm_AInputTag         = cms.InputTag("susyScanA"),
+                            sparm_muInputTag        = cms.InputTag("susyScanMu"),
+                            sparm_tanBetaInputTag   = cms.InputTag("susyScanTanBeta"),
+                            sparm_xsecInputTag      = cms.InputTag("susyScanXsec"),
+                            aliasPrefix             = cms.untracked.string("sparm")
+)
