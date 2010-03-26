f6677fd6afac971f50786432646d9b0155e96ef5
jribnik <jribnik>
2010-03-26 14:55:27 +0000

    Corrected InputTags for sparm_A and sparm_xsec

--- NtupleMaker/python/sParmMaker_cfi.py
+++ NtupleMaker/python/sParmMaker_cfi.py
@@ -3,9 +3,9 @@ import FWCore.ParameterSet.Config as cms
 sParmMaker = cms.EDProducer("SParmMaker",
                             sparm_m0InputTag        = cms.InputTag("susyScanM0"),
                             sparm_m12InputTag       = cms.InputTag("susyScanM12"),
-                            sparm_AInputTag         = cms.InputTag("susyScanA"),
+                            sparm_AInputTag         = cms.InputTag("susyScanA0"),
                             sparm_muInputTag        = cms.InputTag("susyScanMu"),
                             sparm_tanBetaInputTag   = cms.InputTag("susyScanTanBeta"),
-                            sparm_xsecInputTag      = cms.InputTag("susyScanXsec"),
+                            sparm_xsecInputTag      = cms.InputTag("susyScanCrossSection"),
                             aliasPrefix             = cms.untracked.string("sparm")
 )
