e2c64460196007de623e353c866c4be81e61ee53
benhoob <benhoob>
2010-03-28 12:30:50 +0000

    Made compatible with full mSUGRA grid

--- NtupleMacros/OSSusy/SusyScan.C
+++ NtupleMacros/OSSusy/SusyScan.C
@@ -14,15 +14,59 @@
 #include "TMath.h"
 //#include "histtools.h"
 
-int nm0points  = 2;
-int nm12points = 2;
+//mSUGRA scan parameters-----------------------------
+
+const int   nm0points    = 81;
+const float m0min        = 0.;
+const float m0max        = 4050.;
+const int   nm12points   = 26;
+const float m12min       = 100.;
+const float m12max       = 620.;
+
+const float bkg_syst_error   = 0.5;
+const float upper_limit_100  = 62.3; 
+const float upper_limit_175  = 11.3; 
 
 TH2F* hpred100;
 TH2F* hobs100; 
 TH2F* hzbi100; 
+TH2F* hexc100; 
 TH2F* hpred175;
 TH2F* hobs175; 
 TH2F* hzbi175; 
+TH2F* hexc175; 
+
+float getM0FromIndex(int index){
+
+  float binsize = (m0max - m0min) / (float) nm0points;
+  float m0      = index * binsize + m0min;
+  return m0;
+
+}
+
+float getM12FromIndex(int index){
+
+  float binsize = (m12max - m12min) / (float) nm12points;
+  float m12      = index * binsize + m12min;
+  return m12;
+
+}
+
+// int getIndexFromM0(float m0){
+  
+//   float binsize = (m0max - m0min) / (float) nm0points;
+//   int index     = (int)(m0 - m0min) / binsize;
+//   return index;
+    
+// }
+
+// int getIndexFromM12(float m12){
+  
+//   float binsize = (m12max - m12min) / (float) nm12points;
+//   int index     = (int)(m12 - m12min) / binsize;
+//   return index;
+    
+// }
 
 namespace hist {
   void add(const char* outHistName, const char* patORpfx0);
@@ -33,27 +77,11 @@ namespace hist {
 void loadHist(const char* filename, const char* directory = 0, const char* pfx = 0, 
 	      const char* pat = "*", Bool_t doAdd = kFALSE);
 
-float getm0( int m0index ){
- 
-  if(m0index == 0) return 650.;
-  if(m0index == 1) return 700.;
-
-  return -9999.;
-}
-
-float getm12( int m12index ){
- 
-  if(m12index == 0) return 100.;
-  if(m12index == 1) return 140.;
-
-  return -9999.;
-}
-
 void BookHists();
 
 double getZBi(double n_on , double mu_b_hat , double sigma_b);
 
-void SusyScan(char* filename) {
+void SusyScan(char* filename , bool printgif = false) {
 
   TFile *fin = TFile::Open(filename);
   gROOT->SetStyle("Plain");
@@ -94,8 +122,18 @@ void SusyScan(char* filename) {
                
   BookHists();
 
-  cout   << "|    m0     |   m12     |  pred100  |  obs100   |  pred175  |  obs175   |" << endl; 
-  ofile  << "|    m0     |   m12     |  pred100  |  obs100   |  pred175  |  obs175   |" << endl; 
+  ofile  << "|" << setw(8) << "m0"        << setw(8)  
+         << "|" << setw(8) << "m12"       << setw(8)  
+         << "|" << setw(8) << "pred100"   << setw(8)  
+         << "|" << setw(8) << "obs100"    << setw(8)  
+         << "|" << setw(8) << "ZBi100"    << setw(8)  
+         << "|" << setw(8) << "excl?"     << setw(8)  
+         << "|" << setw(8) << "pred175"   << setw(8)  
+         << "|" << setw(8) << "obs175"    << setw(8)  
+         << "|" << setw(8) << "ZBi175"    << setw(8)  
+         << "|" << setw(8) << "excl?"     << setw(8) << "|" << endl;
+
+
 
   //loop over susy scan points
   for(int im0 = 0 ; im0 < nm0points ; im0++){
@@ -105,9 +143,12 @@ void SusyScan(char* filename) {
       TH1F* hdilPt = (TH1F*)sm_dilPt->Clone(Form("hdilPt_m0_%i_m12_%i",im0,im12));
       TH1F* htcmet = (TH1F*)sm_tcmet->Clone(Form("htcmet_m0_%i_m12_%i",im0,im12));
 
-      TH1F* hsusydilPt   = (TH1F*) fin->Get(Form("susy_hdilPt_m0%i_m12%i",im0,im12));
-      TH1F* hsusytcmet   = (TH1F*) fin->Get(Form("susy_htcmet_m0%i_m12%i",im0,im12));
+      TH1F* hsusydilPt   = (TH1F*) fin->Get(Form("susy_hdilPt_m0_%i_m12_%i",im0,im12));
+      TH1F* hsusytcmet   = (TH1F*) fin->Get(Form("susy_htcmet_m0_%i_m12_%i",im0,im12));
       
+      hsusydilPt->Scale(1000.);
+      hsusytcmet->Scale(1000.);
+
       hdilPt->Add(hsusydilPt);
       htcmet->Add(hsusytcmet);
       
@@ -127,51 +168,72 @@ void SusyScan(char* filename) {
       double met_observed100 = htcmet->Integral(bin100, 101);
       double met_observed175 = htcmet->Integral(bin175, 101);
       
-      float m0  = getm0( im0 );
-      float m12 = getm0( im12 );
+      float m0  = getM0FromIndex( im0 );
+      float m12 = getM12FromIndex( im12 );
+
+      float ZBi100 = getZBi( met_observed100 , dilpt_prediction100 , bkg_syst_error * dilpt_prediction100  );
+      float ZBi175 = getZBi( met_observed175 , dilpt_prediction175 , bkg_syst_error * dilpt_prediction175  );
+
+      float excl100 = met_observed100 > upper_limit_100 ? 1 : 0;
+      float excl175 = met_observed175 > upper_limit_175 ? 1 : 0;
 
       //print text to file
-      ofile << "|" << setw(6) << setprecision(3) << m0 << setw(6) << "|" << setw(6) << m12 << setw(6) << "|" 
-           << setw(6) << dilpt_prediction100 << setw(6) << "|" << setw(6) << met_observed100 << setw(6) << "|"
-           << setw(6) << dilpt_prediction175 << setw(6) << "|" << setw(6) << met_observed175 << setw(6) << "|" << endl;
-      
-      cout << "|" << setw(6) << setprecision(3) << m0 << setw(6) << "|" << setw(6) << m12 << setw(6) << "|" 
-           << setw(6) << dilpt_prediction100 << setw(6) << "|" << setw(6) << met_observed100 << setw(6) << "|"
-           << setw(6) << dilpt_prediction175 << setw(6) << "|" << setw(6) << met_observed175 << setw(6) << "|" << endl;
+      ofile << "|" << setw(8) << setprecision(3) << m0 << setw(8) << "|" << setw(8) << m12 << setw(8) << "|" 
+            << setw(8) << dilpt_prediction100 << setw(8) << "|" << setw(8) << met_observed100 << setw(8) << "|"
+            << setw(8) << ZBi100 << setw(8) << "|" << setw(8) << excl100 << setw(8) << "|"
+            << setw(8) << dilpt_prediction175 << setw(8) << "|" << setw(8) << met_observed175 << setw(8) << "|"
+            << setw(8) << ZBi175 << setw(8) << "|" << setw(8) << excl175 << setw(8) << "|" << endl;
+
     
       //fill histos
-      hpred100 -> Fill(m0,m12,dilpt_prediction100);
-      hobs100  -> Fill(m0,m12,met_observed100);
-      hzbi100  -> Fill(m0,m12, getZBi( met_observed100 , dilpt_prediction100 , 0.2 * dilpt_prediction100  ) );
-      hpred175 -> Fill(m0,m12,dilpt_prediction175);
-      hobs175  -> Fill(m0,m12,met_observed175);
-      hzbi175  -> Fill(m0,m12, getZBi( met_observed175 , dilpt_prediction175 , 0.2 * dilpt_prediction175 ) );
+      hpred100 -> Fill(m0,m12, dilpt_prediction100);
+      hobs100  -> Fill(m0,m12, met_observed100);
+      hzbi100  -> Fill(m0,m12, ZBi100);
+      hexc100  -> Fill(m0,m12, excl100);
+      hpred175 -> Fill(m0,m12, dilpt_prediction175);
+      hobs175  -> Fill(m0,m12, met_observed175);
+      hzbi175  -> Fill(m0,m12, ZBi175);
+      hexc175  -> Fill(m0,m12, excl175);
 
-      //cout<<" ZBi100 "<<getZBi( met_observed100 , dilpt_prediction100 , 0.2 * dilpt_prediction100  )
-      //    <<" ZBi175 "<<getZBi( met_observed175 , dilpt_prediction175 , 0.2 * dilpt_prediction175  )<<endl;
     }
       
   }
   
-  TCanvas *c100 = new TCanvas("c100","met > 100 GeV canvas",1200,900);
-  c100->Divide(2,2);
-  c100->cd(1);
+  TCanvas *c100a = new TCanvas("c100a","met > 100 GeV canvas",1200,450);
+  c100a->Divide(2,1);
+  c100a->cd(1);
   hpred100->Draw("colz");
-  c100->cd(2);
+  c100a->cd(2);
   hobs100->Draw("colz");
-  c100->cd(3);
-  hzbi100->Draw("colz");
-
-  TCanvas *c175 = new TCanvas("c175","met > 175 GeV canvas",1200,900);
-  c175->Divide(2,2);
-  c175->cd(1);
-  hpred175->Draw("colz");
-  c175->cd(2);
-  hobs175->Draw("colz");
-  c175->cd(3);
-  hzbi175->Draw("colz");
-
   
+  TCanvas *c100b = new TCanvas("c100b","met > 100 GeV canvas",1200,450);
+  c100b->Divide(2,1);
+  c100b->cd(1);
+  hzbi100->Draw("colz");
+  c100b->cd(2);
+  hexc100->Draw("colz");
+
+  TCanvas *c175a = new TCanvas("c175a","met > 175 GeV canvas",1200,450);
+  c175a->Divide(2,1);
+  c175a->cd(1);
+  hpred175->Draw("colz");
+  c175a->cd(2);
+  hobs175->Draw("colz");
+
+  TCanvas *c175b = new TCanvas("c175b","met > 175 GeV canvas",1200,450);
+  c175b->Divide(2,1);
+  c175b->cd(1);
+  hzbi175->Draw("colz");
+  c175b->cd(2);
+  hexc175->Draw("colz");
+  
+  if(printgif){
+    c100a->Print("plots/yields_met100.gif");
+    c100b->Print("plots/sig_met100.gif");
+    c175a->Print("plots/yields_met175.gif");
+    c175b->Print("plots/sig_met175.gif");
+  }
+
   ofile.close();
   //fout->cd();
   //fout->Write();
@@ -199,41 +261,48 @@ double getZBi(double n_on , double mu_b_hat , double sigma_b){
 void BookHists(){
   
   //book histos for predicted/observed yields and significance
-  int   nm0    = 80;
+  int   nm0    = 81;
   float m0min  = 0.;
-  float m0max  = 4000.;
-  int   nm12   = 25;
+  float m0max  = 4050.;
+  int   nm12   = 26;
   float m12min = 100.;
-  float m12max = 800.;
+  float m12max = 620.;
 
   hpred100 = new TH2F("hpred100" , "Predicted Yields (met>100)" , nm0,m0min,m0max,nm12,m12min,m12max);
   hobs100  = new TH2F("hobs100"  , "Observed Yields (met>100)"  , nm0,m0min,m0max,nm12,m12min,m12max);
   hzbi100  = new TH2F("hzbi100"  , "ZBI (met>100)"              , nm0,m0min,m0max,nm12,m12min,m12max);
+  hexc100  = new TH2F("hexc100"  , "Exclusion (met>100)"        , nm0,m0min,m0max,nm12,m12min,m12max);
   hpred175 = new TH2F("hpred175" , "Predicted Yields (met>175)" , nm0,m0min,m0max,nm12,m12min,m12max);
   hobs175  = new TH2F("hobs175"  , "Observed Yields (met>175)"  , nm0,m0min,m0max,nm12,m12min,m12max);
   hzbi175  = new TH2F("hzbi175"  , "ZBI (met>175)"              , nm0,m0min,m0max,nm12,m12min,m12max);
-  
+  hexc175  = new TH2F("hexc175"  , "Exclusion (met>175)"        , nm0,m0min,m0max,nm12,m12min,m12max);  
+
   hpred100->GetXaxis()->SetTitle("m_{0} (GeV)");
   hobs100 ->GetXaxis()->SetTitle("m_{0} (GeV)");
   hzbi100 ->GetXaxis()->SetTitle("m_{0} (GeV)");
+  hexc100 ->GetXaxis()->SetTitle("m_{0} (GeV)");
   hpred175->GetXaxis()->SetTitle("m_{0} (GeV)");
   hobs175 ->GetXaxis()->SetTitle("m_{0} (GeV)");
   hzbi175 ->GetXaxis()->SetTitle("m_{0} (GeV)");
+  hexc175 ->GetXaxis()->SetTitle("m_{0} (GeV)");
 
   hpred100->GetYaxis()->SetTitle("m_{1/2} (GeV)");
   hobs100 ->GetYaxis()->SetTitle("m_{1/2} (GeV)");
   hzbi100 ->GetYaxis()->SetTitle("m_{1/2} (GeV)");
+  hexc100 ->GetYaxis()->SetTitle("m_{1/2} (GeV)");
   hpred175->GetYaxis()->SetTitle("m_{1/2} (GeV)");
   hobs175 ->GetYaxis()->SetTitle("m_{1/2} (GeV)");
   hzbi175 ->GetYaxis()->SetTitle("m_{1/2} (GeV)");
+  hexc175 ->GetYaxis()->SetTitle("m_{1/2} (GeV)");
 
   hpred100->SetTitle("Predicted Yield (met > 100 GeV)");
   hobs100 ->SetTitle("Observed Yield (met > 100 GeV)");
   hzbi100 ->SetTitle("Z_{Bi} (met > 100 GeV)");
+  hexc100 ->SetTitle("Exclusion (met > 100 GeV)");
   hpred175->SetTitle("Predicted Yield (met > 175 GeV)");
   hobs175 ->SetTitle("Observed Yield (met > 175 GeV)");
   hzbi175 ->SetTitle("Z_{Bi} (met > 175 GeV)");
-
+  hexc175 ->SetTitle("Exclusion (met > 175 GeV)");
 }
 
 
