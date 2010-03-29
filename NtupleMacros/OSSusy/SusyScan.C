6cc7aec5ed9fafc58d8396e9ac266c0a1019f91f
benhoob <benhoob>
2010-03-29 12:12:41 +0000

    Include Z_N

--- NtupleMacros/OSSusy/SusyScan.C
+++ NtupleMacros/OSSusy/SusyScan.C
@@ -12,6 +12,7 @@
 #include <fstream>
 #include "TLegendEntry.h"
 #include "TMath.h"
+#include "zn.cc"
 //#include "histtools.h"
 
 //mSUGRA scan parameters-----------------------------
@@ -30,10 +31,12 @@ const float upper_limit_175  = 11.3;
 TH2F* hpred100;
 TH2F* hobs100; 
 TH2F* hzbi100; 
+TH2F* hzn100; 
 TH2F* hexc100; 
 TH2F* hpred175;
 TH2F* hobs175; 
 TH2F* hzbi175; 
+TH2F* hzn175; 
 TH2F* hexc175; 
 
 float getM0FromIndex(int index){
@@ -52,22 +55,6 @@ float getM12FromIndex(int index){
 
 }
 
-// int getIndexFromM0(float m0){
-  
-//   float binsize = (m0max - m0min) / (float) nm0points;
-//   int index     = (int)(m0 - m0min) / binsize;
-//   return index;
-    
-// }
-
-// int getIndexFromM12(float m12){
-  
-//   float binsize = (m12max - m12min) / (float) nm12points;
-//   int index     = (int)(m12 - m12min) / binsize;
-//   return index;
-    
-// }
-
 namespace hist {
   void add(const char* outHistName, const char* patORpfx0);
   TLegend* legend(TCanvas* canvas, Option_t* option = "lpf", Bool_t addColor = kFALSE, Int_t token = -1, 
@@ -80,6 +67,8 @@ void loadHist(const char* filename, const char* directory = 0, const char* pfx =
 void BookHists();
 
 double getZBi(double n_on , double mu_b_hat , double sigma_b);
+
+void drawPlot(TH2F* h , TCanvas *c, bool printgif = false);
 
 void SusyScan(char* filename , bool printgif = false) {
 
@@ -119,7 +108,7 @@ void SusyScan(char* filename , bool printgif = false) {
   
   //TFile *fout = new TFile("SusyScan.root","RECREATE");
   //fout->cd();
-               
+         
   BookHists();
 
   ofile  << "|" << setw(8) << "m0"        << setw(8)  
@@ -127,13 +116,13 @@ void SusyScan(char* filename , bool printgif = false) {
          << "|" << setw(8) << "pred100"   << setw(8)  
          << "|" << setw(8) << "obs100"    << setw(8)  
          << "|" << setw(8) << "ZBi100"    << setw(8)  
+         << "|" << setw(8) << "ZN100"     << setw(8)  
          << "|" << setw(8) << "excl?"     << setw(8)  
          << "|" << setw(8) << "pred175"   << setw(8)  
          << "|" << setw(8) << "obs175"    << setw(8)  
          << "|" << setw(8) << "ZBi175"    << setw(8)  
+         << "|" << setw(8) << "ZN175"     << setw(8)  
          << "|" << setw(8) << "excl?"     << setw(8) << "|" << endl;
-
-
 
   //loop over susy scan points
   for(int im0 = 0 ; im0 < nm0points ; im0++){
@@ -146,9 +135,6 @@ void SusyScan(char* filename , bool printgif = false) {
       TH1F* hsusydilPt   = (TH1F*) fin->Get(Form("susy_hdilPt_m0_%i_m12_%i",im0,im12));
       TH1F* hsusytcmet   = (TH1F*) fin->Get(Form("susy_htcmet_m0_%i_m12_%i",im0,im12));
       
-      hsusydilPt->Scale(1000.);
-      hsusytcmet->Scale(1000.);
-
       hdilPt->Add(hsusydilPt);
       htcmet->Add(hsusytcmet);
       
@@ -167,78 +153,84 @@ void SusyScan(char* filename , bool printgif = false) {
       
       double met_observed100 = htcmet->Integral(bin100, 101);
       double met_observed175 = htcmet->Integral(bin175, 101);
-      
+
+      //get sparms
       float m0  = getM0FromIndex( im0 );
       float m12 = getM12FromIndex( im12 );
 
+      //get Z-values
       float ZBi100 = getZBi( met_observed100 , dilpt_prediction100 , bkg_syst_error * dilpt_prediction100  );
       float ZBi175 = getZBi( met_observed175 , dilpt_prediction175 , bkg_syst_error * dilpt_prediction175  );
 
+      float ZN100 = getzn( met_observed100 - dilpt_prediction100 , dilpt_prediction100 , 0 , bkg_syst_error * 100);
+      float ZN175 = getzn( met_observed175 - dilpt_prediction175 , dilpt_prediction175 , 0 , bkg_syst_error * 100);
+      
+      //determine if point is excluded
       float excl100 = met_observed100 > upper_limit_100 ? 1 : 0;
       float excl175 = met_observed175 > upper_limit_175 ? 1 : 0;
-
+      
       //print text to file
-      ofile << "|" << setw(8) << setprecision(3) << m0 << setw(8) << "|" << setw(8) << m12 << setw(8) << "|" 
-            << setw(8) << dilpt_prediction100 << setw(8) << "|" << setw(8) << met_observed100 << setw(8) << "|"
-            << setw(8) << ZBi100 << setw(8) << "|" << setw(8) << excl100 << setw(8) << "|"
-            << setw(8) << dilpt_prediction175 << setw(8) << "|" << setw(8) << met_observed175 << setw(8) << "|"
-            << setw(8) << ZBi175 << setw(8) << "|" << setw(8) << excl175 << setw(8) << "|" << endl;
-
+      ofile << "|" 
+            << setw(8) << setprecision(3) << m0 << setw(8) << "|" 
+            << setw(8) << m12 << setw(8) << "|" 
+            << setw(8) << dilpt_prediction100 << setw(8) << "|" 
+            << setw(8) << met_observed100 << setw(8) << "|"
+            << setw(8) << ZBi100 << setw(8) << "|" 
+            << setw(8) << ZN100 << setw(8) << "|" 
+            << setw(8) << excl100 << setw(8) << "|"
+            << setw(8) << dilpt_prediction175 << setw(8) << "|" 
+            << setw(8) << met_observed175 << setw(8) << "|"
+            << setw(8) << ZBi175 << setw(8) << "|" 
+            << setw(8) << ZN175 << setw(8) << "|" 
+            << setw(8) << excl175 << setw(8) << "|" << endl;
     
       //fill histos
       hpred100 -> Fill(m0,m12, dilpt_prediction100);
       hobs100  -> Fill(m0,m12, met_observed100);
       hzbi100  -> Fill(m0,m12, ZBi100);
+      hzn100   -> Fill(m0,m12, ZN100);
       hexc100  -> Fill(m0,m12, excl100);
       hpred175 -> Fill(m0,m12, dilpt_prediction175);
       hobs175  -> Fill(m0,m12, met_observed175);
       hzbi175  -> Fill(m0,m12, ZBi175);
+      hzn175   -> Fill(m0,m12, ZN175);
       hexc175  -> Fill(m0,m12, excl175);
 
     }
       
   }
   
-  TCanvas *c100a = new TCanvas("c100a","met > 100 GeV canvas",1200,450);
-  c100a->Divide(2,1);
-  c100a->cd(1);
-  hpred100->Draw("colz");
-  c100a->cd(2);
-  hobs100->Draw("colz");
-  
-  TCanvas *c100b = new TCanvas("c100b","met > 100 GeV canvas",1200,450);
-  c100b->Divide(2,1);
-  c100b->cd(1);
-  hzbi100->Draw("colz");
-  c100b->cd(2);
-  hexc100->Draw("colz");
+  const int ncan = 1;
+  TCanvas *can[ncan];
 
-  TCanvas *c175a = new TCanvas("c175a","met > 175 GeV canvas",1200,450);
-  c175a->Divide(2,1);
-  c175a->cd(1);
-  hpred175->Draw("colz");
-  c175a->cd(2);
-  hobs175->Draw("colz");
-
-  TCanvas *c175b = new TCanvas("c175b","met > 175 GeV canvas",1200,450);
-  c175b->Divide(2,1);
-  c175b->cd(1);
-  hzbi175->Draw("colz");
-  c175b->cd(2);
-  hexc175->Draw("colz");
-  
-  if(printgif){
-    c100a->Print("plots/yields_met100.gif");
-    c100b->Print("plots/sig_met100.gif");
-    c175a->Print("plots/yields_met175.gif");
-    c175b->Print("plots/sig_met175.gif");
-  }
+  drawPlot(hpred100 , can[0] , printgif);
+  drawPlot(hobs100  , can[1] , printgif);
+  drawPlot(hzbi100  , can[2] , printgif);
+  drawPlot(hzn100   , can[3] , printgif);
+  drawPlot(hexc100  , can[4] , printgif);
+  drawPlot(hpred175 , can[4] , printgif);
+  drawPlot(hobs175  , can[5] , printgif);
+  drawPlot(hzbi175  , can[6] , printgif);
+  drawPlot(hzn175   , can[8] , printgif);
+  drawPlot(hexc175  , can[9] , printgif);
 
   ofile.close();
   //fout->cd();
   //fout->Write();
   //fout->Close();
 
+}
+
+void drawPlot(TH2F* h , TCanvas *c , bool printgif){
+  c = new TCanvas(Form("%s_can",h->GetName()),Form("%s_can",h->GetName()),800,600);
+  c -> Divide(2,1);
+  h->Draw("colz");
+  
+  if(printgif){
+    c -> Modified();
+    c -> Update();
+    c -> Print(Form("plots/%s.gif",h->GetName()));
+  }
 }
 
 double getZBi(double n_on , double mu_b_hat , double sigma_b){
@@ -271,37 +263,45 @@ void BookHists(){
   hpred100 = new TH2F("hpred100" , "Predicted Yields (met>100)" , nm0,m0min,m0max,nm12,m12min,m12max);
   hobs100  = new TH2F("hobs100"  , "Observed Yields (met>100)"  , nm0,m0min,m0max,nm12,m12min,m12max);
   hzbi100  = new TH2F("hzbi100"  , "ZBI (met>100)"              , nm0,m0min,m0max,nm12,m12min,m12max);
+  hzn100   = new TH2F("hzn100"   , "ZN (met>100)"               , nm0,m0min,m0max,nm12,m12min,m12max);
   hexc100  = new TH2F("hexc100"  , "Exclusion (met>100)"        , nm0,m0min,m0max,nm12,m12min,m12max);
   hpred175 = new TH2F("hpred175" , "Predicted Yields (met>175)" , nm0,m0min,m0max,nm12,m12min,m12max);
   hobs175  = new TH2F("hobs175"  , "Observed Yields (met>175)"  , nm0,m0min,m0max,nm12,m12min,m12max);
   hzbi175  = new TH2F("hzbi175"  , "ZBI (met>175)"              , nm0,m0min,m0max,nm12,m12min,m12max);
+  hzn175   = new TH2F("hzn175"   , "ZN (met>175)"               , nm0,m0min,m0max,nm12,m12min,m12max);
   hexc175  = new TH2F("hexc175"  , "Exclusion (met>175)"        , nm0,m0min,m0max,nm12,m12min,m12max);  
 
   hpred100->GetXaxis()->SetTitle("m_{0} (GeV)");
   hobs100 ->GetXaxis()->SetTitle("m_{0} (GeV)");
   hzbi100 ->GetXaxis()->SetTitle("m_{0} (GeV)");
+  hzn100  ->GetXaxis()->SetTitle("m_{0} (GeV)");
   hexc100 ->GetXaxis()->SetTitle("m_{0} (GeV)");
   hpred175->GetXaxis()->SetTitle("m_{0} (GeV)");
   hobs175 ->GetXaxis()->SetTitle("m_{0} (GeV)");
   hzbi175 ->GetXaxis()->SetTitle("m_{0} (GeV)");
+  hzn175  ->GetXaxis()->SetTitle("m_{0} (GeV)");
   hexc175 ->GetXaxis()->SetTitle("m_{0} (GeV)");
 
   hpred100->GetYaxis()->SetTitle("m_{1/2} (GeV)");
   hobs100 ->GetYaxis()->SetTitle("m_{1/2} (GeV)");
   hzbi100 ->GetYaxis()->SetTitle("m_{1/2} (GeV)");
+  hzn100  ->GetYaxis()->SetTitle("m_{1/2} (GeV)");
   hexc100 ->GetYaxis()->SetTitle("m_{1/2} (GeV)");
   hpred175->GetYaxis()->SetTitle("m_{1/2} (GeV)");
   hobs175 ->GetYaxis()->SetTitle("m_{1/2} (GeV)");
   hzbi175 ->GetYaxis()->SetTitle("m_{1/2} (GeV)");
+  hzn175  ->GetYaxis()->SetTitle("m_{1/2} (GeV)");
   hexc175 ->GetYaxis()->SetTitle("m_{1/2} (GeV)");
 
   hpred100->SetTitle("Predicted Yield (met > 100 GeV)");
   hobs100 ->SetTitle("Observed Yield (met > 100 GeV)");
   hzbi100 ->SetTitle("Z_{Bi} (met > 100 GeV)");
+  hzn100  ->SetTitle("Z_{N} (met > 100 GeV)");
   hexc100 ->SetTitle("Exclusion (met > 100 GeV)");
   hpred175->SetTitle("Predicted Yield (met > 175 GeV)");
   hobs175 ->SetTitle("Observed Yield (met > 175 GeV)");
   hzbi175 ->SetTitle("Z_{Bi} (met > 175 GeV)");
+  hzn175  ->SetTitle("Z_{N} (met > 175 GeV)");
   hexc175 ->SetTitle("Exclusion (met > 175 GeV)");
 }
 
