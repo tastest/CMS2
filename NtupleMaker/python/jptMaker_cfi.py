c424176d4e22a3d2814274e38843d941567fef42
olivito <olivito>
2010-04-09 17:19:50 +0000

    This commit was manufactured by cvs2svn to create tag 'whunt_v0'.

--- NtupleMaker/python/jptMaker_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-jptMaker = cms.EDFilter("JPTMaker",
-	aliasPrefix = cms.untracked.string("jpts"),
-                        jptInputTag       = cms.InputTag("JetPlusTrackZSPCorJetAntiKt5")
-)
--- NtupleMaker/python/patMETMaker_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-patMETMaker = cms.EDFilter("PATMETMaker",
-	aliasPrefix = cms.untracked.string("met_pat"),
-                           patMETsInputTag = cms.InputTag("patMETs")
-)
-
-
