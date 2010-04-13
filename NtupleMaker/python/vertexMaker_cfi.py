bbb632c6a2cc2921c593a6d4e96f18774643301b
olivito <olivito>
2010-04-13 16:28:12 +0000

    This commit was manufactured by cvs2svn to create tag 'whunt_v2'.

--- NtupleMaker/python/vertexMaker_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-vertexMaker = cms.EDFilter("VertexMaker",
-	aliasPrefix = cms.untracked.string("vtxs"),
-                           primaryVertexInputTag = cms.InputTag("offlinePrimaryVertices"),
-)
-
