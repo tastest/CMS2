509ed2add25754650b955d266cbea4c4506c19b5
olivito <olivito>
2010-04-14 19:28:31 +0000

    This commit was manufactured by cvs2svn to create tag 'whunt_v3'.

--- NtupleMaker/python/pfJetMaker_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-pfJetMaker = cms.EDFilter("PFJetMaker",
-             pfJetsInputTag   = cms.InputTag("ak5PFJets"),
-             pfJetPtCut       = cms.double(5.),
-             L2L3JetCorrectorName = cms.string("L2L3JetCorrectorAK5PF")
-)
-
-
--- NtupleMaker/python/photonMaker_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-photonMaker = cms.EDFilter("PhotonMaker",
-	aliasPrefix = cms.untracked.string("photons"),
-    # Photon collection
-    photonsInputTag = cms.InputTag("photons"),
-)
-
