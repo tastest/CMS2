38f8cd9a46f382e770d0cc639a2000c14d27638c
olivito <olivito>
2010-04-15 15:10:43 +0000

    This commit was manufactured by cvs2svn to create tag 'whunt_v4'.

--- NtupleMaker/python/hcalNoiseSummaryMaker_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hcalNoiseSummaryMaker = cms.EDFilter("HcalNoiseSummaryMaker",
-	aliasPrefix = cms.untracked.string("hcalnoise"),
-                                     hcalNoiseSummaryTag = cms.InputTag("hcalnoise")
-)
