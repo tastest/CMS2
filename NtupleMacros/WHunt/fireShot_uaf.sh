e483a0861ad7d1391ea84b7a19797ca0f97816e0
jribnik <jribnik>
2010-04-19 11:58:22 +0000

    Use new fireShot implementation. These guys should be moved to a huntcore or something and out of the WHunt and DilepHunt directories.

--- NtupleMacros/WHunt/fireShot_uaf.sh
+++ NtupleMacros/WHunt/fireShot_uaf.sh
@@ -1,59 +1,59 @@
 #!/bin/bash
 
-cd ~/devel/whunt
-
-# Need CMSSW environment for cmsShow
-if [ ! -d CMSSW_3_5_6/src ]; then
-    scramv1 p CMSSW CMSSW_3_5_6
-fi
-cd CMSSW_3_5_6/src
-eval `scramv1 ru -sh`
-cd -
+cd ~/devel/fireShot
 
 echo Waiting for the starting gun...
-while [ 1 ]; do
-    if [ -e .fireShot.bang ]; then
+while [ 1 ]
+do
+    if [ -e .fireShot.bang ]
+    then
         echo Yeehaw time to ride!
 
-        for f in `find ~/public_html/whunt/picks -name "*.root"`; do
+        for f in `find ~/devel/fireShot/picks -name "*.root"`
+        do
             run_=`echo $f | sed 's/^.*FEVT_\(.*\).root$/\1/' | awk -F'_' '{print $1}'`
             ls_=`echo $f | sed 's/^.*FEVT_\(.*\).root$/\1/' | awk -F'_' '{print $2}'`
             evt_=`echo $f | sed 's/^.*FEVT_\(.*\).root$/\1/' | awk -F'_' '{print $3}'`
 
             # Already done?
-            ls ~/public_html/whunt/shots/fireworks_${run_}_${ls_}_${evt_}.png >/dev/null 2>&1
-            if [ $? -eq 0 ]; then
-                continue;
+            ls ~/devel/fireShot/shots/fireworks_${run_}_${ls_}_${evt_}.png >/dev/null 2>&1
+            if [ $? -eq 0 ]
+            then
+                echo Skipping $run_ $ls_ $evt_
+                continue
             fi
 
-            cmsShow -c whunt.fwc $f &
-            #pid=`ps u | grep cmsShow.exe | awk '{print $2}'`
-            #echo pid: $pid
+            # There _should_ be a cmsShow instance
+            # ready and willing on localhost:$PORT
+            echo "Psst, cmsShow, I've got the goods: $f"
+            echo "$f" | nc -w 10 localhost 9999
             sleep 30
 
             ssh noms-1 "rm ~/.fireShot.done"
             ssh noms-1 "echo $run_ $ls_ $evt_ >~/.fireShot.prep"
 
-            while [ 1 ]; do
+            while [ 1 ]
+            do
                 ssh noms-1 "ls ~/.fireShot.done >/dev/null 2>&1"
-                if [ $? -eq 0 ]; then
-                    #kill -9 $pid
-                    killall cmsShow.exe
+                if [ $? -eq 0 ]
+                then
                     break
                 fi
 
                 sleep 10
             done
 
-            if [ -e ~/devel/whunt/xwd/fireworks_${run_}_${ls_}_${evt_}.xwd ]; then
+            if [ -e ~/devel/fireShot/xwd/fireworks_${run_}_${ls_}_${evt_}.xwd ]
+            then
                 echo Found dump, converting to png
-                convert ~/devel/whunt/xwd/fireworks_${run_}_${ls_}_${evt_}.xwd ~/public_html/whunt/shots/fireworks_${run_}_${ls_}_${evt_}.png
+                convert ~/devel/fireShot/xwd/fireworks_${run_}_${ls_}_${evt_}.xwd ~/devel/fireShot/shots/fireworks_${run_}_${ls_}_${evt_}.png
             else
                 echo Dump not found, not good
             fi
         done
 
         rm .fireShot.bang
+        echo "That was fun, thanks!"
     fi
 
     sleep 10
