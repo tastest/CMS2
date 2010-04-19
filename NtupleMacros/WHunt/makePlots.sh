868a219674270c9c1146e7b26307ef0aba678387
jribnik <jribnik>
2010-04-19 00:27:44 +0000

    Process lists of candidates in a generic way

--- NtupleMacros/WHunt/makePlots.sh
+++ NtupleMacros/WHunt/makePlots.sh
@@ -3,7 +3,7 @@
 # Run basic plotting and scanning script
 # and transfer plots and dumps to uaf
 # Note that because cms-tas0? is not slc5
-# we are executing wcands.sh on lxplus5
+# we are executing processCands.sh on lxplus5
 # and transferring everything back over
 cp wcands.txt wcands.txt_previous
 root -b -l -q makePlots.C
@@ -42,7 +42,7 @@ make build
 python makeWSummaryPlot.py wfinder
 #
 scp wcands.txt lxplus303:~/scratch0/whunt
-ssh lxplus303 /afs/cern.ch/user/j/jribnik/scratch0/whunt/wcands.sh
+ssh lxplus303 /afs/cern.ch/user/j/jribnik/scratch0/whunt/processCands.sh wcands.txt /tas03/disk01/whunt
 scp plots/* uaf-2.t2.ucsd.edu:~/public_html/whunt/plots
 scp dumps/* uaf-2.t2.ucsd.edu:~/public_html/whunt/dumps
 scp picks/* uaf-2.t2.ucsd.edu:~/public_html/whunt/picks
