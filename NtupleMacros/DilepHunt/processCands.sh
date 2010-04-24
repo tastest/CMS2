8271c232f570938bb64546348fe0f53d6d8cb4fc
jribnik <jribnik>
2010-04-24 10:55:01 +0000

    Check for picks before picking, add a couple lines to email

--- NtupleMacros/DilepHunt/processCands.sh
+++ NtupleMacros/DilepHunt/processCands.sh
@@ -65,9 +65,12 @@ do
     if [ ! -e .donotemail ]
     then
         echo "Check out: http://uaf-2.t2.ucsd.edu/~jribnik/dilephunt/" >email.tmp
-        echo >>email.tmp
+        echo               >>email.tmp
         echo "Here she is:">>email.tmp
-        echo >>email.tmp
+        echo               >>email.tmp
+        echo "run ${run_}" >>email.tmp
+        echo "ls ${ls_}"   >>email.tmp
+        echo "evt ${evt_}" >>email.tmp
         cat $dumpFile >>email.tmp
         cat email.tmp | mail -s "[$candsfiletrunc ALERT] New candidate found!" jribnik@cern.ch
         cat email.tmp | mail -s "[$candsfiletrunc ALERT] New candidate found!" ingo.bloch@cern.ch
@@ -78,11 +81,14 @@ do
     # Only pickAnEvent.pl events that have
     # not already been picked
     alreadypicked=0
-    #ssh cms-tas03 ls /tas03/disk01/fireShot/picks/*${run_}_${ls_}_${evt_}.root >/dev/null 2>&1
-    #if [ $? -eq 0 ]
-    #then
-    #    alreadypicked=1
-    #fi
+    if [ -e /afs/cern.ch/user/j/jribnik/scratch0/picked.txt ]
+    then
+        grep "${run_} ${ls_} ${evt_}" /afs/cern.ch/user/j/jribnik/scratch0/picked.txt >/dev/null 2>&1
+        if [ $? -eq 0 ]
+        then
+            alreadypicked=1
+        fi
+    fi
 
     # If we must pick, only do so for candi==0
     # as we have not yet transferred this pick
