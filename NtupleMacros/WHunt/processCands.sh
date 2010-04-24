2c41ff1c2bbe1ed741a0585e8bb08ea2b4743ec8
jribnik <jribnik>
2010-04-24 10:58:41 +0000

    Consolidate

--- NtupleMacros/WHunt/processCands.sh
+++ NtupleMacros/WHunt/processCands.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Turn output of TTree::Scan into something
-# useful, and trigger pickAnEvent.pl
+# Turn output of TTree::Scan into something useful,
+# i.e. a dump, and trigger pickAnEvent.pl
+# E-mail whomever about existence of new candidate
 
 cd /afs/cern.ch/user/j/jribnik/scratch0/whunt
 if [ $# -ne 2 ]
@@ -59,14 +60,35 @@ do
         echo $field $value >>$dumpFile
     done
 
+    # E-mail the dump unless there exists
+    # the do not email file
+    if [ ! -e .donotemail ]
+    then
+        echo "Check out: http://uaf-2.t2.ucsd.edu/~jribnik/whunt/" >email.tmp
+        echo               >>email.tmp
+        echo "Here she is:">>email.tmp
+        echo               >>email.tmp
+        echo "run ${run_}" >>email.tmp
+        echo "ls ${ls_}"   >>email.tmp
+        echo "evt ${evt_}" >>email.tmp
+        cat $dumpFile >>email.tmp
+        cat email.tmp | mail -s "[$candsfiletrunc ALERT] New candidate found!" jribnik@cern.ch
+        cat email.tmp | mail -s "[$candsfiletrunc ALERT] New candidate found!" ingo.bloch@cern.ch
+        cat email.tmp | mail -s "[$candsfiletrunc ALERT] New candidate found!" ayagil@physics.ucsd.edu
+        cat email.tmp | mail -s "[$candsfiletrunc ALERT] New candidate found!" fgolf@physics.ucsd.edu
+    fi
+
     # Only pickAnEvent.pl events that have
     # not already been picked
     alreadypicked=0
-    #ssh cms-tas03 ls /tas03/disk01/fireShot/picks/*${run_}_${ls_}_${evt_}.root >/dev/null 2>&1
-    #if [ $? -eq 0 ]
-    #then
-    #    alreadypicked=1
-    #fi
+    if [ -e /afs/cern.ch/user/j/jribnik/scratch0/picked.txt ]
+    then
+        grep "${run_} ${ls_} ${evt_}" /afs/cern.ch/user/j/jribnik/scratch0/picked.txt >/dev/null 2>&1
+        if [ $? -eq 0 ]
+        then
+            alreadypicked=1
+        fi
+    fi
 
     # If we must pick, only do so for candi==0
     # as we have not yet transferred this pick
