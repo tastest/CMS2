3a82160998397bd1ff56086ce6a6a458fd96ba93
olivito <olivito>
2010-05-06 05:59:43 +0000

    This commit was manufactured by cvs2svn to create tag 'V03-03-26'.

--- NtupleMacros/Tools/CL/setup.C
+++
@@ -1,8 +0,0 @@
-//
-{
-gSystem->Load("libThread.so");
-gSystem->Load("libNet.so");
-gSystem->Load("libTreePlayer.so");
-gSystem->Load("libCMS2NtupleMacrosCORE.so");
-gSystem->Load("libCMS2CL.so");
-}
