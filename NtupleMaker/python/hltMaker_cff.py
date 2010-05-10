86a753a8a4055b24b3ab8f65bb7b2baf4c993bf2
olivito <olivito>
2010-05-10 22:39:45 +0000

    This commit was manufactured by cvs2svn to create branch 'VB03-04-10'.

--- NtupleMaker/python/hltMaker_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from CMS2.NtupleMaker.hltMaker_cfi import *
-hltMakerSequence = cms.Sequence(hltMaker * hlt8e29Maker * hlt1E31Maker)
-
--- NtupleMaker/python/pdfinfoMaker_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-pdfinfoMaker = cms.EDFilter( "PDFInfoMaker",
-	aliasPrefix = cms.untracked.string("pdfinfo"),
-)
-
-
