20cadcfbc7362f7ca2ba981048fa682bc9c05d0d
dmytro <dmytro>
2010-06-01 08:22:14 +0000

    updated x-sections to match https://twiki.cern.ch/twiki/bin/view/CMS/StandardModelCrossSections?rev=16 (Drell-Yan still needs to be updated)

--- NtupleMacros/WW/processData.C
+++ NtupleMacros/WW/processData.C
@@ -82,16 +82,16 @@ void processData()
   string dataset = "data";
  
   if (runWW)
-    ProcessSample(dataset+"/WW_Spring10-START3X_V26_S09-v1_DiLep/V03-04-08"+version+"/merged_ntuple*.root", WW, 100.0, 42.84, fullDataSet, kRed);
+    ProcessSample(dataset+"/WW_Spring10-START3X_V26_S09-v1_DiLep/V03-04-08"+version+"/merged_ntuple*.root", WW, 100.0, 43, fullDataSet, kRed);
 
   if (runWZ)
-    ProcessSample(dataset+"/WZ_Spring10-START3X_V26_S09-v1/V03-04-08"+version+"/merged_ntuple*.root", WZ, 100.0, 18.0, fullDataSet, kBlue);
+    ProcessSample(dataset+"/WZ_Spring10-START3X_V26_S09-v1/V03-04-08"+version+"/merged_ntuple*.root", WZ, 100.0, 18.2, fullDataSet, kBlue);
   
   if (runZZ)
     ProcessSample(dataset+"/ZZ_Spring10-START3X_V26_S09-v1_DiLep/V03-04-08"+version+"/merged_ntuple*.root", ZZ, 100.0, 5.9, fullDataSet, kGreen);
  
   if (runWjets)
-    ProcessSample(dataset+"/WJets-madgraph_Spring10-START3X_V26_S09-v1_SingleLep/V03-04-08"+version+"/dilep-skim.root", Wjets, 100.0, 27360, fullDataSet, 40);
+    ProcessSample(dataset+"/WJets-madgraph_Spring10-START3X_V26_S09-v1_SingleLep/V03-04-08"+version+"/dilep-skim.root", Wjets, 100.0, 28049, fullDataSet, 40);
 
   if (runDYee)
     ProcessSample(dataset+"/Zee_Spring10-START3X_V26_S09-v1/V03-04-08-01"+version+"/merged_ntuple*.root", DYee, 100.0, 1482.0, fullDataSet, kMagenta, identifyDYEvents);
@@ -108,7 +108,7 @@ void processData()
     //ProcessSample(dataset+"/TTbar_Spring10-START3X_V26_S09-v1/V03-04-08"+version+"/merged_ntuple*.root", ttbar, 100.0, 165.0, fullDataSet, kYellow);
 
   if (runtW)
-    ProcessSample(dataset+"/SingleTop_tWChannel-madgraph_Spring10-START3X_V26_S09-v1/V03-04-07"+version+"/merged_ntuple*.root", tW, 100.0, 11.00, fullDataSet, 63);
+    ProcessSample(dataset+"/SingleTop_tWChannel-madgraph_Spring10-START3X_V26_S09-v1/V03-04-07"+version+"/merged_ntuple*.root", tW, 100.0, 10.6, fullDataSet, 63);
   
   std::vector<string> qcdSamples;
   //qcdSamples.push_back(dataset+"/QCD_Pt30_Summer09-MC_31X_V3_7TeV-v1/"+version+"/merged_ntuple*.root");
