0c39369c41d7328b3f52ee1568ca664faea355e2
olivito <olivito>
2010-06-04 12:27:00 +0000

    This commit was manufactured by cvs2svn to create tag 'eleId_v4'.

--- NtupleMaker/python/recoErrorLogMaker_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-recoErrorLogMaker = cms.EDFilter("RECOErrorLogMaker",
-                              errorSummaryCollInputTag = cms.InputTag("logErrorHarvester"),
-                              #supported severity is warning, error
-                              #recommended is error to keep the event size down
-                              minSeverity             = cms.string("error"), 
-)
