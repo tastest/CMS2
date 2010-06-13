0537ff2428f4dbec841bbbd4c22c4d6ba59f2a4c
fgolf <fgolf>
2010-06-13 18:34:57 +0000

    updating MC ntupling tag to HEAD

--- NtupleMaker/interface/PFMuonMaker.h
+++ NtupleMaker/interface/PFMuonMaker.h
@@ -1,23 +1,17 @@
 // -*- C++ -*-
 //
 // Package:    NtupleMaker
-// Class:      BeamHaloMaker
+// Class:      NtupleMaker
 // 
-/**\class NtupleMaker NtupleMaker.cc CMS2/NtupleMaker/src/BeamHaloMaker.cc
+/**\class NtupleMaker NtupleMaker.cc CMS2/NtupleMaker/src/NtupleMaker.cc
 
    Description: <one line class summary>
 
    Implementation:
    <Notes on implementation>
 */
-//
-// Original Author:  pts/4
-//         Created:  Fri Jun  6 11:07:38 CDT 2008
-// $Id: BeamHaloMaker.h,v 1.3.4.1 2010/06/13 18:30:12 fgolf Exp $
-//
-//
-#ifndef NTUPLEMAKER_BEAMSPOTMAKER_H
-#define NTUPLEMAKER_BEAMSPOTMAKER_H
+#ifndef NTUPLEMAKER_PFMUONMAKER_H
+#define NTUPLEMAKER_PFMUONMAKER_H
 
 // system include files
 #include <memory>
@@ -25,31 +19,33 @@
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDProducer.h"
-
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "TString.h"
+#include "Math/VectorUtil.h"
 
 //
 // class decleration
 //
 
-class BeamHaloMaker : public edm::EDProducer {
+class PFMuonMaker : public edm::EDProducer {
 public:
-     explicit BeamHaloMaker (const edm::ParameterSet&);
-     ~BeamHaloMaker();
+     explicit PFMuonMaker (const edm::ParameterSet&);
+     ~PFMuonMaker();
 
 private:
      virtual void beginJob() ;
+     virtual void beginRun(edm::Run&, const edm::EventSetup&) ;
      virtual void produce(edm::Event&, const edm::EventSetup&);
      virtual void endJob() ;
-     std::vector<TString> convertToVectorTString(const std::vector<char>);
-
-     edm::InputTag beamHaloInputTag;
-     edm::InputTag cscHaloInputTag;
+    
+     // ----------member data ---------------------------
+     edm::InputTag pfCandidatesTag_;
+     edm::InputTag isoc_vm_tag_;
+     edm::InputTag ison_vm_tag_;
+     edm::InputTag isop_vm_tag_;
+     edm::InputTag pfAllMuons_tag_;
 };
 
-
 #endif
+
