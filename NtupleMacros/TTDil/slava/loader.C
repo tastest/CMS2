e0fdbccb2375127fece0c54030f440354bec1a11
slava77 <slava77>
2010-06-18 12:35:23 +0000

    add vector<TH1F*> and vector<TFile*>

--- NtupleMacros/TTDil/slava/loader.C
+++ NtupleMacros/TTDil/slava/loader.C
@@ -1,5 +1,9 @@
 #include <vector>
+#include "TFile.h"
+#include "TH1F.h"
 #ifdef __MAKECINT__
 #pragma link C++ class vector<vector<float> >+;
 #pragma link C++ class vector<vector<int> >+;
+#pragma link C++ class vector<TFile*>+;
+#pragma link C++ class vector<TH1F*>+;
 #endif
