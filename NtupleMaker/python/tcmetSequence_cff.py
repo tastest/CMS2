ddb16504a70f73fce12414b8c8935a37b04ce5e5
fgolf <fgolf>
2010-07-19 12:43:49 +0000

    rerun tcMET reconstruction starting with PF clusters rather than calo towers

---
+++ NtupleMaker/python/tcmetSequence_cff.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoMET.METProducers.TCMET_cfi import *
+from JetMETCorrections.Type1MET.MuonTCMETValueMapProducer_cff import *
+from CMS2.NtupleMaker.tcmetMaker_cfi import *
+
+muonTCMETValueMapProducerNew = muonTCMETValueMapProducer.clone()
+muonTCMETValueMapProducerNew.muonMinValidStaHits = cms.int32(0)
+
+tcMetNew = tcMet.clone()
+tcMetNew.tcmetDepValueMap = cms.InputTag("muonTCMETValueMapProducerNew", "muCorrData")
+
+tcmetMakerNew = tcmetMaker.clone()
+tcmetMakerNew.aliasPrefix = cms.untracked.string("evtnew")
+tcmetMakerNew.tcmet_tag_ = cms.InputTag("tcMetNew")
+tcmetMakerNew.tcmet_vm_tag_ = cms.InputTag("muonTCMETValueMapProducerNew", "muCorrData")
+
+tcmetSequence = cms.Sequence(muonTCMETValueMapProducerNew * tcMetNew * tcmetMakerNew)
