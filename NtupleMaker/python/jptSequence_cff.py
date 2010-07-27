3f83c603e7aca285c979db6a67377c995af8fc98
kalavase <kalavase>
2010-07-27 19:40:53 +0000

    updated input tag and sequence

--- NtupleMaker/python/jptSequence_cff.py
+++ NtupleMaker/python/jptSequence_cff.py
@@ -2,13 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoJets.JetAssociationProducers.trackExtrapolator_cfi import *
 from RecoJets.JetAssociationProducers.ak5JTA_cff import *
-ak5JetTracksAssociatorAtVertex.jets = cms.InputTag("prunedUncorrectedCMS2Jets")
-ak5JetTracksAssociatorAtCaloFace.jets = cms.InputTag("prunedUncorrectedCMS2Jets")
-ak5JetExtender.jets = cms.InputTag("prunedUncorrectedCMS2Jets")
+ak5JetTracksAssociatorAtVertex.jets = cms.InputTag("ak5CaloJets")
+ak5JetTracksAssociatorAtCaloFace.jets = cms.InputTag("ak5CaloJets")
+ak5JetExtender.jets = cms.InputTag("ak5CaloJets")
 
 from RecoJets.JetPlusTracks.JetPlusTrackCorrections_cff import *
-JetPlusTrackZSPCorJetAntiKt5.src = cms.InputTag("prunedUncorrectedCMS2Jets")
+JetPlusTrackZSPCorJetAntiKt5.src = cms.InputTag("ak5CaloJets")
 
-from CMS2.NtupleMaker.jptMaker_cfi import jptMaker
-
-JPTCorrections = cms.Sequence(trackExtrapolator * ak5JTA * JetPlusTrackCorrectionsAntiKt5 * jptMaker)
+JPTCorrections = cms.Sequence(trackExtrapolator * ak5JTA * JetPlusTrackCorrectionsAntiKt5)
