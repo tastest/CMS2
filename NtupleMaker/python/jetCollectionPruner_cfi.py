387a321266a9a6b4da4913dc9a55c6cb1346cc6d
kalavase <kalavase>
2010-07-27 19:41:29 +0000

    now we do all three jet collections at one time

--- NtupleMaker/python/jetCollectionPruner_cfi.py
+++ NtupleMaker/python/jetCollectionPruner_cfi.py
@@ -3,13 +3,14 @@ import FWCore.ParameterSet.Config as cms
 from RecoJets.JetProducers.ak5JetID_cfi import ak5JetID
 
 prunedUncorrectedCMS2Jets = cms.EDFilter("JetCollectionPruner",
-   inputUncorrectedJetCollection = cms.InputTag("ak5CaloJets"),
-   CaloJetCorrectorL2L3          = cms.string('ak5CaloL2L3'),
-   uncorrectedJetPtCut           = cms.double(5.0), ##cut on the UNCORRECTED reco jets!!!!!
-   usecorrectedCut               = cms.bool(False), #if you want to keep OR of passing uncorrected and corrected cuts
-   correctedJetPtCut             = cms.double(10.0), ##cut on the CORRECTED reco jets!!!!!										 
+                                         inputUncorrectedJPTJetCollection = cms.InputTag("JetPlusTrackZSPCorJetAntiKt5"),
+                                         inputUncorrectedPFJetCollection  = cms.InputTag("ak5PFJets"),
+                                         inputUncorrectedTrkJetCollection = cms.InputTag("ak5TrackJets"),
+                                         uncorrectedJPTJetPtCut           = cms.double(8.0), ##cut on uncorrected JPT jets!!!!!
+                                         uncorrectedTrkJetPtCut           = cms.double(5.0),                                                   
+                                         uncorrectedPFJetPtCut            = cms.double(7.0)
 )
-
+                                         
 
 cms2ak5JetID = ak5JetID.clone()
-cms2ak5JetID.src = cms.InputTag("prunedUncorrectedCMS2Jets")
+cms2ak5JetID.src = cms.InputTag("prunedUncorrectedCMS2Jets", "calojet")
