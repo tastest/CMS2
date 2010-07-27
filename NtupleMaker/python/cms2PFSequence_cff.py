86ca4e6cdc43c33eff25aeced064a1aabfdf30e4
kalavase <kalavase>
2010-07-27 19:39:33 +0000

    added pf btag maker here

--- NtupleMaker/python/cms2PFSequence_cff.py
+++ NtupleMaker/python/cms2PFSequence_cff.py
@@ -1,26 +1,27 @@
 import FWCore.ParameterSet.Config as cms
 
-from CMS2.NtupleMaker.pfElectronSequence_cff import *
-from CMS2.NtupleMaker.pfMuonSequence_cff import *
-from CMS2.NtupleMaker.pfJetMaker_cfi import *
-from CMS2.NtupleMaker.pfmetMaker_cfi import *
-from CMS2.NtupleMaker.pftauMaker_cfi import *
-from CMS2.NtupleMaker.pfMuToMuAssMaker_cfi import *
-from CMS2.NtupleMaker.muToPFMuAssMaker_cfi import *
-from CMS2.NtupleMaker.pfElToElAssMaker_cfi import *
-from CMS2.NtupleMaker.elToPFElAssMaker_cfi import *
+from CMS2.NtupleMaker.pfElectronSequence_cff                               import *
+from CMS2.NtupleMaker.pfMuonSequence_cff                                   import *
+from CMS2.NtupleMaker.pfJetMaker_cfi                                       import *
+from CMS2.NtupleMaker.pfmetMaker_cfi                                       import *
+from CMS2.NtupleMaker.pftauMaker_cfi                                       import *
+from CMS2.NtupleMaker.pfMuToMuAssMaker_cfi                                 import *
+from CMS2.NtupleMaker.muToPFMuAssMaker_cfi                                 import *
+from CMS2.NtupleMaker.pfElToElAssMaker_cfi                                 import *
+from CMS2.NtupleMaker.elToPFElAssMaker_cfi                                 import *
+from CMS2.NtupleMaker.bTagPFJetMaker_cfi                                   import *
 
-from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllMuons_cfi import *
-from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllElectrons_cfi import *
-from PhysicsTools.PFCandProducer.TopProjectors.pfNoMuon_cfi import *
-from PhysicsTools.PFCandProducer.Isolation.pfIsolatedMuons_cfi import *
-from PhysicsTools.PFCandProducer.pfNoPileUp_cff  import *
-from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllNeutralHadrons_cfi  import *
+from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllMuons_cfi          import *
+from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllElectrons_cfi      import *
+from PhysicsTools.PFCandProducer.TopProjectors.pfNoMuon_cfi                import *
+from PhysicsTools.PFCandProducer.Isolation.pfIsolatedMuons_cfi             import *
+from PhysicsTools.PFCandProducer.pfNoPileUp_cff                            import *
+from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllNeutralHadrons_cfi import *
 from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllChargedHadrons_cfi import *
-from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllPhotons_cfi import *
-from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllMuons_cfi import *
-from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllElectrons_cfi import *
-from PhysicsTools.PFCandProducer.pfMuons_cff import *
+from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllPhotons_cfi        import *
+from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllMuons_cfi          import *
+from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllElectrons_cfi      import *
+from PhysicsTools.PFCandProducer.pfMuons_cff                               import *
 
 CMS2pfIsolatedMuons = pfIsolatedMuons.clone()
 CMS2pfIsolatedMuons.src = cms.InputTag("pfAllMuons")
@@ -34,6 +35,6 @@ CMS2pfNoMuon.topCollection = cms.InputTag("CMS2pfIsolatedMuons")
 CMS2pfAllElectrons = pfAllElectrons.clone()
 CMS2pfAllElectrons.src = cms.InputTag("CMS2pfNoMuon")
 
-cms2PFSequence = cms.Sequence(pfJetMaker + pfmetMaker + pftauMaker + pfNoPileUpSequence + pfAllNeutralHadrons + pfAllChargedHadrons + pfAllPhotons + pfAllMuons + CMS2pfMuonIsolationSequence + CMS2pfIsolatedMuons + CMS2pfNoMuon + CMS2pfAllElectrons + pfMuonMaker + pfMuToMuAssMaker + muToPFMuAssMaker + CMS2pfElectronIsolationSequence + pfElectronMaker + pfElToElAssMaker + elToPFElAssMaker)
+cms2PFSequence = cms.Sequence(pfJetMaker + pfmetMaker + pftauMaker + pfNoPileUpSequence + pfAllNeutralHadrons + pfAllChargedHadrons + pfAllPhotons + pfAllMuons + CMS2pfMuonIsolationSequence + CMS2pfIsolatedMuons + CMS2pfNoMuon + CMS2pfAllElectrons + pfMuonMaker + pfMuToMuAssMaker + muToPFMuAssMaker + CMS2pfElectronIsolationSequence + pfElectronMaker + pfElToElAssMaker + elToPFElAssMaker + bTagPFJetMaker)
 
-cms2PFNoTauSequence = cms.Sequence(pfJetMaker + pfmetMaker + pfNoPileUpSequence + pfAllNeutralHadrons + pfAllChargedHadrons + pfAllPhotons + pfAllMuons + CMS2pfMuonIsolationSequence + CMS2pfIsolatedMuons + CMS2pfNoMuon + CMS2pfAllElectrons + pfMuonMaker + pfMuToMuAssMaker + muToPFMuAssMaker + CMS2pfElectronIsolationSequence + pfElectronMaker + pfElToElAssMaker + elToPFElAssMaker)
+cms2PFNoTauSequence = cms.Sequence(pfJetMaker + pfmetMaker + pfNoPileUpSequence + pfAllNeutralHadrons + pfAllChargedHadrons + pfAllPhotons + pfAllMuons + CMS2pfMuonIsolationSequence + CMS2pfIsolatedMuons + CMS2pfNoMuon + CMS2pfAllElectrons + pfMuonMaker + pfMuToMuAssMaker + muToPFMuAssMaker + CMS2pfElectronIsolationSequence + pfElectronMaker + pfElToElAssMaker + elToPFElAssMaker + bTagPFJetMaker)
