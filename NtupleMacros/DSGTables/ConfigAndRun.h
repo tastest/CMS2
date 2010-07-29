3b619432c089844a74d383fc458d66403475c986
jmuelmen <jmuelmen>
2010-07-29 11:53:32 +0000

    updated list of samples

--- NtupleMacros/DSGTables/ConfigAndRun.h
+++ NtupleMacros/DSGTables/ConfigAndRun.h
@@ -17,11 +17,8 @@ enum {
      LOOP_WZ	,
      LOOP_ZZ	,
      LOOP_WJETS	,
-     LOOP_DYEE	,
-     LOOP_DYMM	,
-     LOOP_DYTT	,
+     LOOP_DY	,
      LOOP_TTBAR	,
-     LOOP_TW	,
 };
 
 template <class Looper> int run (cuts_t cuts, const string &name, uint32 which_ones = 0xffffffff)
@@ -31,11 +28,15 @@ template <class Looper> int run (cuts_t cuts, const string &name, uint32 which_o
      const string log = name + ".log";
      Sample data = 
 	  fWW()          +
-	  fWZ()          +
-	  fZZ()          +
-	  fWjets()       +
-	  fDYee()        +
-	  fttbar()       +
+ 	  fWZ()          +
+ 	  fZZ()          +
+ 	  fWe()       +
+ 	  fWmu()       +
+ 	  fWtau()       +
+ 	  fZee()        +
+ 	  fZmm()        +
+ 	  fZtt()        +
+ 	  fttbar()       +
 	  fLM8();         
      data.name = "data";
      // by default, we run this list of samples; if we're told by the
@@ -43,24 +44,26 @@ template <class Looper> int run (cuts_t cuts, const string &name, uint32 which_o
      Looper looper_ww		(fWW()		, cuts, log.c_str());	if (which_ones & (1 << LOOP_WW    )) looper_ww          .Loop();
      Looper looper_wz		(fWZ()		, cuts, log.c_str());	if (which_ones & (1 << LOOP_WZ    )) looper_wz          .Loop();
      Looper looper_zz		(fZZ()		, cuts, log.c_str());	if (which_ones & (1 << LOOP_ZZ    )) looper_zz          .Loop();
-     Looper looper_wjets	(fWjets()	, cuts, log.c_str());	if (which_ones & (1 << LOOP_WJETS )) looper_wjets       .Loop();
-     Looper looper_dyee		(fDYee()	, cuts, log.c_str());	if (which_ones & (1 << LOOP_DYEE  )) looper_dyee        .Loop();
-     Looper looper_dymm		(fDYmm()	, cuts, log.c_str());	if (which_ones & (1 << LOOP_DYMM  )) looper_dymm        .Loop();
-     Looper looper_dytt		(fDYtt()	, cuts, log.c_str());	if (which_ones & (1 << LOOP_DYTT  )) looper_dytt        .Loop();
+     Looper looper_we		(fWe()		, cuts, log.c_str());	if (which_ones & (1 << LOOP_WJETS )) looper_we	        .Loop();
+     Looper looper_wmu		(fWmu()		, cuts, log.c_str());	if (which_ones & (1 << LOOP_WJETS )) looper_wmu	        .Loop();
+     Looper looper_wtau		(fWtau()	, cuts, log.c_str());	if (which_ones & (1 << LOOP_WJETS )) looper_wtau        .Loop();
+     Looper looper_dyee		(fZee()	, cuts, log.c_str());	if (which_ones & (1 << LOOP_DY  )) looper_dyee        .Loop();
+     Looper looper_dymm		(fZmm()	, cuts, log.c_str());	if (which_ones & (1 << LOOP_DY  )) looper_dymm        .Loop();
+     Looper looper_dytt		(fZtt()	, cuts, log.c_str());	if (which_ones & (1 << LOOP_DY  )) looper_dytt        .Loop();
      Looper looper_ttbar	(fttbar()	, cuts, log.c_str());	if (which_ones & (1 << LOOP_TTBAR )) looper_ttbar       .Loop();
-     Looper looper_tw		(ftW()		, cuts, log.c_str());	if (which_ones & (1 << LOOP_TW    )) looper_tw          .Loop();
      Looper looper_data		(data		, cuts, log.c_str());	looper_data          .Loop();
      // then we collect them all and print a table
      const Looper *loopers[] = { 
 	  &looper_ww          ,
 	  &looper_wz          ,
 	  &looper_zz          ,
-	  &looper_wjets       ,
+	  &looper_we       ,
+	  &looper_wmu       ,
+	  &looper_wtau      ,
 	  &looper_dyee        ,
 	  &looper_dymm        ,
 	  &looper_dytt        ,
 	  &looper_ttbar       ,
-	  &looper_tw          ,
 	  &looper_data        ,
      };
      // when all the loopers are done, we save the histograms to file
@@ -70,6 +73,7 @@ template <class Looper> int run (cuts_t cuts, const string &name, uint32 which_o
      for (unsigned int i = 0; i < sizeof(loopers) / sizeof(Looper *); ++i) {
 // 	  tables->tables_.push_back(loopers[i]->dsgTable);
 	  loopers[i]->dsgTable.Write();
+	  loopers[i]->dsgTable_emu.Write();
      }
 //      tables->Write();
      f->Close();
