6e31f7bf88aecdbb956fa42a6fb1e8125d136fbd
olivito <olivito>
2010-08-12 18:40:58 +0000

    This commit was manufactured by cvs2svn to create tag 'genericsearch_V00-01-00'.

--- NtupleMaker/python/pfmetMaker_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-pfmetMaker = cms.EDProducer("PFMETMaker",
-	aliasPrefix = cms.untracked.string("evt"),
-                          pfMetInputTag_ = cms.InputTag("pfMet")
-)
-
-
