4e90d9fc9c5f36754a10f1cc435817d8c2ae0d31
gutsche <gutsche>
2010-08-13 23:21:03 +0000

    scripts to merge and compare JSON files

--- NtupleMacros/CreateJSON/doAll.C
+++ NtupleMacros/CreateJSON/doAll.C
@@ -6,12 +6,12 @@
 
 	TChain *ch = new TChain("Events"); 
 
-	string dataset = prefix + "MinimumBias_Commissioning10-SD_EG-Jun14thSkim_v1_RECO/V03-04-26-02/merge*.root";
-	ch->Add(dataset.c_str());
+	// string dataset = prefix + "MinimumBias_Commissioning10-SD_EG-Jun14thSkim_v1_RECO/V03-04-26-02/merge*.root";
+	// ch->Add(dataset.c_str());
 	// dataset = prefix + "MinimumBias_Commissioning10-SD_Mu-Jun14thSkim_v1_RECO/V03-04-26-02/merge*.root";
 	// ch->Add(dataset.c_str());
-	// dataset = prefix + "EG_Run2010A-Jun14thReReco_v1_RECO/V03-04-26-01/merge*.root";
-	// ch->Add(dataset.c_str());
+	dataset = prefix + "EG_Run2010A-Jun14thReReco_v1_RECO/V03-04-26-01/merge*.root";
+	ch->Add(dataset.c_str());
 	// dataset = prefix + "Mu_Run2010A-Jun14thReReco_v1_RECO/V03-04-26-01/merge*.root";
 	// ch->Add(dataset.c_str());
 	// dataset = prefix + "EG_Run2010A-Jul16thReReco-v2_RECO/V03-04-26-07/merge*.root";
