20c0eb9e9f8b2767e00e417f85ca27e1a4ac39cd
dlevans <dlevans>
2010-09-13 14:34:16 +0000

    first commit of pdf analysis package

---
+++ NtupleMacros/PDFAnalysis/doMC.C
@@ -0,0 +1,79 @@
+
+void doMC() {
+
+    //
+    // the looper
+    //
+    gSystem->Load("libTree.so");
+    gSystem->Load("libPhysics.so");
+    gSystem->Load("libEG.so");
+    gSystem->Load("libMathCore.so");
+    gSystem->Load("/tas07/disk00/jribnik/lhapdf-5.8.3/lib/.libs/libLHAPDF.so");
+
+    gSystem->Load("libCMS2NtupleMacrosCORE.so");
+    gSystem->Load("libCMS2NtupleMacrosLooper.so");
+
+    gSystem->Load("../Tools/MiniFWLite/libMiniFWLite.so");
+    gROOT->ProcessLine(".L histtools.C+");
+
+    //
+    // output file for histograms
+    //
+
+    MyScanChain *looper = new MyScanChain();
+
+    //
+    // specify the PDF set to use
+    // this should be a valid LHgrid file
+    //
+    looper->specifyPDF("/tas07/disk00/jribnik/lhapdf/share/lhapdf/PDFsets/cteq61");
+
+    //
+    // chains for input files
+    //TString ntuple_location = "/nfs-3/userdata/cms2/";
+    TString ntuple_location = "/tas/cms2/";
+
+    // ttbar
+    TChain *chain_ttbar = new TChain("Events");
+    chain_ttbar->Add(ntuple_location + "TTbar_Spring10-START3X_V26_S09-v1/V03-04-08/merged_ntuple*.root");
+
+    TChain *chain_ttbarmg = new TChain("Events");
+    chain_ttbarmg->Add(ntuple_location + "TTbarJets-madgraph_Spring10-START3X_V26_S09-v1/V03-04-13-07/diLepPt2010Skim/*.root");
+
+    // zee
+    TChain *chain_zee = new TChain("Events");
+    chain_zee->Add(ntuple_location + "EarlyDataSamples/Zee_Spring10-START3X_V26_S09-v1/merged_ntuple*.root");
+
+    // WW
+    TChain *chain_ww = new TChain("Events");
+    chain_ww->Add(ntuple_location + "WW_Spring10-START3X_V26_S09-v1/V03-04-13-07/merged_ntuple.root");
+
+    //
+    // run it
+    //
+
+    //looper->ScanChain("ttbar", chain_ttbar, (157.5/165.0));
+    //looper->ScanChain("ttbarmg", chain_ttbarmg, (157.5/165.0));
+    looper->ScanChain("dyee", chain_zee, (1666.0/1300.0));
+    looper->ScanChain("ww", chain_ww);
+
+    //
+    // write histograms
+    // 
+
+    const char* outFile = "histos_mc.root";
+    hist::saveHist(outFile); 
+    hist::deleteHistos();
+
+    //
+    // tidy up
+    //
+
+    delete looper;
+    delete chain_ttbar;
+    delete chain_ttbarmg;
+    delete chain_zee;
+    delete chain_ww;
+
+}
+
