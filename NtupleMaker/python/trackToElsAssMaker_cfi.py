a89644911ab7a7a7c753448197d044d89d4f8945
olivito <olivito>
2010-09-27 16:41:52 +0000

    This commit was manufactured by cvs2svn to create tag 'V01-01'.

--- NtupleMaker/python/trackToElsAssMaker_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-trackToElsAssMaker = cms.EDProducer("TrackToElAssMaker",
-	aliasPrefix = cms.untracked.string("trks"),
-    electronsInputTag = cms.InputTag("gsfElectrons"),
-    tracksInputTag    = cms.InputTag("generalTracks")
-)
-
-
