006b7d7f2d602622971b43707d738723fec27122
benhoob <benhoob>
2011-02-15 12:01:40 +0000

    Update selection and add a few more plots

--- NtupleMacros/HuntGather2011/plotter/makeGatherPlotsOS.C
+++ NtupleMacros/HuntGather2011/plotter/makeGatherPlotsOS.C
@@ -8,26 +8,38 @@ void makeGatherPlotsOS(const std::vector<BabySample*> &babyVector, const float &
     //
     // Define specific cuts for OS plots
     //
+    TCut os_pt2010("(pt1 > 20 && pt2 > 10) || (pt1 > 10 && pt2 > 20)");
+    TCut os_dilep2("(abs(eormu1) == 11 && e1_vbtf90full) || (abs(eormu1) == 13 && mu1_muonidfull && ! mu1_cosmic)");
+    TCut os_dilep3("(abs(eormu2) == 11 && e2_vbtf90full) || (abs(eormu2) == 13 && mu2_muonidfull && ! mu2_cosmic)");
+    TCut os_dilep4("ntrks > 2");
+    TCut os_dilep5("eormu1*eormu2<0");
+    TCut os_dilep_all("base_dilep",os_pt2010+os_dilep2+os_dilep3+os_dilep4+os_dilep5);
+    TCut os_zveto("(((hyp_type == 0 || hyp_type == 3) && eormu1*eormu2 < 0) && abs(mass-91.) > 15.) || ((hyp_type == 0 || hyp_type == 3) && eormu1*eormu2 > 0) || (hyp_type == 1 || hyp_type == 2)");
+    TCut os_kin("njets>1 && tcmet>50. && sumjetpt>100.");
+    TCut os_preselection("os_preselection",os_dilep_all+os_zveto+os_kin);
 
-    TCut osanal_dilep   ("osanal_dilep"   ,inclusivenonz_dilep1+base_dilep+os_dilep+"tcmet>50.&&sumjetpt>100.");
-    TCut osanal_of_dilep("osanal_of_dilep",osanal_dilep+of_dilep);
-    TCut osanal_sf_dilep("osanal_sf_dilep",osanal_dilep+sf_dilep);
-
+    TCut y("y","tcmet/sqrt(sumjetpt)");
     //
     // Apply preselection cuts to the samples in the baby vector
     // These preselection cuts will apply to all plots!
     //
 
-    PreselectBabies(babyVector, osanal_dilep);
+    PreselectBabies(babyVector, os_dilep_all);
 
     //
     // Make the plots
     //
 
-    DrawAll("mass", "os_of_mass", osanal_of_dilep, luminosity, 40, 0., 500., 0, babyVector);
-    DrawAll("mass", "os_sf_mass", osanal_sf_dilep, luminosity,40, 0., 500.,0, babyVector);
-    DrawAll("sumjetpt", "os_sumjetpt_int", osanal_dilep, luminosity,40, 0.,800., 1, babyVector);
-    DrawAll("tcmet", "os_tcmet_int", osanal_dilep, luminosity, 40, 0.,300., 1, babyVector);
+    DrawAll("mass",                    "os_mass",            os_preselection, luminosity, 40, 0., 500, 0, babyVector);
+    DrawAll("tcmet",                   "os_tcmet",           os_preselection, luminosity, 40, 0., 300, 0, babyVector);
+    DrawAll( y,                        "os_y",               os_preselection, luminosity, 40, 0.,  20, 0, babyVector);
+    DrawAll("sumjetpt",                "os_sumjetpt",        os_preselection, luminosity ,40, 0., 500, 0, babyVector);
+    DrawAll("njets",                   "os_njets",           os_preselection, luminosity , 5, 0.,   5, 0, babyVector);
+
+
+
+  
+  
 
 }
 
