7ef853e79f7b4d7f48fa28f681188c9e03be518b
fgolf <fgolf>
2011-03-15 23:14:09 +0000

    remove tcMetWithPFclusters branches that aren't available in 3_9_X

--- NtupleMaker/interface/TCMETMaker.h
+++ NtupleMaker/interface/TCMETMaker.h
@@ -13,7 +13,7 @@
 //
 // Original Author:  Puneeth Kalavase
 //         Created:  Fri Jun  6 11:07:38 CDT 2008
-// $Id: TCMETMaker.h,v 1.6 2010/11/09 10:21:30 benhoob Exp $
+// $Id: TCMETMaker.h,v 1.6.2.1 2011/03/15 23:14:09 fgolf Exp $
 //
 //
 
@@ -45,10 +45,9 @@ private:
   virtual void endJob() ;
 
   // ----------member data ---------------------------
-  edm::InputTag muon_tag;
-  edm::InputTag tcmet_tag;
-  edm::InputTag pftcmet_tag;
-  edm::InputTag tcmet_vm_tag;  
+    edm::InputTag muon_tag;
+    edm::InputTag tcmet_tag;
+    edm::InputTag tcmet_vm_tag;  
 	std::string aliasprefix_;
 };
 
--- NtupleMaker/python/tcmetMaker_cfi.py
+++ NtupleMaker/python/tcmetMaker_cfi.py
@@ -4,7 +4,6 @@ tcmetMaker = cms.EDProducer("TCMETMaker",
                             aliasPrefix = cms.untracked.string("evt"),
                             muon_tag_     = cms.InputTag("muons"),
                             tcmet_tag_    = cms.InputTag("tcMet"),
-                            pftcmet_tag_  = cms.InputTag("tcMetWithPFclusters"),
                             tcmet_vm_tag_ = cms.InputTag("muonTCMETValueMapProducer", "muCorrData")
 )
 
