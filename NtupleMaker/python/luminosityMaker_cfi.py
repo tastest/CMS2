71201715ff5b315e9ba7f5cead767d1c34ecb092
olivito <olivito>
2011-03-22 14:20:41 +0000

    This commit was manufactured by cvs2svn to create tag 'tp_V00-02-00'.

--- NtupleMaker/python/luminosityMaker_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-luminosityMaker = cms.EDProducer("LuminosityMaker",
-                                 aliasPrefix = cms.untracked.string("ls"),
-                                 lumiSummaryInputTag = cms.InputTag("lumiProducer")
-                                 )
-
-
