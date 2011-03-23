7b228d38e4caca86b7c3d563dab86e0ad83312ba
yygao <yygao>
2011-03-23 13:01:03 +0000

    update

--- NtupleMacros/HWWME/MCUtil.h
+++ NtupleMacros/HWWME/MCUtil.h
@@ -13,82 +13,75 @@
 #include "TH1F.h"
 #include "TAxis.h"
 
-TFile *mcutilFile_;
+TH2F  *els_numer_mc_;
+TH2F  *els_denom_mc_;
+TH2F  *els_eff_mc_;
 
-TH2F  *els_numer_mc;
-TH2F  *els_denom_mc;
-TH2F  *els_eff_mc;
+TH1F  *els_numer_mc_eta_;
+TH1F  *els_denom_mc_eta_;
+TH1F  *els_eff_mc_eta_;
 
-TH1F  *els_numer_mc_eta;
-TH1F  *els_denom_mc_eta;
-TH1F  *els_eff_mc_eta;
+TH1F  *els_numer_mc_pt_;
+TH1F  *els_denom_mc_pt_;
+TH1F  *els_eff_mc_pt_;
 
-TH1F  *els_numer_mc_pt;
-TH1F  *els_denom_mc_pt;
-TH1F  *els_eff_mc_pt;
+TH2F  *mus_numer_mc_;
+TH2F  *mus_denom_mc_;
+TH2F  *mus_eff_mc_;
 
-TH2F  *mus_numer_mc;
-TH2F  *mus_denom_mc;
-TH2F  *mus_eff_mc;
+TH1F  *mus_numer_mc_eta_;
+TH1F  *mus_denom_mc_eta_;
+TH1F  *mus_eff_mc_eta_;
 
-TH1F  *mus_numer_mc_eta;
-TH1F  *mus_denom_mc_eta;
-TH1F  *mus_eff_mc_eta;
+TH1F  *mus_numer_mc_pt_;
+TH1F  *mus_denom_mc_pt_;
+TH1F  *mus_eff_mc_pt_;
 
-TH1F  *mus_numer_mc_pt;
-TH1F  *mus_denom_mc_pt;
-TH1F  *mus_eff_mc_pt;
+TH1F *kx_;
+TH1F *ky_;
 
-TH1F *kx;
-TH1F *ky;
-TH1F *kt;
+// These histograms are used for the yield counting in ME
+TH1F *dilmass_ee_;
+TH1F *dilmass_mm_;
+TH1F *dilmass_em_;
 
-TH1F *nux;
-TH1F *nuy;
-TH1F *nuz;
+void InitMCUtilHist(const char* process, TFile *utilFile_) {
+  cout << "InitMCUtilHist(): " << " process = " << process <<endl; 
 
+  utilFile_->cd();
+  els_numer_mc_ = new TH2F(Form("%s_heleNumer", process), "Number of electron numerators", 20, -2.5, 2.5, 20, 0, 100);
+  els_denom_mc_ = new TH2F(Form("%s_heleDenom",process),  "Number of electron denominators", 20, -2.5, 2.5, 20, 0, 100);
+  els_eff_mc_ = new TH2F(Form("%s_heleEff",process),  "Electron Efficiency", 20, -2.5, 2.5, 20, 0, 100);
 
-void InitMCUtilHist(std::string process) {
-  cout << "InitMCUtilHist()"<<endl; 
+  els_numer_mc_eta_ = new TH1F(Form("%s_heleNumerEta",process), "Number of electron numerators", 20, -2.5, 2.5);
+  els_denom_mc_eta_ = new TH1F(Form("%s_heleDenomEta",process), "Number of electron denominators", 20, -2.5, 2.5);
+  els_eff_mc_eta_ = new TH1F(Form("%s_heleEffEta",process), "Electron Efficiency", 20, -2.5, 2.5);
 
-  if(process != "")
-    mcutilFile_ = TFile::Open(string(process + "_MCUtil.root").c_str(),"RECREATE");
-  else 
-    mcutilFile_ = TFile::Open("MCUtil.root","RECREATE");
+  els_numer_mc_pt_ = new TH1F(Form("%s_heleNumerPt",process), "Number of electron numerators", 20, 0, 100);
+  els_denom_mc_pt_ = new TH1F(Form("%s_heleDenomPt",process), "Number of electron denominators", 20, 0, 100);
+  els_eff_mc_pt_ = new TH1F(Form("%s_heleEffPt",process), "Electron Efficiency", 20, 0, 100);
 
-  els_numer_mc = new TH2F("els_numer_mc", "els_numer_mc", 20, -2.5, 2.5, 20, 10, 100);
-  els_denom_mc = new TH2F("els_denom_mc", "els_denom_mc", 20, -2.5, 2.5, 20, 10, 100);
-  els_eff_mc = new TH2F("els_eff_mc", "els_eff_mc", 20, -2.5, 2.5, 20, 10, 100);
+  mus_numer_mc_ = new TH2F(Form("%s_hmuNumer",process), "Number of muon numerators", 20, -2.5, 2.5, 20, 0, 100);
+  mus_denom_mc_ = new TH2F(Form("%s_hmuDenom",process), "Number of muon denominators", 20, -2.5, 2.5, 20, 0, 100);
+  mus_eff_mc_ = new TH2F(Form("%s_hmuEff",process), "Muon Efficiency", 20, -2.5, 2.5, 20, 0, 100);
 
-  els_numer_mc_eta = new TH1F("els_numer_mc_eta", "els_numer_mc_eta", 20, -2.5, 2.5);
-  els_denom_mc_eta = new TH1F("els_denom_mc_eta", "els_denom_mc_eta", 20, -2.5, 2.5);
-  els_eff_mc_eta = new TH1F("els_eff_mc_eta", "els_eff_mc_eta", 20, -2.5, 2.5);
+  mus_numer_mc_eta_ = new TH1F(Form("%s_hmuNumerEta",process), "Number of muon numerators", 20, -2.5, 2.5);
+  mus_denom_mc_eta_ = new TH1F(Form("%s_hmuDenomEta",process), "Number of muon denominators", 20, -2.5, 2.5);
+  mus_eff_mc_eta_ = new TH1F(Form("%s_hmuEffEta",process),  "Muon Efficiency", 20, -2.5, 2.5);
 
-  els_numer_mc_pt = new TH1F("els_numer_mc_pt", "els_numer_mc_pt", 20, 10, 100);
-  els_denom_mc_pt = new TH1F("els_denom_mc_pt", "els_denom_mc_pt", 20, 10, 100);
-  els_eff_mc_pt = new TH1F("els_eff_mc_pt", "els_eff_mc_pt", 20, 10, 100);
+  mus_numer_mc_pt_ = new TH1F(Form("%s_hmuNumerPt",process), "Number of muon numerators", 20, 0, 100);
+  mus_denom_mc_pt_ = new TH1F(Form("%s_hmuDenomPt",process), "Number of muon denominators", 20, 0, 100);
+  mus_eff_mc_pt_ = new TH1F(Form("%s_hmuEffPt",process), "Muon Efficiency", 20, 0, 100);
 
-  mus_numer_mc = new TH2F("mus_numer_mc", "mus_numer_mc", 20, -2.5, 2.5, 20, 10, 100);
-  mus_denom_mc = new TH2F("mus_denom_mc", "mus_denom_mc", 20, -2.5, 2.5, 20, 10, 100);
-  mus_eff_mc = new TH2F("mus_eff_mc", "mus_eff_mc", 20, -2.5, 2.5, 20, 10, 100);
+  kx_ = new TH1F(Form("%s_kx",process), "System Boost in X", 50, -50, 50);
+  ky_ = new TH1F(Form("%s_ky",process), "System Boost in Y", 50, -50, 50);
 
-  mus_numer_mc_eta = new TH1F("mus_numer_mc_eta", "mus_numer_mc_eta", 20, -2.5, 2.5);
-  mus_denom_mc_eta = new TH1F("mus_denom_mc_eta", "mus_denom_mc_eta", 20, -2.5, 2.5);
-  mus_eff_mc_eta = new TH1F("mus_eff_mc_eta", "mus_eff_mc_eta", 20, -2.5, 2.5);
-
-  mus_numer_mc_pt = new TH1F("mus_numer_mc_pt", "mus_numer_mc_pt", 20, 10, 100);
-  mus_denom_mc_pt = new TH1F("mus_denom_mc_pt", "mus_denom_mc_pt", 20, 10, 100);
-  mus_eff_mc_pt = new TH1F("mus_eff_mc_pt", "mus_eff_mc_pt", 20, 10, 100);
-
-  kx = new TH1F("kx", "kx", 100, -100, 100);
-  ky = new TH1F("ky", "ky", 100, -100, 100);
-  kt = new TH1F("kt", "kt", 100, 0, 100);
-
-
-  nux = new TH1F("nux", "nux", 100, -100, 100);
-  nuy = new TH1F("nuy", "nuy", 100, -100, 100);
-  nuz = new TH1F("nuz", "nuz", 100, -300, 300);
-
+  dilmass_ee_ = new TH1F(Form("%s_dilmass_ee",process), "Di-lepton mass in ee", 50, 0, 250);
+  dilmass_ee_ ->Sumw2();
+  dilmass_em_ = new TH1F(Form("%s_dilmass_em",process), "Di-lepton mass in em", 50, 0, 250);
+  dilmass_em_ ->Sumw2();
+  dilmass_mm_ = new TH1F(Form("%s_dilmass_mm",process), "Di-lepton mass in mm", 50, 0, 250);
+  dilmass_mm_ ->Sumw2();
 
 
 }
@@ -97,7 +90,6 @@ void FillEffHist(TString process, double weight);
 void FillKtHist(TString process, double weight);
 
 // Utility Functions
-unsigned int getVVType();
 bool isIdentified( TString process);
 
 void getEff(double & numer, double & denom, double & eff, double & efferr ) 
@@ -112,9 +104,10 @@ void getEff(double & numer, double & denom, double & eff, double & efferr )
 // than weighted event counts
 // 
 
-void Fill2DEffHist(TH2F* & hist_numer, TH2F* & hist_denom, TH2F* & hist_eff) {
-  // cout << "Fill2DEffHist()" << endl;
-  if(!hist_numer | !hist_denom ) return;
+void Fill2DEffHist(TH2F* hist_numer, TH2F* hist_denom, TH2F* hist_eff) {
+  cout << "Fill2DEffHist()" << endl;
+  if(!hist_numer || !hist_denom ) return;
+  
   // check the hist_numer and hist_denom have the same bin structures
   if(   hist_numer->GetNbinsX() != hist_denom->GetNbinsX() 
 	|| hist_numer->GetNbinsY() != hist_denom->GetNbinsY() ) return;
@@ -143,9 +136,9 @@ void Fill2DEffHist(TH2F* & hist_numer, TH2F* & hist_denom, TH2F* & hist_eff) {
 // than weighted event counts
 // 
 
-void Fill1DEffHist(TH1F* & hist_numer, TH1F* & hist_denom, TH1F* & hist_eff) {
+void Fill1DEffHist(TH1F* hist_numer, TH1F* hist_denom, TH1F* hist_eff) {
   // cout << "Fill1DEffHist()" << endl;
-  if(!hist_numer | !hist_denom ) return;
+  if(!hist_numer || !hist_denom ) return;
   // check the hist_numer and hist_denom have the same bin structures
   if(    hist_numer->GetNbinsX() != hist_denom->GetNbinsX() 
 	 || hist_numer->GetXaxis()->GetXmin() != hist_denom->GetXaxis()->GetXmin()
@@ -164,51 +157,49 @@ void Fill1DEffHist(TH1F* & hist_numer, TH1F* & hist_denom, TH1F* & hist_eff) {
 }
 
 
-void saveMCUtilOutput()
+void saveMCUtilOutput(TFile *utilFile_)
 {
-    Fill2DEffHist(els_numer_mc, els_denom_mc, els_eff_mc);
-    Fill1DEffHist(els_numer_mc_eta, els_denom_mc_eta, els_eff_mc_eta);
-    Fill1DEffHist(els_numer_mc_pt, els_denom_mc_pt, els_eff_mc_pt);
-    
-    Fill2DEffHist(mus_numer_mc, mus_denom_mc, mus_eff_mc);
-    Fill1DEffHist(mus_numer_mc_eta, mus_denom_mc_eta, mus_eff_mc_eta);
-    Fill1DEffHist(mus_numer_mc_pt, mus_denom_mc_pt, mus_eff_mc_pt);
+  cout << "saveMCUtilHist()" << endl;
+  utilFile_->cd();
+  
+  Fill2DEffHist(els_numer_mc_, els_denom_mc_, els_eff_mc_);
+  Fill1DEffHist(els_numer_mc_eta_, els_denom_mc_eta_, els_eff_mc_eta_);
+  Fill1DEffHist(els_numer_mc_pt_, els_denom_mc_pt_, els_eff_mc_pt_);
+  
+  Fill2DEffHist(mus_numer_mc_, mus_denom_mc_, mus_eff_mc_);
+  Fill1DEffHist(mus_numer_mc_eta_, mus_denom_mc_eta_, mus_eff_mc_eta_);
+  Fill1DEffHist(mus_numer_mc_pt_, mus_denom_mc_pt_, mus_eff_mc_pt_);
+  
+  els_denom_mc_->Write();
+  els_numer_mc_->Write();
+  els_eff_mc_->Write();
+  
+  els_denom_mc_eta_->Write();
+  els_numer_mc_eta_->Write();
+  els_eff_mc_eta_->Write();
+  
+  els_denom_mc_pt_->Write();
+  els_numer_mc_pt_->Write();
+  els_eff_mc_pt_->Write();
+  
+  mus_denom_mc_->Write();
+  mus_numer_mc_->Write();
+  mus_eff_mc_->Write();
+  
+  mus_denom_mc_eta_->Write();
+  mus_numer_mc_eta_->Write();
+  mus_eff_mc_eta_->Write();
+  
+  mus_denom_mc_pt_->Write();
+  mus_numer_mc_pt_->Write();
+  mus_eff_mc_pt_->Write();
+  
+  kx_->Write();
+  ky_->Write();
 
-    mcutilFile_->cd();
-
-    els_denom_mc->Write();
-    els_numer_mc->Write();
-    els_eff_mc->Write();
-
-    els_denom_mc_eta->Write();
-    els_numer_mc_eta->Write();
-    els_eff_mc_eta->Write();
-
-    els_denom_mc_pt->Write();
-    els_numer_mc_pt->Write();
-    els_eff_mc_pt->Write();
-
-    mus_denom_mc->Write();
-    mus_numer_mc->Write();
-    mus_eff_mc->Write();
-
-    mus_denom_mc_eta->Write();
-    mus_numer_mc_eta->Write();
-    mus_eff_mc_eta->Write();
-
-    mus_denom_mc_pt->Write();
-    mus_numer_mc_pt->Write();
-    mus_eff_mc_pt->Write();
-
-    kx->Write();
-    ky->Write();
-    kt->Write();
-
-    nux->Write();
-    nuy->Write();
-    nuz->Write();
-    
-    mcutilFile_->Close();
+  dilmass_ee_->Write();
+  dilmass_mm_->Write();
+  dilmass_em_->Write();
   
 }
 
