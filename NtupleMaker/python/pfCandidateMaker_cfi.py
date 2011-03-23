99073c701d6ee788c78b56371ce6d70a351ef321
olivito <olivito>
2011-03-23 13:13:01 +0000

    This commit was manufactured by cvs2svn to create tag 'wwVsHww_diLep2020_v3'.

--- NtupleMaker/python/pfCandidateMaker_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-pfCandidateMaker = cms.EDProducer("PFCandidateMaker",
-                                  pfCandidatesTag     = cms.InputTag("particleFlow"),
-                                  pfElectronsTag      = cms.InputTag("particleFlow","electrons"),
-                                  tracksInputTag      = cms.InputTag("generalTracks"),
-                                  minDRelectron       = cms.double(0.1)
-)
