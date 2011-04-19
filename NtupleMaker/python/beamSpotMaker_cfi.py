33ed2cb24d3687f6b10668fb98d46417220e0390
olivito <olivito>
2011-04-19 11:14:07 +0000

    This commit was manufactured by cvs2svn to create tag 'frstudy_19Apr2011'.

--- NtupleMaker/python/beamSpotMaker_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-beamSpotMaker = cms.EDProducer("BeamSpotMaker",
-	aliasPrefix = cms.untracked.string("evt_bs"),
-    beamSpotInputTag = cms.InputTag("offlineBeamSpot")
-)
-
-
