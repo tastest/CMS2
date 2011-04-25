e7ad53e23ae6c5c061c27cb5d7730b6337808fce
dlevans <dlevans>
2011-04-25 10:02:31 +0000

    update for copying of max2011 plots as well as run2011

--- NtupleMacros/HuntGather2011/plotter/gather.sh
+++ NtupleMacros/HuntGather2011/plotter/gather.sh
@@ -12,5 +12,6 @@ eval `scramv1 runtime -sh`
 
 root -q -b makeGatherPlots.C\(\"/nfs-3/userdata/cms2/gather/\"\)
 cp /home/users/dlevans/gather/production/CMSSW_4_1_2_patch1/src/CMS2/NtupleMacros/HuntGather2011/output/run2011*.png /home/users/dlevans/public_html/gather2011/
+cp /home/users/dlevans/gather/production/CMSSW_4_1_2_patch1/src/CMS2/NtupleMacros/HuntGather2011/output/max2011*.png /home/users/dlevans/public_html/gather2011/
 touch /home/users/dlevans/public_html/gather2011/index.php
 
