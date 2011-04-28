af56dee5f8c622a77f34bf44090ea54b00a6d208
dlevans <dlevans>
2011-04-28 13:32:28 +0000

    get default daily DCS good run list

--- NtupleMacros/HuntGather2011/plotter/gather.sh
+++ NtupleMacros/HuntGather2011/plotter/gather.sh
@@ -11,12 +11,12 @@ source /code/osgcode/cmssoft/cms/cmsset_default.sh
 eval `scramv1 runtime -sh`
 
 echo "getting new dcs run list"
-curl http://dlevans.web.cern.ch/dlevans/dcs.txt > ../runlists/dcs.txt
+curl https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions11/7TeV/DCSOnly/json_DCSONLY.txt > ../runlists/dcs.txt
 python ../../Tools/convertGoodRunsList_JSON.py ../runlists/dcs.txt > ../runlists/dcs_jmu.txt
 echo $?
 
 root -q -b makeGatherPlots.C\(\"/nfs-3/userdata/cms2/gather/\"\)
 cp /home/users/dlevans/gather/production/CMSSW_4_1_2_patch1/src/CMS2/NtupleMacros/HuntGather2011/output/run2011*.png /home/users/dlevans/public_html/gather2011/
 cp /home/users/dlevans/gather/production/CMSSW_4_1_2_patch1/src/CMS2/NtupleMacros/HuntGather2011/output/max2011*.png /home/users/dlevans/public_html/gather2011/
-touch /home/users/dlevans/public_html/gather2011/index.php
+touch /home/users/dlevans/public_html/gather2011
 
