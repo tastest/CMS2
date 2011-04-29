30d7e1a88320a84d773693aface2b8e9c00a768b
olivito <olivito>
2011-04-29 18:28:44 +0000

    This commit was manufactured by cvs2svn to create tag 'tprime_2011_V00'.

--- NtupleMaker/python/mitConversionMaker_cfi.py
+++
@@ -1,8 +0,0 @@
-#import FWCore.ParameterSet.Config as cms
-#
-#mitConversionMaker = cms.EDFilter("MITConversionMaker",
-#                                  elsInputTag = cms.InputTag("gsfElectrons"),
-#                                  mitConversionsInputTag = cms.InputTag("mvfConversionRemoval"),
-#                                  ctfTrksInputTag   = cms.InputTag("generalTracks"),
-#                                  beamSpotInputTag       = cms.InputTag("offlineBeamSpot")
-#)
