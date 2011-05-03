2b53eac095a279e1205cecf99b64535503171063
olivito <olivito>
2011-05-03 18:35:58 +0000

    This commit was manufactured by cvs2svn to create tag 'huntgather2011_V00-01-02'.

--- NtupleMaker/python/pdfinfoMaker_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-pdfinfoMaker = cms.EDProducer( "PDFInfoMaker",
-        genEventInfoInputTag = cms.string("generator"),                               
-	aliasPrefix = cms.untracked.string("pdfinfo")
-)
-
-
