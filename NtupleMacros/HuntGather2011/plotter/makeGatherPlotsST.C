4e37655d5e0e31daa0e883f772de2a32cd37ed43
dlevans <dlevans>
2011-05-05 15:54:07 +0000

    update to more useful st plots

--- NtupleMacros/HuntGather2011/plotter/makeGatherPlotsST.C
+++ NtupleMacros/HuntGather2011/plotter/makeGatherPlotsST.C
@@ -6,31 +6,27 @@ void makeGatherPlotsST(TString prefix, const std::vector<BabySample*> &babyVecto
     std::cout << "Making ST plots...\n";
 
     //
-    // Define specific cuts for ST plots
-    //
-
-    TCut base_tcmeffgt400_dilep  ("base_tcmeffgt400_dilep",base_dilep+"tcmeff>400.");
-    TCut base_tcmetgt50_dilep    ("base_tcmetgt50_dilep",base_dilep+"tcmet>50.");
-    TCut base_sumjetptgt200_dilep("base_sumjetptgt200_dilep",base_dilep+"sumjetptSS>200.");
-    TCut base_dilptgt100_dilep   ("base_dilptgt100_dilep",base_dilep+"dilpt>100.");
-
-    //
     // Apply preselection cuts to the samples in the baby vector
     // These preselection cuts will apply to all plots!
     //
 
-    PreselectBabies(babyVector, base_dilep);
+    PreselectBabies(babyVector, base_dilep+"tcmeff>250");
 
     //
     // Make the plots
     //
 
-    DrawAll("njetsSS", prefix+"_meff_njetsclean",base_dilep,luminosity,10,-0.5,9.5,0, babyVector);
-    DrawAll("njetsSS", prefix+"_meff_tcmeffgt400_njetsclean",base_tcmeffgt400_dilep,luminosity,10,-0.5,9.5,0, babyVector);
-    DrawAll("tcmeff", prefix+"_meff_tcmeff_int",base_dilep,luminosity,40,0.,1000.,1, babyVector);
-    DrawAll("tcmeff", prefix+"_meff_tcmetgt50_tcmeff_int",base_tcmetgt50_dilep,luminosity,40,0.,1000.,1, babyVector);
-    DrawAll("tcmeff", prefix+"_meff_sumjetptgt200_tcmeff_int",base_sumjetptgt200_dilep,luminosity,40,0.,1000.,1, babyVector);
-    DrawAll("tcmeff", prefix+"_meff_dilptgt100_tcmeff_int",base_dilptgt100_dilep,luminosity,40,0.,1000.,1, babyVector);
+    DrawAll("tcmeff", prefix+"_tcmeff_all",TCut("base_dilep", base_dilep),luminosity,40,250.,2250., false, babyVector);
+    DrawAll("njetsSS", prefix+"_njets_tcmeff250_all",TCut("base_dilep", base_dilep+"tcmeff>250"),luminosity,10,-0.5,9.5,0, babyVector);
+
+    DrawAll("tcmeff", prefix+"_tcmeff_met50_all",TCut("tcmet>50", base_dilep + "tcmet>50"),luminosity,40,250.,2250., false, babyVector);
+    DrawAll("tcmeff", prefix+"_tcmeff_met200_all",TCut("tcmet>200", base_dilep + "tcmet>200"),luminosity,40,250.,2250., false, babyVector);
+
+    DrawAll("tcmeff", prefix+"_tcmeff_lep50_all",TCut("dilepton pt > 50", base_dilep + "dilpt>50"),luminosity,40,250.,2250., false, babyVector);
+    DrawAll("tcmeff", prefix+"_tcmeff_lep200_all",TCut("dilepton pt > 200", base_dilep + "dilpt>200"),luminosity,40,250.,2250., false, babyVector);
+
+    DrawAll("tcmeff", prefix+"_tcmeff_jet100_all", TCut("sumjetpt > 100", base_dilep + "sumjetptSS>100"),luminosity,40,250.,2250., false, babyVector);
+    DrawAll("tcmeff", prefix+"_tcmeff_jet400_all", TCut("sumjetpt > 400", base_dilep + "sumjetptSS>400"),luminosity,40,250.,2250., false, babyVector);
 
 }
 
