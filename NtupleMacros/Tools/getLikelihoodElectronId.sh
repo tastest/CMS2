189187f94d83d3ab2866c07bb07ecb4c7a8665d7
dlevans <dlevans>
2011-05-09 10:31:32 +0000

    update for new version of LH ID

--- NtupleMacros/Tools/getLikelihoodElectronId.sh
+++ NtupleMacros/Tools/getLikelihoodElectronId.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-REV=edm-Feb2011
-PDFS=/afs/cern.ch/user/e/emanuele/public/4Likelihood/PDFsSQLite/CMSSW_3_5_X/pdfs_MC.root
+#REV=edm-Feb2011
+#PDFS=/afs/cern.ch/user/e/emanuele/public/4Likelihood/PDFsSQLite/CMSSW_3_5_X/pdfs_MC.root
+REV=edm06May11
+PDFS=/afs/cern.ch/user/e/emanuele/public/4Likelihood/PDFsSQLite/CMSSW_4_1_X/pdfs_MC.root
 
 # likelihood id
-cvs co -d EgammaAnalysisTools/src/ -r $REV UserCode/emanuele/EgammaAnalysisTools/src/LikelihoodPdf.cc
-cvs co -d EgammaAnalysisTools/include/ -r $REV UserCode/emanuele/EgammaAnalysisTools/include/LikelihoodPdf.h
-cvs co -d EgammaAnalysisTools/src/ -r $REV UserCode/emanuele/EgammaAnalysisTools/src/LikelihoodSpecies.cc
-cvs co -d EgammaAnalysisTools/include/ -r $REV UserCode/emanuele/EgammaAnalysisTools/include/LikelihoodSpecies.h
-cvs co -d EgammaAnalysisTools/src/ -r $REV UserCode/emanuele/EgammaAnalysisTools/src/LikelihoodPdfProduct.cc
-cvs co -d EgammaAnalysisTools/include/ -r $REV UserCode/emanuele/EgammaAnalysisTools/include/LikelihoodPdfProduct.h
-cvs co -d EgammaAnalysisTools/src/ -r $REV UserCode/emanuele/EgammaAnalysisTools/src/ElectronLikelihood.cc
-cvs co -d EgammaAnalysisTools/include/ -r $REV UserCode/emanuele/EgammaAnalysisTools/include/ElectronLikelihood.h
+cvs co -r $REV -p UserCode/emanuele/EgammaAnalysisTools/src/LikelihoodPdf.cc             > EgammaAnalysisTools/src/LikelihoodPdf.cc
+cvs co -r $REV -p UserCode/emanuele/EgammaAnalysisTools/include/LikelihoodPdf.h          > EgammaAnalysisTools/include/LikelihoodPdf.h
+cvs co -r $REV -p UserCode/emanuele/EgammaAnalysisTools/src/LikelihoodSpecies.cc         > EgammaAnalysisTools/src/LikelihoodSpecies.cc
+cvs co -r $REV -p UserCode/emanuele/EgammaAnalysisTools/include/LikelihoodSpecies.h      > EgammaAnalysisTools/include/LikelihoodSpecies.h
+cvs co -r $REV -p UserCode/emanuele/EgammaAnalysisTools/src/LikelihoodPdfProduct.cc      > EgammaAnalysisTools/src/LikelihoodPdfProduct.cc
+cvs co -r $REV -p UserCode/emanuele/EgammaAnalysisTools/include/LikelihoodPdfProduct.h   > EgammaAnalysisTools/include/LikelihoodPdfProduct.h
+cvs co -r $REV -p UserCode/emanuele/EgammaAnalysisTools/src/ElectronLikelihood.cc        > EgammaAnalysisTools/src/ElectronLikelihood.cc
+cvs co -r $REV -p UserCode/emanuele/EgammaAnalysisTools/include/ElectronLikelihood.h     > EgammaAnalysisTools/include/ElectronLikelihood.h
 
 # other includes needed
-cvs co -d EgammaAnalysisTools/include -r $REV UserCode/emanuele/EgammaAnalysisTools/include/LikelihoodMeasurements.h
-cvs co -d EgammaAnalysisTools/include -r $REV UserCode/emanuele/EgammaAnalysisTools/include/LikelihoodSwitches.h
+cvs co -r $REV -p UserCode/emanuele/EgammaAnalysisTools/include/LikelihoodMeasurements.h > EgammaAnalysisTools/include/LikelihoodMeasurements.h
+cvs co -r $REV -p UserCode/emanuele/EgammaAnalysisTools/include/LikelihoodSwitches.h     > EgammaAnalysisTools/include/LikelihoodSwitches.h
 
 # my example and makefile
-cvs co -p UserCode/DLEvans/LikelihoodElectronId/Makefile > EgammaAnalysisTools/Makefile
-cvs co -p UserCode/DLEvans/LikelihoodElectronId/Makefile.arch > EgammaAnalysisTools/Makefile.arch
-cvs co -p UserCode/DLEvans/LikelihoodElectronId/include/LikelihoodUtil.h > EgammaAnalysisTools/include/LikelihoodUtil.h
-cvs co -p UserCode/DLEvans/LikelihoodElectronId/src/LikelihoodUtil.cc > EgammaAnalysisTools/src/LikelihoodUtil.cc
+cvs co -p UserCode/DLEvans/LikelihoodElectronId/Makefile >                  EgammaAnalysisTools/Makefile
+cvs co -p UserCode/DLEvans/LikelihoodElectronId/Makefile.arch >             EgammaAnalysisTools/Makefile.arch
+cvs co -p UserCode/DLEvans/LikelihoodElectronId/include/LikelihoodUtil.h >  EgammaAnalysisTools/include/LikelihoodUtil.h
+cvs co -p UserCode/DLEvans/LikelihoodElectronId/src/LikelihoodUtil.cc >     EgammaAnalysisTools/src/LikelihoodUtil.cc
 
 # make directory to hold libs
 if [ ! -d EgammaAnalysisTools/lib ]; then
