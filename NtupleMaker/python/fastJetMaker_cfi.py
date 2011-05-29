7b7c951387328bec6e7cc7bbe44000209f73868d
olivito <olivito>
2011-05-29 21:58:23 +0000

    This commit was manufactured by cvs2svn to create tag 'huntgather2011_V00-01-07'.

--- NtupleMaker/python/fastJetMaker_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-fastJetMaker = cms.EDProducer("FastJetMaker",
-                              aliasPrefix = cms.untracked.string("evt"),
-                              rhoJEC_tag     = cms.InputTag("kt6PFJetsDeterministicJEC","rho"),
-                              rhoIso_tag     = cms.InputTag("kt6PFJetsDeterministicIso","rho")
-                              )
--- NtupleMaker/python/hcalNoiseSummaryMaker_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hcalNoiseSummaryMaker = cms.EDProducer("HcalNoiseSummaryMaker",
-	aliasPrefix = cms.untracked.string("hcalnoise"),
-                                     hcalNoiseSummaryTag = cms.InputTag("hcalnoise")
-)
