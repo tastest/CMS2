9696e6d84c0159906048b08aacc76835b55bf36c
dmytro <dmytro>
2011-06-03 22:33:58 +0000

    *** empty log message ***

--- NtupleMacros/WW/smurfAnalysis.cxx
+++ NtupleMacros/WW/smurfAnalysis.cxx
@@ -1,19 +1,31 @@
 {
   gSystem->CompileMacro("../../../Smurf/Core/SmurfTree.h","k");
   gSystem->CompileMacro("smurfAnalysis.C","k");
-  SmurfAnalysis a(0.191,"/smurf/data/Run2011_Spring11_SmurfV4/tas-TightLooseFullMET-alljets");
+  SmurfAnalysis a(0.191,"/smurf/data/Run2011_Spring11_SmurfV5/tas-TightLooseFullMET-alljets");
   // SmurfAnalysis a(0.191,"/smurf/dmytro/samples/smurf", "files/ww_el_fr_smurfV4.root", "el_fr_v4", "files/ww_mu_fr_smurfV4.root", "mu_fr_m2");
-  a.setGoodRunList("Cert_160404-163869_7TeV_PromptReco_Collisions11_JSON.txt");
+  // a.setGoodRunList("Cert_160404-163869_7TeV_PromptReco_Collisions11_JSON.txt");
 
   cout << "\n****************** WW 0-jet Selection *******************\n" << endl;
   a.measurement_.sig_type = SmurfTree::qqww;
   a.measurement_.type = SmurfAnalysis::WW0Jet;
   a.processSamples();
-  a.showYields();
+  a.showYields(1.0);
+  cout << "\n****************** WW 1-jet Selection *******************\n" << endl;
+  a.measurement_.sig_type = SmurfTree::qqww;
+  a.measurement_.type = SmurfAnalysis::WW1Jet;
+  a.processSamples();
+  a.showYields(1.0);
+  cout << "\n****************** WW 2-jet Selection *******************\n" << endl;
+  a.measurement_.sig_type = SmurfTree::qqww;
+  a.measurement_.type = SmurfAnalysis::WW2Jets;
+  a.processSamples();
+  a.showYields(1.0);
+
+  return;
+
   a.estimateFakeBackground();
   a.estimateTopBackground();
   a.estimateDYBackground();
-  return;
 
   cout << "\n****************** WW 1-jet Selection *******************\n" << endl;
   a.measurement_.sig_type = SmurfTree::qqww;
