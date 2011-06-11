dd5aeaf68b0e1fe04b9be3371453f5d71d26f2d5
olivito <olivito>
2011-06-11 00:30:08 +0000

    This commit was manufactured by cvs2svn to create tag 'huntgather2011_V00-01-15'.

--- NtupleMaker/python/fastJetSequence_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from RecoJets.Configuration.RecoPFJets_cff import *
-from CMS2.NtupleMaker.fastJetMaker_cfi import *
-
-kt6PFJetsForRhoComputation = kt6PFJets.clone(doRhoFastjet = True)
-
-fastJetSequence = cms.Sequence( kt6PFJets * kt6PFJetsForRhoComputation * fastJetMaker )
