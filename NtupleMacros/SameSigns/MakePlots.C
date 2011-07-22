de126fc0cd0f23ead27f7221d454d12a6ab136e6
olivito <olivito>
2011-07-22 14:16:17 +0000

    This commit was manufactured by cvs2svn to create tag 'V04-02-20-02'.

--- NtupleMacros/SameSigns/MakePlots.C
+++
@@ -1,6 +0,0 @@
-void MakePlots(TString histofilename){
-  gROOT->ProcessLine(".x ../Tools/setup.C");
-  loadHist(histofilename);
-  histofilename+="_out";
-  browseStacks(true, false, histofilename);
-}
