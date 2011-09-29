100e68442038c855b04065a5469ed221fbd9d690
fgolf <fgolf>
2011-09-29 18:12:43 +0000

    load libTMVA.so needed for electron ID MVA

--- NtupleMacros/HuntGather2011/babymaker/makeGatherBaby.C
+++ NtupleMacros/HuntGather2011/babymaker/makeGatherBaby.C
@@ -6,9 +6,11 @@ void makeGatherBaby(const char *inputFileName, const char *outputFileName)
     gSystem->Load("libPhysics.so");
     gSystem->Load("libEG.so");
     gSystem->Load("libMathCore.so");
+    gSystem->Load("libTMVA.so");
     gSystem->Load("../libs/libCMS2NtupleMacrosCORE.so");
     gSystem->Load("../libs/libHuntGather2011Babymaker.so");
     gSystem->Load("../libs/libCMS2NtupleMacrosCOREMT2.so");
+    gSystem->Load("../libs/libCMS2NtupleMacrosTools.so");
     gSystem->Load("../../Tools/MiniFWLite/libMiniFWLite.so");
 
     dilepbabymaker *dilepbaby = new dilepbabymaker();
