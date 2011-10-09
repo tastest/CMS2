83c0cc723e5194e0cf959a8364ecd3493db30fc8
yanjuntu <yanjuntu>
2011-10-09 19:30:18 +0000

    small update

--- NtupleMacros/Tools/skimData/checkAndRunSkim.sh
+++ NtupleMacros/Tools/skimData/checkAndRunSkim.sh
@@ -15,7 +15,7 @@ echo "Start skims at "`date`
 for di in ${input_dir_sub}; do
     ls ${input_dir_sub}/${pattern} 
     find ${di} -name ${pattern} | while read -r f; do
-	fO=`echo ${f} | sed -e "s?${input_dir_sub}/?${output_dir_sub}/?g" `
+	fO=`echo ${f} | sed -e "s?${input_dir_sub}?${output_dir_sub}?g" `
 	[ ! -f "${fO}" -o "${f}" -nt ${fO} ]   && echo ${fO} && root -l -b -q "makeSkim.C(\"${f}\",\"${fO}_ready\",\"${skim_C}\",\"${miniFWlib}\")"
 	[ -f "${fO}_ready" ] && echo ${fO}_ready && mv -f ${fO}_ready ${fO}
     done
