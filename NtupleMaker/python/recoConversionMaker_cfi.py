f0a1b8d7e190c43f28dd03e945a7d8d12441644f
olivito <olivito>
2011-10-13 04:48:09 +0000

    This commit was manufactured by cvs2svn to create tag 'huntgather2011_V00-02-03'.

--- NtupleMaker/python/recoConversionMaker_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-recoConversionMaker = cms.EDProducer("RecoConversionMaker",
-                                     recoConversionInputTag = cms.InputTag("allConversions"),
-                                     beamSpotInputTag       = cms.InputTag("offlineBeamSpot")
-)
-
-
