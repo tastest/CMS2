7b9b69bdf0b6669cf9fb2882ce2a2274344b24af
slava77 <slava77>
2011-10-24 21:05:18 +0000

    more comments

--- NtupleMacros/Tools/twoFakesSolution.C
+++ NtupleMacros/Tools/twoFakesSolution.C
@@ -1,6 +1,8 @@
 //solutions are taken from AN-2010-261, the notation is preserved to some extent
 
-//prompt-prompt passing numerator cuts
+//same flavor: returns N prompt-prompt passing numerator cuts
+// inputs: n2 both numerators, n1 only one passes numerator cuts, n0 both leptons fail numerator
+// inputs: eff -- efficiency of prompt/signal wrt denominator cuts; fr -- fake rate
 double frSF_pp(double n2, double n1, double n0, double eff, double fr){
   double epsilon = fr/(1. - fr);
   double eta = (1. - eff)/eff;
@@ -12,7 +14,9 @@ double frSF_pp(double n2, double n1, double n0, double eff, double fr){
   return (n2 - epsilon*n1 + epsilon*epsilon*n0)*frac;
 }
 
-//prompt-fake passing numerator cuts
+//same flavor: returns N prompt-fake passing numerator cuts
+// inputs: n2 both numerators, n1 only one passes numerator cuts, n0 both leptons fail numerator
+// inputs: eff -- efficiency of prompt/signal wrt denominator cuts; fr -- fake rate
 double frSF_pf(double n2, double n1, double n0, double eff, double fr){
   double epsilon = fr/(1. - fr);
   double eta = (1. - eff)/eff;
@@ -24,7 +28,9 @@ double frSF_pf(double n2, double n1, double n0, double eff, double fr){
   return epsilon*(-2.*epsilon*n0 + (1.+epsilon*eta)*n1 - 2.*eta*n2)*frac;
 }
 
-//fake-fake passing numerator cuts
+// same flavor: returns N fake-fake passing numerator cuts
+// inputs: n2 both numerators, n1 only one passes numerator cuts, n0 both leptons fail numerator
+// inputs: eff -- efficiency of prompt/signal wrt denominator cuts; fr -- fake rate
 double frSF_ff(double n2, double n1, double n0, double eff, double fr){
   double epsilon = fr/(1. - fr);
   double eta = (1. - eff)/eff;
@@ -36,8 +42,10 @@ double frSF_ff(double n2, double n1, double n0, double eff, double fr){
   return epsilon*epsilon*(n0 - eta*n1 + eta*eta*n2)*frac;
 }
 
-// prompt-prompt passing numerator cuts
-// nE1M1 -- both numerators; n10 e-num mu-nonnum; n01 e-nonum mu-num; n0 -- both nonnumerators
+// opposite flavor: returns N prompt-prompt passing numerator cuts
+// inputs: nE1M1 -- both numerators; n10 e-num mu-nonnum; n01 e-nonum mu-num; n0 -- both nonnumerators
+//         effE (effM) -- efficiency of prompt/signal wrt denominator cuts for electrons (muons)
+//         frE (frM) -- fake rate for electrons (muons)
 double frOF_EpMp(double nE1M1, double nE1M0, double nE0M1, double nE0M0, 
 	       double effE, double effM, double frE, double frM){
 
@@ -52,8 +60,10 @@ double frOF_EpMp(double nE1M1, double nE1M0, double nE0M1, double nE0M0,
   return frac* ( nE1M1 - epsilonM*nE1M0 - epsilonE*nE0M1 + epsilonE*epsilonM*nE0M0);
 }
 
-// e-prompt mu-fake passing numerator cuts
-// nE1M1 -- both numerators; n10 e-num mu-nonnum; n01 e-nonum mu-num; n0 -- both nonnumerators
+// opposite flavor: returns N e-prompt mu-fake passing numerator cuts
+// inputs: nE1M1 -- both numerators; n10 e-num mu-nonnum; n01 e-nonum mu-num; n0 -- both nonnumerators
+//         effE (effM) -- efficiency of prompt/signal wrt denominator cuts for electrons (muons)
+//         frE (frM) -- fake rate for electrons (muons)
 double frOF_EpMf(double nE1M1, double nE1M0, double nE0M1, double nE0M0, 
 	       double effE, double effM, double frE, double frM){
 
@@ -68,8 +78,10 @@ double frOF_EpMf(double nE1M1, double nE1M0, double nE0M1, double nE0M0,
   return frac*epsilonM * ( -epsilonE*nE0M0 + nE1M0 + epsilonE*etaM*nE0M1 - etaM*nE1M1);
 }
 
-// e-fake mu-prompt passing numerator cuts
-// nE1M1 -- both numerators; n10 e-num mu-nonnum; n01 e-nonum mu-num; n0 -- both nonnumerators
+// opposite flavor: returns N e-fake mu-prompt passing numerator cuts
+// inputs: nE1M1 -- both numerators; n10 e-num mu-nonnum; n01 e-nonum mu-num; n0 -- both nonnumerators
+//         effE (effM) -- efficiency of prompt/signal wrt denominator cuts for electrons (muons)
+//         frE (frM) -- fake rate for electrons (muons)
 double frOF_EfMp(double nE1M1, double nE1M0, double nE0M1, double nE0M0, 
 	       double effE, double effM, double frE, double frM){
 
@@ -84,8 +96,10 @@ double frOF_EfMp(double nE1M1, double nE1M0, double nE0M1, double nE0M0,
   return frac* epsilonE * (  -epsilonM*nE0M0 + etaE*epsilonM*nE1M0 + nE0M1 - etaE*nE1M1);
 }
 
-// e-fake mu-fake passing numerator cuts
-// nE1M1 -- both numerators; n10 e-num mu-nonnum; n01 e-nonum mu-num; n0 -- both nonnumerators
+// opposite flavor: returns e-fake mu-fake passing numerator cuts
+// inputs: nE1M1 -- both numerators; n10 e-num mu-nonnum; n01 e-nonum mu-num; n0 -- both nonnumerators
+//         effE (effM) -- efficiency of prompt/signal wrt denominator cuts for electrons (muons)
+//         frE (frM) -- fake rate for electrons (muons)
 double frOF_EfMf(double nE1M1, double nE1M0, double nE0M1, double nE0M0, 
 	       double effE, double effM, double frE, double frM){
 
