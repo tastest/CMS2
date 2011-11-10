fe6a1062f8bcc61aa8e796ba7f678383b2765a14
olivito <olivito>
2011-11-10 19:13:31 +0000

    This commit was manufactured by cvs2svn to create tag 'ssCORE10November2011'.

--- NtupleMacros/Tools/bTagEff_BTV.h
+++
@@ -1,7 +0,0 @@
-#include <string.h>
-double getMisTagRate(double jet_pt, double jet_eta, string algo);
-double getMisTagRate_Err(double jet_pt, double jet_eta, string algo);
-double getMisTagSF(double jet_pt, double jet_eta, string algo);
-double getMisTagSF_Err(double jet_pt, double jet_eta, string algo);
-double getBTagSF(double jet_pt, double jet_eta, string algo);
-double getBTagSF_Err(double jet_pt, double jet_eta, string algo);
