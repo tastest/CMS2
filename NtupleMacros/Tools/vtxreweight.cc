dfaeeabc6b3c4823cf98229957b49b6dec89a5e3
warren <warren>
2012-02-06 23:11:16 +0000

    Add new function vtxweight_n which takes n vertices as arg instead of recalculating. Add new hist (alt) and optional args to both functions to use them. Interface to old function vtxweight unchanged.

--- NtupleMacros/Tools/vtxreweight.cc
+++ NtupleMacros/Tools/vtxreweight.cc
@@ -34,34 +34,59 @@
 // WHERE NVTX IS THE NUMBER OF DA VERTICES PASSING isGoodDAVertex()
 //------------------------------------------------------------------------------
 
-// $Id: vtxreweight.cc,v 1.4 2011/05/20 14:24:22 warren Exp $
+// $Id: vtxreweight.cc,v 1.5 2012/02/06 23:11:16 warren Exp $
 
 // CINT is allowed to see this, but nothing else:
 #include "vtxreweight.h"
 
 #ifndef __CINT__
 
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <set>
-#include <string>
+#include <iostream>
 
 #include "../CORE/CMS2.h"
 #include "../CORE/eventSelections.h"
 
 bool loaded_vtxreweight_hist = false;
+bool loaded_vtxreweight_hist_alt = false;
 
-float vtxweight( bool isData , bool useDAVertices ){
+float vtxweight_n( const int nvertices , const bool isData , const bool usealt ) {
 
   if( isData ) return 1;
 
-  if( !loaded_vtxreweight_hist ){
+  if( (!usealt && !loaded_vtxreweight_hist) || 
+	  (usealt && !loaded_vtxreweight_hist_alt) ){
+    cout << "vtxreweight.cc: You need to do"                              << endl;
+    cout << "set_vtxreweight_rootfile( filename )"                        << endl;
+    cout << "before calling vtxweight()"                                  << endl;
+    cout << "a sample vtxreweight file can be found at"                   << endl;
+    cout << "/tas/benhoob/vtxreweight/vtxreweight_Spring11MC_23pbPR.root" << endl;
+    cout << "now, quitting"                                               << endl;
+    exit(2);
+  }
+
+  int nvtx = nvertices;
+  TH1F* hist = (usealt ? vtxreweight_hist_alt : vtxreweight_hist );
+  if( nvtx > hist->GetNbinsX() )
+	nvtx = hist->GetNbinsX();
+
+  float weight = 0;
+  weight = hist->GetBinContent( hist->FindBin(nvtx) );
+  if( weight <= 0 ) //we don't want to kill events bc they have no weight
+	weight = 1.;
+  //cout << "nvtx " << nvtx << " weight " << weight << endl;
+  return weight;
+
+}
+
+
+//this is the original version--it used to count vertices and then get the weight
+//move the weight to the above (new) version
+float vtxweight( const bool isData , const bool useDAVertices , const bool usealt ){
+
+  if( isData ) return 1;
+
+  if( (!usealt && !loaded_vtxreweight_hist) || 
+	  (usealt && !loaded_vtxreweight_hist_alt) ){
     cout << "vtxreweight.cc: You need to do"                              << endl;
     cout << "set_vtxreweight_rootfile( filename )"                        << endl;
     cout << "before calling vtxweight()"                                  << endl;
@@ -100,14 +125,10 @@ float vtxweight( bool isData , bool useDAVertices ){
     return 0;
   }
 
-  if( nvtx > vtxreweight_hist->GetNbinsX() ) nvtx = vtxreweight_hist->GetNbinsX();
-
-  //cout << "nvtx " << nvtx << " weight " << vtxreweight_hist->GetBinContent(nvtx) << endl;
-  return vtxreweight_hist->GetBinContent(nvtx);
-
+  return vtxweight_n( nvtx, isData, usealt );
 }
 
-void set_vtxreweight_rootfile ( const char* filename , bool verbose ){
+void set_vtxreweight_rootfile ( const char* filename , const bool verbose , const bool usealt ){
   TFile* file = TFile::Open(filename);
 
   if( file == 0 ){
@@ -116,10 +137,15 @@ void set_vtxreweight_rootfile ( const char* filename , bool verbose ){
     exit(0);
   }
 
-  vtxreweight_hist = (TH1F*) file->Get("hratio");
+  if( !usealt )
+	vtxreweight_hist = (TH1F*) file->Get("hratio");
+  else 
+	vtxreweight_hist_alt = (TH1F*) file->Get("hratio_alt");
+  TH1F* hist = (usealt ? vtxreweight_hist_alt : vtxreweight_hist );
 
-  if( vtxreweight_hist == 0 ){
-    cout << "vtxreweight.cc: error, couldn't retrieve hist hratio from file " << filename << endl;
+  if( hist == 0 ){
+    cout << "vtxreweight.cc: error, couldn't retrieve hist hratio"
+		 << (usealt ? "_alt" : "") << " from file " << filename << endl;
     cout << "Quitting" << endl;
     exit(1);
   }
@@ -128,16 +154,20 @@ void set_vtxreweight_rootfile ( const char* filename , bool verbose ){
     cout << "Opened vtx reweighting file " << filename << endl;
     
     cout << "|" << setw(10) << "nvtx"   << setw(4) 
-	 << "|" << setw(10) << "weight" << setw(4) << "|" << endl;
+		 << "|" << setw(10) << "weight" << setw(4) << "|" << endl;
 
-    for(unsigned int ibin = 1 ; ibin <= (unsigned int)vtxreweight_hist->GetNbinsX() ; ++ibin ){
+    for(unsigned int ibin = 1 ; ibin <= (unsigned int)hist->GetNbinsX() ; ++ibin ){
 
-      cout << "|" << setw(10) << ibin                                   << setw(4) 
-	   << "|" << setw(10) << vtxreweight_hist->GetBinContent(ibin) << setw(4) << "|" << endl;
+      cout << "|" << setw(10) << ibin << setw(4) 
+		   << "|" << setw(10) << hist->GetBinContent(ibin) << setw(4) << "|" << endl;
 
     }
   }
-  loaded_vtxreweight_hist = true;
+
+  if( !usealt )
+	loaded_vtxreweight_hist = true;
+  else
+	loaded_vtxreweight_hist_alt = true;
 
 }
 
