a0e4f5067a6fac40adff96b1d4c0fca4e348dbab
dlevans <dlevans>
2012-04-10 13:45:31 +0000

    add code for lepton tree making from CMS2 ntuple

--- NtupleMacros/HWW2012/LinkDef.h
+++ NtupleMacros/HWW2012/LinkDef.h
@@ -1,10 +1,14 @@
+
+#include "LeptonTreeMaker.h"
+
 #ifdef __CINT__
- 
+
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
 
+#pragma link C++ class LeptonTreeMaker+;
 
 //#pragma link C++ class ROOT::Math::PxPyPzE4D<float>+; 
 //#pragma link C++ class ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >+;
