8879e8a7a0219124d3be11fba5a322e1a16d880f
fgolf <fgolf>
2012-04-16 22:25:11 +0000

    remove 2010 stuff; clean up; add 2012 SS selections

--- NtupleMacros/fakeRate/myBabyMaker.h
+++ NtupleMacros/fakeRate/myBabyMaker.h
@@ -28,7 +28,7 @@ public:
     void InitBabyNtuple ();
     void FillBabyNtuple ();
     void CloseBabyNtuple ();
-    void ScanChain (TChain *, const char *, bool, int, std::string jetcorrPath="../CORE/jetcorr/data/");
+    void ScanChain (TChain *chain, const char *babyFileName, bool isData, int eormu, bool applyFOfilter = true, std::string jetcorrPath="../CORE/jetcorr/data/");
     void SetGoodRunList(const char* fileName, bool goodRunIsJson=false);
 
 private:
@@ -93,10 +93,6 @@ private:
     Float_t foel_mass_;
     Float_t fomu_mass_;
 
-    // tcmet
-    Float_t tcmet_;
-    Float_t tcmetphi_;
-  
     // pfmet
     Float_t pfmet_;
     Float_t pfmetphi_;
@@ -148,8 +144,7 @@ private:
 
     // HT
     float ht_calo_;          
-    float ht_calo_L2L3_;     
-    float ht_jpt_L2L3_;      
+    float ht_calo_L2L3_;
     float ht_pf_;            
     float ht_pf_L2L3_;       
     float ht_pf_L1FastL2L3_;  
@@ -172,30 +167,30 @@ private:
     //////////////////////////////////////////////////////
 
     //////////
-    // 2011 //
+    // 2012 //
     //////////
   
     // SS
       
     // Electrons
-    Bool_t num_el_ssV3_;
-    Bool_t v1_el_ssV3_;
-    Bool_t v2_el_ssV3_;
-    Bool_t v3_el_ssV3_;
+    Bool_t num_el_ssV7_;
+    Bool_t num_el_ssV7_noIso_;
+    Bool_t v1_el_ssV7_;
+    Bool_t v2_el_ssV7_;
+    Bool_t v3_el_ssV7_;
+    
+    // Muons
+    Bool_t num_mu_ssV5_;        // NominalSSv5
+    Bool_t num_mu_ssV5_noIso_;  // NominalSSv5 with no isolation applied
+    Bool_t fo_mu_ssV5_;         // muonSelectionFO_mu_ssV5
+    Bool_t fo_mu_ssV5_noIso_;   // muonSleectionFO_mu_ssV5 with no isolation applied
 
-    // Electrons
-    Bool_t num_el_ssV4_;
-    Bool_t v1_el_ssV4_;
-    Bool_t v2_el_ssV4_;
-    Bool_t v3_el_ssV4_;
-
-    // Electrons
-    Bool_t num_el_ssV5_;
-    Bool_t v1_el_ssV5_;
-    Bool_t v2_el_ssV5_;
-    Bool_t v3_el_ssV5_;
-    Bool_t num_el_ssV5_noIso_;
-
+    //////////
+    // 2011 //
+    //////////
+  
+    // SS
+      
     // Electrons
     Bool_t num_el_ssV6_;
     Bool_t v1_el_ssV6_;
@@ -203,10 +198,6 @@ private:
     Bool_t v3_el_ssV6_;
     Bool_t num_el_ssV6_noIso_;
     
-    // Muons
-    Bool_t numNomSSv3_;   // NominalSSv3
-    Bool_t fo_mussV3_04_; // muonSelectionFO_mu_ssV3
-
     // Muons
     Bool_t numNomSSv4_;   // NominalSSv4
     Bool_t fo_mussV4_04_; // muonSelectionFO_mu_ssV4
@@ -240,55 +231,6 @@ private:
     Bool_t num_mu_OSGV3_;  // OSGeneric_v3
     Bool_t fo_el_OSV3_;    // electronSelection_el_OSV3_FO
     Bool_t fo_mu_OSGV3_;   // OSGeneric_v3_FO
-
-
-    //////////
-    // 2010 //
-    //////////
-  
-    // ttbar
-  
-    // electrons ( not filled for muons )
-    Bool_t numOct6_;      // electronSelection_ttbarV1_pass5
-    Bool_t v1Oct6_;       // electronSelectionFO_el_ttbarV1_v1_pass5
-    Bool_t v2Oct6_;       // electronSelectionFO_el_ttbarV1_v1_pass5
-    Bool_t v3Oct6_;       // electronSelectionFO_el_ttbarV1_v1_pass5
-  
-    // muons     ( not filled for electrons )
-    Bool_t num_;          // NominalTTbarV2
-    Bool_t fo_04_;        // muonSelectionFO_mu_ttbar
-    Bool_t fo_10_;        // muonSelectionFO_mu_ttbar_iso10
-  
-    // Same Sign Susy
-
-    // electrons ( not filled for muons )
-    Bool_t numSSV2_;      // electronSelection_ssV2, false, false && !isSpikeElectron
-    Bool_t v1SSV2_;       // electronSelectionFOV2_ssVBTF80_v1, false, false
-    Bool_t v2SSV2_;       // electronSelectionFOV2_ssVBTF80_v2, false, false
-    Bool_t v3SSV2_;       // electronSelectionFOV2_ssVBTF80_v3, false, false
-
-    // muons
-    Bool_t numNomSSv2_;   // NominalSSv2
-    Bool_t fo_mussV2_04_; // muonSelectionFO_mu_ssV2
-    Bool_t fo_mussV2_10_; // muonSelectionFO_mu_ssV2_iso10
-  
-    // Opposite Sign Susy
-    Bool_t num_OSGv1_;      // OSGeneric_v1 (muons) | nothing for ele
-    Bool_t num_OSZv1_;      // OSZ_v1 (muons)       | nothing for ele
-    Bool_t numOSOct18_;     // not filled for muons; electronSelection_el_OSV1 (for electrons)
-    Bool_t v1OSOct18_;      // electronSelectionFO_el_OSV1_v1
-    Bool_t v2OSOct18_;      // electronSelectionFO_el_OSV1_v2
-    Bool_t v3OSOct18_;      // electronSelectionFO_el_OSV1_v2
-  
-    // WW
-    Bool_t num_wwV1_;       // electronSelection_wwV1 | NominalWWV1 (muons)
-    Bool_t v1_wwV1_;        // electronSelectionFO_el_wwV1_v1
-    Bool_t v2_wwV1_;        // electronSelectionFO_el_wwV1_v2
-    Bool_t v3_wwV1_;        // electronSelectionFO_el_wwV1_v3
-    Bool_t v4_wwV1_;        // electronSelectionFO_el_wwV1_v4
-    Bool_t fo_wwV1_04_;     // muonSelectionFO_mu_wwV1
-    Bool_t fo_wwV1_10_;     // muonSelectionFO_mu_wwV1_iso10
-    Bool_t fo_wwV1_10_d0_;  // muonSelectionFO_mu_wwV1_iso10_d0
 
     //////////////////////////////////////////////////////
     // End Fake Rate Numerator & Denominator Selections //
@@ -452,102 +394,7 @@ HLT_Mu30_eta2p1_v3
     // End 2012 Triggers //
     ///////////////////////
 
-    ///////////////////////  
-    // 2010 Triggers     //
-    ///////////////////////
-
-    // Jet Triggers
   
-    // did it pass the jet trigger and is this lepton unbiased
-    // 0=fail 1="pass but biased" 2="pass and unbiased"  -1="pass but cant find jet trg obj"
-    Int_t  hlt15u_; // HLT_Jet15U
-    Int_t  hlt30u_; // HLT_Jet30U
-    Int_t  hlt50u_; // HLT_Jet50U
-    Int_t  l16u_;   // HLT_L1Jet6U
-    Int_t  l110u_;   // HLT_L1Jet10U
-  
-    // Electron Triggers
-  
-    // What electron trigger did it pass and is the electron matched to a egamma trg object
-    // 0=fail 1="pass but no match" 2="pass and matched" -1="pass but egamm trg obj missing"
-    Int_t ph10_;    // HLT_Photon10_L1R or HLT_Photon10_Cleaned_L1R
-    Int_t ph15_;    // HLT_Photon15_L1R or HLT_Photon15_Cleaned_L1R
-    Int_t ph20_;    // HLT_Photon20_Cleaned_L1R
-    Int_t el10_lw_;     // HLT_Ele10_LW_L1R
-    Int_t el10_sw_;     // HLT_Ele10_SW_L1R
-    Int_t el10_sw_v2_;  // HLT_Ele10_SW_L1R_v2
-    Int_t el10_lw_id_;  // HLT_Ele10_LW_EleId_L1R
-    Int_t el10_sw_id_;  // HLT_Ele10_SW_EleId_L1R
-    Int_t el15_lw_;     // HLT_Ele15_LW_L1R
-    Int_t el15_sw_;     // HLT_Ele15_SW_L1R
-    Int_t el15_lw_id_;  // HLT_Ele15_LW_EleId_L1R
-    Int_t el15_sw_id_;  // HLT_Ele15_SW_EleId_L1R
-    Int_t el15_sw_cid_; // HLT_Ele15_SW_CaloEleId_L1R
-    Int_t el20_sw_;     // HLT_Ele20_SW_L1R
-    Int_t el25_sw_;     // HLT_Ele25_SW_L1R
-    Int_t el17_sw_;     // HLT_Ele17_SW_L1R
-    Int_t el17_sw_v2_;  // HLT_Ele17_SW_L1R_v2
-    Int_t el17_iso_;    // HLT_Ele17_Isol_L1R
-    Int_t el17_loose_;  // HLT_Ele17_SW_LooseEleId_L1R
-    Int_t el17_sw_cid_; // HLT_Ele17_SW_CaloEleId_L1R
-    Int_t el17_sw_id_;  // HLT_Ele17_SW_EleId_L1R
-    Int_t el17_tiso_;   // HLT_Ele17_SW_TightEleIdIsol_L1R
-    Int_t el17_tiso_v1_;// HLT_Ele17_SW_TightEleIdIsol_L1R_v1
-    Int_t Del10_sw_;    // HLT_DoubleEle10_SW_L1R
-  
-    //  Minimm dR to the closest eg object
-    Float_t drph10_;    // HLT_Photon10_L1R or HLT_Photon10_Cleaned_L1R
-    Float_t drph15_;    // HLT_Photon15_L1R or HLT_Photon15_Cleaned_L1R
-    Float_t drph20_;    // HLT_Photon20_Cleaned_L1R
-    Float_t drel10_lw_;    // HLT_Ele10_LW_L1R
-    Float_t drel10_sw_;    // HLT_Ele10_SW_L1R
-    Float_t drel10_sw_v2_; // HLT_Ele10_SW_L1R_v2
-    Float_t drel10_lw_id_; // HLT_Ele10_LW_EleId_L1R
-    Float_t drel10_sw_id_; // HLT_Ele10_SW_EleId_L1R
-    Float_t drel15_lw_;    // HLT_Ele15_LW_L1R
-    Float_t drel15_sw_;    // HLT_Ele15_SW_L1R
-    Float_t drel15_lw_id_; // HLT_Ele15_LW_EleId_L1R
-    Float_t drel15_sw_id_; // HLT_Ele15_SW_EleId_L1R
-    Float_t drel15_sw_cid_; // HLT_Ele15_SW_CaloEleId_L1R
-    Float_t drel20_sw_;     // HLT_Ele20_SW_L1R
-    Float_t drel25_sw_;     // HLT_Ele25_SW_L1R
-    Float_t drel17_sw_;     // HLT_Ele17_SW_L1R
-    Float_t drel17_sw_v2_;  // HLT_Ele17_SW_L1R_v2
-    Float_t drel17_iso_;    // HLT_Ele17_Isol_L1R
-    Float_t drel17_loose_;  // HLT_Ele17_SW_LooseEleId_L1R
-    Float_t drel17_sw_cid_; // HLT_Ele17_SW_CaloEleId_L1R
-    Float_t drel17_sw_id_;  // HLT_Ele17_SW_EleId_L1R
-    Float_t drel17_tiso_;   // HLT_Ele17_SW_TightEleIdIsol_L1R
-    Float_t drel17_tiso_v1_;// HLT_Ele17_SW_TightEleIdIsol_L1R_v1
-    Float_t drDel10_sw_;    // HLT_DoubleEle10_SW_L1R
-  
-    // Muon Triggers
-  
-    // What muon trigger did it pass
-    // 0=fail 1="pass but no match" 2="pass and matched" -1="pass but muon trg obj missing"
-    Int_t mu17_;      // HLT_Mu17_v1
-    Int_t mu15_;      // HLT_Mu15_v1
-    Int_t mu13_;      // HLT_Mu13_v1
-    Int_t mu11_;      // HLT_Mu11
-    Int_t mu9_;       // HLT_Mu9
-    Int_t mu7_;       // HLT_Mu7
-    Int_t mu5_;       // HLT_Mu5
-  
-    //  Minimm dR to the closest HLT mu object
-    Float_t drmu17_;  // HLT_Mu17_v1
-    Float_t drmu15_;  // HLT_Mu15_v1
-    Float_t drmu13_;  // HLT_Mu13_v1
-    Float_t drmu11_;  // HLT_Mu11
-    Float_t drmu9_;   // HLT_Mu9
-    Float_t drmu7_;   // HLT_Mu7
-    Float_t drmu5_;   // HLT_Mu5
-  
-    ///////////////////////  
-    // End 2010 Triggers //
-    ///////////////////////
-
-
-
     //////////////
     // Jets     //
     //////////////
@@ -593,13 +440,6 @@ HLT_Mu30_eta2p1_v3
     // Same for btagged PF Corrected jets L1FastL2L3
     Float_t ptbtagpfcL1Fj1_;       // highest pt btagged jet well separated from the lepton
     Float_t dphibtagpfcL1Fj1_;     // dphi between highest pt btagged jet well separated from the lepton and lepton
-
-    // Same for corrected JPT jets
-    Float_t ptjptcj1_;       // highest pt jet well separated from the lepton
-    Float_t ptjptcj1_b2b_;   // highest pt jet away frmo lepton by dR >= 1.0 and dPhi > 2.5
-    Float_t dphijptcj1_b2b_; // dphi between lepton and jet for jets away from lepton by dR >= 1.0
-    Int_t   njptcj1_;        // number of jets above 10 GeV and away from lepton by dR >= 1.0
-    Bool_t  btagjptc_; 
     
     Float_t rho_;
 
