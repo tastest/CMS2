9475b79e05054956241a804ba7868da3838090b7
olivito <olivito>
2012-04-23 05:48:36 +0000

    This commit was manufactured by cvs2svn to create tag 'FakeRate23April2012'.

--- NtupleMaker/python/sParmMaker_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-sParmMaker = cms.EDProducer("SParmMaker",
-                            sparm_inputTag       = cms.InputTag("source"),
-                            aliasPrefix          = cms.untracked.string("sparm")
-)
