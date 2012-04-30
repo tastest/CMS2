0cecf5dd14899973e4d988ae5e0184bc3783a1da
dlevans <dlevans>
2012-04-30 09:38:36 +0000

    update to skimming script

--- NtupleMacros/LeptonTreeLooper/scripts/doSkimLeptonTree.sh
+++ NtupleMacros/LeptonTreeLooper/scripts/doSkimLeptonTree.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+### 28th april good run list
+CERT="Cert_190456-191859_8TeV_PromptReco_Collisions12_JSON"
+CERTNAME="../runlists/${CERT}.jmu"
+TAG="V00-01-07"
+root -b -q skimLeptonTree.C+\(\"${CERTNAME}\",\"/smurf/dlevans/LeptonTree/${TAG}/SingleMuRun2012APromptV1/\",\"merged_${CERT}.root\"\)
+root -b -q skimLeptonTree.C+\(\"${CERTNAME}\",\"/smurf/dlevans/LeptonTree/${TAG}/DoubleMuRun2012APromptV1/\",\"merged_${CERT}.root\"\)
+
+
+
 ###
 CERT="../runlists/HWW_2011.jmu"
 TAG="V00-01-05"
@@ -13,7 +22,7 @@ TAG="V00-01-06"
 #root -b -q skimLeptonTree.C+\(\"${CERT}\",\"/smurf/dlevans/LeptonTree/${TAG}/SingleMuRun2012APromptV1/\",\"merged_Cert_190456-191276.root\"\)
 #root -b -q skimLeptonTree.C+\(\"${CERT}\",\"/smurf/dlevans/LeptonTree/${TAG}/SingleElectronRun2012APromptV1/\",\"merged_Cert_190456-191276.root\"\)
 #root -b -q skimLeptonTree.C+\(\"${CERT}\",\"/smurf/dlevans/LeptonTree/${TAG}/DoubleElectronRun2012APromptV1/\",\"merged_Cert_190456-191276.root\"\)
-root -b -q skimLeptonTree.C+\(\"${CERT}\",\"/smurf/dlevans/LeptonTree/${TAG}/PhotonsRun2012APromptV1/\",\"merged_Cert_190456-191276.root\"\)
+#root -b -q skimLeptonTree.C+\(\"${CERT}\",\"/smurf/dlevans/LeptonTree/${TAG}/PhotonsRun2012APromptV1/\",\"merged_Cert_190456-191276.root\"\)
 
 ##
 CERT="../runlists/DCSOnly_2012A_230412.jmu"
