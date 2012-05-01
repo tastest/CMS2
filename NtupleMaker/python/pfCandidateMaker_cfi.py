d06305aee4c2f43c264add231b454d1d073d9a7c
olivito <olivito>
2012-05-01 21:44:26 +0000

    This commit was manufactured by cvs2svn to create tag 'FakeRate01May2012'.

--- NtupleMaker/python/pfCandidateMaker_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-pfCandidateMaker = cms.EDProducer("PFCandidateMaker",
-                                  pfCandidatesTag     = cms.InputTag("particleFlow"),
-                                  pfElectronsTag      = cms.InputTag("particleFlow","electrons"),
-                                  tracksInputTag      = cms.InputTag("generalTracks"),
-                                  vertexInputTag      = cms.InputTag("offlinePrimaryVertices"),
-                                  minDRelectron       = cms.double(0.1)
-)
