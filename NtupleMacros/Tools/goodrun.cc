a344967389c54133265602d8abf5faaad1c4d558
kelley <kelley>
2012-05-11 16:32:21 +0000

    fixed subtle but with assert -- needed #define NDEBUG

--- NtupleMacros/Tools/goodrun.cc
+++ NtupleMacros/Tools/goodrun.cc
@@ -1,10 +1,11 @@
-// $Id: goodrun.cc,v 1.10 2011/03/30 22:20:27 warren Exp $
+// $Id: goodrun.cc,v 1.11 2012/05/11 16:32:21 kelley Exp $
 
 // CINT is allowed to see this, but nothing else:
 #include "goodrun.h"
 
 #ifndef __CINT__
 
+#define NDEBUG
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
