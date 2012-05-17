ee1f0e070ed3b3088333b37dbab9f06c1279c579
benhoob <benhoob>
2012-05-17 10:26:58 +0000

    remove good DA vertex option

--- NtupleMacros/Tools/vtxreweight.h
+++ NtupleMacros/Tools/vtxreweight.h
@@ -1,7 +1,7 @@
 #ifndef VTXREWEIGHT_H
 #define VTXREWEIGHT_H
 
-float vtxweight( const bool isData = false , const bool useDAVertices = true , const bool usealt = false );
+float vtxweight( const bool isData = false , const bool usealt = false );
 float vtxweight_n( const int nvtx , const bool isData = false , const bool usealt = false );
 void set_vtxreweight_rootfile ( const char* filename = "vtxreweight.root" , bool verbose = false , const bool usealt = false );
 
