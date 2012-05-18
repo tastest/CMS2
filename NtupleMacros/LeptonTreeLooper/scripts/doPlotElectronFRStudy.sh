fcebe82c5dbb41c55f3b92c9e1537d72dde85a57
dlevans <dlevans>
2012-05-18 11:02:02 +0000

    electron fr study

---
+++ NtupleMacros/LeptonTreeLooper/scripts/doPlotElectronFRStudy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+root -b -q plotElectronFRStudy.C\(\"EB\",\"PT20UP\"\)
+root -b -q plotElectronFRStudy.C\(\"EB\",\"PT1020\"\)
+root -b -q plotElectronFRStudy.C\(\"EE\",\"PT20UP\"\)
+root -b -q plotElectronFRStudy.C\(\"EE\",\"PT1020\"\)
+
---
+++ NtupleMacros/LeptonTreeLooper/scripts/plotElectronFRStudy.C
@@ -0,0 +1,61 @@
+
+void plotElectronFRStudy(TString det, TString pt)
+{
+
+    //
+    // load lins
+    //
+
+    gSystem->Load("libTree.so");
+    gSystem->Load("libPhysics.so");
+    gSystem->Load("libEG.so");
+    gSystem->Load("libMathCore.so");
+    gROOT->ProcessLine(".L ../libLeptonTreeLooper.so");
+
+    gROOT->ProcessLine(".L ~/tdrStyle.C");
+    gROOT->ProcessLine("setTDRStyle()");
+    gStyle->SetOptTitle(0);
+    gROOT->ForceStyle();
+
+    //
+    // open file
+    //
+
+    TString inputFile = "/smurf/dlevans/LeptonTree/V00-02-00_frtests2/DoubleElectronRun2012APromptV1/merged_Cert_190456-193336_8TeV_PromptReco_Collisions12_JSON.root";
+    TFile *f = new TFile(inputFile, "READ");
+    TTree *leptons = (TTree*)f->Get("leptons");
+    gROOT->cd();
+
+    TCut kinematics("");
+    TCut detIso  = ("hcaliso/probe.Pt() < 0.2 && ecaliso/probe.Pt() < 0.2 && trkiso/probe.Pt() < 0.2");
+    TCut pfIso04("(pfchiso04 + TMath::Max(0.0, pfnhiso04 + pfemiso04 - ea04 * TMath::Max(0.0, rhoIsoAll)))/probe.Pt() < 0.15");
+    TCut denominator("(eventSelection&(1<<4))==(1<<4) && HLT_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_probe > 0");
+    TCut numerator("(leptonSelection&(1<<6))==(1<<6)"+pfIso04);
+    if (det == "EE" && pt == "PT1020") {
+        kinematics = TCut("abs(sceta) > 1.479 && probe.Pt() < 20.0");
+    }
+    if (det == "EE" && pt == "PT20UP") {
+        kinematics = TCut("abs(sceta) > 1.479 && probe.Pt() > 20.0");
+    }
+    if (det == "EB" && pt == "PT1020") {
+        kinematics = TCut("abs(sceta) < 1.479 && probe.Pt() < 20.0");
+    }
+    if (det == "EB" && pt == "PT20UP") {
+        kinematics = TCut("abs(sceta) < 1.479 && probe.Pt() > 20.0");
+    }
+
+    TCut detIso  = ("hcaliso/probe.Pt() < 0.2 && ecaliso/probe.Pt() < 0.2 && trkiso/probe.Pt() < 0.2");
+    TCut pfIso04("(pfchiso04 + TMath::Max(0.0, pfnhiso04 + pfemiso04 - ea04 * TMath::Max(0.0, rhoIsoAll)))/probe.Pt() < 0.15");
+
+    float n_denominator = leptons->GetEntries(kinematics + denominator + detIso);
+    float n_numerator = leptons->GetEntries(numerator + kinematics + denominator + detIso);
+    printf("%s %s\t Fake Rate (DetIso Denominator) = %4.3f\n", det.Data(), pt.Data(), n_numerator/n_denominator);
+
+    float n_denominator = leptons->GetEntries(kinematics + denominator + pfIso04);
+    float n_numerator = leptons->GetEntries(numerator + kinematics + denominator + pfIso04);
+    printf("%s %s\t Fake Rate (PFIso Denominator) = %4.3f\n", det.Data(), pt.Data(), n_numerator/n_denominator);
+
+    f->Close();
+
+}
+
