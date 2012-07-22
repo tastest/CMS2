aea9f0ae9ba068df8270eb228591417bb95a46f3
jaehyeok <jaehyeok>
2012-07-22 23:06:39 +0000

    example scripts to run parallel jobs

---
+++ NtupleMacros/HWW2012/run_uaf8.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 190450 191426 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 191426 193687 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 193752 194108 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 194108 194225 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 194225 194455 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 194455 194691 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 194691 194896 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 194896 195015 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 195015 195164 & 
+
---
+++ NtupleMacros/HWW2012/run_uaf9.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 195164 195378 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 195378 195399 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 195399 195700 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 195700 195904 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 195904 196100 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 196100 196300 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 196300 196400 &
+./processData.exe 0 /nfs-6/userdata/jaehyeok/jun11_from195538/DoubleElectron_Run2012B-PromptReco-v1/V05-02-28/vvfilter/merged_ntuple.root 196400 196600 &
