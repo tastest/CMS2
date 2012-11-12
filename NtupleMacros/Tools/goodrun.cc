29918d15c1a812db7e69ab0b3e9dbfe0e99c8411
kelley <kelley>
2012-11-12 18:58:08 +0000

    removed NDEBUG from goodrun.cc which was causing asserts to be ignored

--- NtupleMacros/Tools/goodrun.cc
+++ NtupleMacros/Tools/goodrun.cc
@@ -1,11 +1,10 @@
-// $Id: goodrun.cc,v 1.11 2012/05/11 16:32:21 kelley Exp $
+// $Id: goodrun.cc,v 1.12 2012/11/12 18:58:08 kelley Exp $
 
 // CINT is allowed to see this, but nothing else:
 #include "goodrun.h"
 
 #ifndef __CINT__
 
-#define NDEBUG
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
