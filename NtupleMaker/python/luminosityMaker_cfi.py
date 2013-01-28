30a4a21da45bcaf789c539355c3d120f343eff34
olivito <olivito>
2013-01-28 23:44:30 +0000

    This commit was manufactured by cvs2svn to create tag 'ss2012_V02-05-19'.

--- NtupleMaker/python/luminosityMaker_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from RecoLuminosity.LumiProducer.lumiProducer_cfi import *
-
-luminosityMaker = cms.EDProducer(
-  "LuminosityMaker",
-  aliasPrefix         = cms.untracked.string("ls"),
-  lumiSummaryInputTag = cms.InputTag("LumiProducer"),
-  isData              = cms.bool(False)
-)
