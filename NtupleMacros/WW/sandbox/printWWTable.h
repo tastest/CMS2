ab7fcaf85a6fa19c5c50e2a07daf14800afc57b7
olivito <olivito>
2013-02-05 00:30:32 +0000

    This commit was manufactured by cvs2svn to create tag 'FakeRate04Feb2013'.

--- NtupleMacros/WW/sandbox/printWWTable.h
+++
@@ -1,8 +0,0 @@
-#ifndef PRINTWWTABLE_H
-#define PRINTWWTABLE_H
-
-void printWWTable (const class WWLooperBase **, int, const char *, 
-		   uint32 which_ones = 0xffffffff);
-void printWWMuTagTable (const class WWLooperBase **, int, const char *);
-
-#endif
