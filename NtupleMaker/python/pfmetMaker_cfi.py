e3ad4bab118a922568b76a11561c8d4838a7ab6a
olivito <olivito>
2013-04-06 23:44:26 +0000

    This commit was manufactured by cvs2svn to create tag 'ss2012_V02-05-10'.

--- NtupleMaker/python/pfmetMaker_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-pfmetMaker = cms.EDProducer("PFMETMaker",
-                            aliasPrefix = cms.untracked.string("evt"),
-                            pfMetInputTag_ = cms.InputTag("pfMet"),
-                            pfMetCorInputTag_ = cms.InputTag("pfType1CorrectedMet")
-)
-
-
