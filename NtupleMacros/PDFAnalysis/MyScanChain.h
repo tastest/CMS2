96b227ce7b71c57e8e9d57e04d401526d6cc664a
dlevans <dlevans>
2013-04-15 09:22:09 +0000

    MyScanChain.cc

--- NtupleMacros/PDFAnalysis/MyScanChain.h
+++ NtupleMacros/PDFAnalysis/MyScanChain.h
@@ -15,24 +15,36 @@ class TTree;
 
 #define MAXWEIGHT 110
 
+// indices in LHAPDF for the genset and the 
+// alternate set
+static const unsigned int set_ = 2;
+static const unsigned int genset_ = 1;
+
 class MyScanChain {
 
     public:
 
-        MyScanChain();
+        MyScanChain() {};
+        MyScanChain(std::string genPdfName, unsigned int genPdfSubset);
         ~MyScanChain() {};
 
-        int ScanChain(std::string sampleName, std::string pdfName, const char *file);
+        int ScanChain(std::string sampleName, TChain *chain, std::string pdfName);
 
     private:
 
-        // for the smurf tree
-        float scale1fb_;
-        float Q_;
-        float x1_, x2_;
-        float id1f_, id2f_;
-        int id1_, id2_;
-        float bdt_;
+        // test event selection
+        bool Cuts();
+
+        // variables to read
+        // from tree
+        float           scale1fb_;
+        float           Q_;
+        float           x1_, x2_;
+        int             id1_, id2_;
+
+        // pdf parameters
+        std::string     genPdfName_;
+        unsigned int    genPdfSubset_;
 
 };
 
