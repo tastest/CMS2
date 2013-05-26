a7457c749d36a2f5570f59e79afb8c35f6fafa45
olivito <olivito>
2013-05-26 18:56:18 +0000

    This commit was manufactured by cvs2svn to create tag 'ss2012-V02-05-13'.

--- NtupleMacros/LeptonTreeLooper/scripts/doPlotElectronFRStudy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-root -b -q plotElectronFRStudy.C\(\"EB\",\"PT20UP\"\)
-root -b -q plotElectronFRStudy.C\(\"EB\",\"PT1020\"\)
-root -b -q plotElectronFRStudy.C\(\"EE\",\"PT20UP\"\)
-root -b -q plotElectronFRStudy.C\(\"EE\",\"PT1020\"\)
-
